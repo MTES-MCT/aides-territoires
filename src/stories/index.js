5598f42e624224e9be67b9f5c0d0e527e79f798f
yann <yann.boisselier@gmail.com>
2018-03-07 23:39:36 +0100

    update readme and stories component

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,18 +6,28 @@ import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
 import { Button, Welcome } from "@storybook/react/demo";
+import PageLoader from "../features/app/components/presentationals/pageLoader/PageLoader";
+import Header from "../features/app/components/presentationals/header/Header";
 import SearchForm from "../features/search/components/presentationals/searchForm/SearchForm";
 import SearchFormContainer from "../features/search/components/containers/searchFormContainer/SearchFormContainer";
+
+storiesOf("Header", module).add("Header", () => <Header />);
+storiesOf("PageLoader", module).add("PageLoader without text", () => (
+  <PageLoader />
+));
+storiesOf("PageLoader", module).add("PageLoader with text", () => (
+  <PageLoader>Chargement en cours ...</PageLoader>
+));
 
 storiesOf("SearchForm", module).add("SearchForm", () => (
   <SearchForm
     suggestions={[]}
-    onSearchSubmit={() => {}}
-    onSearchChange={() => {}}
+    onSearchSubmit={action("onSearchSubmit")}
+    onSearchChange={action("onSearchChange")}
   />
 ));
 
 storiesOf("SearchFormContainer", module).add(
-  "SearchForm connected to Api",
-  () => <SearchFormContainer />
+  "SearchForm with autocompletion via API",
+  () => <SearchFormContainer onSearchSubmit={action("onSearchSubmit")} />
 );
