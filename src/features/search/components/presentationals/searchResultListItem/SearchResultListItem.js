93551f6b75afe87b02a56aea5800963745d66f72
yann <yann.boisselier@gmail.com>
2018-03-13 17:52:36 +0100

    use column name instead of numeric index

--- src/features/search/components/presentationals/searchResultListItem/SearchResultListItem.js
+++ src/features/search/components/presentationals/searchResultListItem/SearchResultListItem.js
@@ -6,10 +6,10 @@ class SearchResultListItem extends React.Component {
     const { result } = this.props;
     return (
       <div className="search-result-list-item box">
-        <h2 className="title is-3">{result[1]}</h2>
-        <div className="objectif">{result[5]}</div>
-        <div className="tag lieu is-success">{result[4]}</div>
-        <div className="tag thematique">{result[0]}</div>
+        <h2 className="title is-3">{result.intitulé}</h2>
+        <div className="objectif">{result.objectifs}</div>
+        <div className="tag lieu is-success">{result.périmètre}</div>
+        <div className="tag thematique">{result.sousThématique}</div>
       </div>
     );
   }
