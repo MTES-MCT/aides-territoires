20b73cfb63cedd4c83b8a3f705d6f8ca840b431c
yann <yann.boisselier@gmail.com>
2018-03-28 16:33:19 +0200

    example config file for server

---
+++ server/example.ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "aides-territoires-server",
+      script: "./index.js",
+      watch: true,
+      env: {
+        PORT: 8100,
+        NODE_ENV: "production",
+        SEND_IN_BLUE_API_KEY:
+         "your-key",
+        CONTACT_FORM_TO: "yann@yann.fr"
+      }
+    }
+  ]
+};
