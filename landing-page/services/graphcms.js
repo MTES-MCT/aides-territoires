47dbb3e9f7201f64cbca83b475d9d36f962e55d5
yann <yann.boisselier@gmail.com>
2018-03-31 00:33:02 +0200

    remove console.log, fix pm2 perf

--- landing-page/services/graphcms.js
+++ landing-page/services/graphcms.js
@@ -3,5 +3,4 @@ import { GraphQLClient } from "graphql-request";
 const { publicRuntimeConfig } = getConfig();
 console.log("url", publicRuntimeConfig.GRAPHCMS_API_URL);
 const client = new GraphQLClient(publicRuntimeConfig.GRAPHCMS_API_URL);
-console.log("client", client);
 export default client;
