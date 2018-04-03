d1c6da3c27b4b05cf2db6d11dd1a00bc4394dcdc
yann <yann.boisselier@gmail.com>
2018-04-03 18:27:06 +0200

    ajout d'une image pour faire moins triste

--- react-app/src/layouts/defaultLayout/DefaultLayout.js
+++ react-app/src/layouts/defaultLayout/DefaultLayout.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Navigation from "../../presentationals/navigation/Navigation";
 import Header from "../../presentationals/header/Header";
+import ImageTown from "../../presentationals/imageTown/ImageTown";
+
+const styles = {
+  pageContent: {
+    paddingTop: "50px"
+  }
+};
 
 class DefaultLayout extends React.Component {
   render() {
     return (
       <div>
         <Navigation />
-        <Header />
-        <div className="page-content">{this.props.children}</div>
+        {/*<Header />*/}
+        <div style={styles.pageContent} className="page-content">
+          {this.props.children}
+        </div>
+        <ImageTown />
       </div>
     );
   }
--- react-app/src/pages/parcoursPhaseAvantProjet/ParcoursPhaseAvantProjet.js
+++ react-app/src/pages/parcoursPhaseAvantProjet/ParcoursPhaseAvantProjet.js
@@ -2,22 +2,61 @@ import React from "react";
 import DefaultLayout from "../../layouts/defaultLayout/DefaultLayout";
 import { Link } from "react-router-dom";
 import RaisedButton from "material-ui/RaisedButton";
+import Checkbox from "material-ui/Checkbox";
+import ActionFavorite from "material-ui/svg-icons/action/favorite";
+import ActionFavoriteBorder from "material-ui/svg-icons/action/favorite-border";
+import Visibility from "material-ui/svg-icons/action/visibility";
+import VisibilityOff from "material-ui/svg-icons/action/visibility-off";
 
-const style = {
-  margin: "12px"
+const styles = {
+  button: {
+    margin: "12px"
+  },
+  block: {
+    maxWidth: 500,
+    margin: "auto"
+  },
+  checkbox: {
+    marginBottom: 16
+  }
 };
 
 class ParcoursPhaseAvantProjet extends React.Component {
+  state = {
+    checked: false
+  };
+  updateCheck() {
+    this.setState(oldState => {
+      return {
+        checked: !oldState.checked
+      };
+    });
+  }
   render() {
     return (
       <DefaultLayout>
         <div className="container">
           <section className="section has-text-centered">
             <h2 className="title is-2">Phase d'avant projet</h2>
-            <Link style={style} to="/parcours/phase">
+            <h2 className="subtitle is-2">
+              petit texte de description de l'avant projet xxx
+            </h2>
+            <div className="section" style={styles.block}>
+              <Checkbox label="Maîtrise foncière" style={styles.checkbox} />
+              <Checkbox
+                label="réalisation diagnostics et études diverses"
+                style={styles.checkbox}
+              />
+              <Checkbox
+                label="Aménagement du foncier"
+                style={styles.checkbox}
+              />
+              <Checkbox label="Autre (gouvernance)" style={styles.checkbox} />
+            </div>
+            <Link style={styles.button} to="/parcours/phase">
               <RaisedButton label="Précédent" secondary={true} />
             </Link>
-            <Link style={style} to="/parcours/phase/avant-projet">
+            <Link style={styles.button} to="/parcours/phase/avant-projet">
               <RaisedButton label="Suivant" primary={true} />
             </Link>
           </section>
