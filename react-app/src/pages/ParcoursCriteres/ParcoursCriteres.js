409257b690bec544e5ab9c5a392b1b4fc5bcd9be
yann <yann.boisselier@gmail.com>
2018-04-06 13:50:44 +0200

    add page criteres

--- react-app/src/pages/ParcoursCriteres/ParcoursCriteres.js
+++ react-app/src/pages/ParcoursCriteres/ParcoursCriteres.js
@@ -2,6 +2,29 @@ import React from "react";
 import DefaultLayout from "../../layouts/DefaultLayout/DefaultLayout";
 import { Link } from "react-router-dom";
 import RaisedButton from "material-ui/RaisedButton";
+import Avatar from "material-ui/Avatar";
+import Chip from "material-ui/Chip";
+import FontIcon from "material-ui/FontIcon";
+import SvgIconFace from "material-ui/svg-icons/action/face";
+import { blue300, indigo900 } from "material-ui/styles/colors";
+
+const styles = {
+  chip: {
+    margin: 4
+  },
+  wrapper: {
+    display: "flex",
+    flexWrap: "wrap"
+  }
+};
+
+function handleRequestDelete() {
+  // alert("You clicked the delete button.");
+}
+
+function handleClick() {
+  // alert("You clicked the Chip.");
+}
 
 class ParcoursCriteres extends React.Component {
   constructor(props) {
@@ -16,10 +39,47 @@ class ParcoursCriteres extends React.Component {
         <div className="container">
           <section className="section has-text-centered">
             <h2 className="title is-2">Critères</h2>
-            <Link to="/">
-              <RaisedButton label="Rechercher" primary={true} />
-            </Link>
+            <div style={styles.wrapper}>
+              <Chip
+                onRequestDelete={handleRequestDelete}
+                onClick={handleClick}
+                style={styles.chip}
+              >
+                Quartier durable
+              </Chip>
+
+              <Chip
+                onRequestDelete={handleRequestDelete}
+                onClick={handleClick}
+                style={styles.chip}
+              >
+                <Avatar color="#444" icon={<SvgIconFace />} />
+                Critère 2
+              </Chip>
+
+              <Chip onClick={handleClick} style={styles.chip}>
+                <Avatar size={32}>A</Avatar>
+                Critère 3
+              </Chip>
+
+              <Chip
+                backgroundColor={blue300}
+                onRequestDelete={handleRequestDelete}
+                onClick={handleClick}
+                style={styles.chip}
+              >
+                <Avatar size={32} color={blue300} backgroundColor={indigo900}>
+                  new
+                </Avatar>
+                Critère 4
+              </Chip>
+            </div>
           </section>
+          <div className="has-text-centered">
+            <Link to="/parcours/phase">
+              <RaisedButton label="Continuer" primary={true} />
+            </Link>
+          </div>
         </div>
       </DefaultLayout>
     );
