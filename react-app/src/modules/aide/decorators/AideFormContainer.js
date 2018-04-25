e5942aaac24af745a6b68e768787dc465a1ebe2a
yann <yann.boisselier@gmail.com>
2018-04-25 12:13:56 +0200

    test apollo query on formContainer

--- react-app/src/modules/aide/decorators/AideFormContainer.js
+++ react-app/src/modules/aide/decorators/AideFormContainer.js
@@ -1,14 +1,24 @@
 import React from "react";
 import AideForm from "modules/aide/presentationals/AideForm";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
-export default class AideFormContainer extends React.Component {
+class AideFormContainer extends React.Component {
   state = {
     formValues: []
   };
   handleSubmit = values => {
-    // postOnRocketChatNantes(values);
+    console.log(values);
   };
   render() {
     return <AideForm onSubmit={this.handleSubmit} />;
   }
 }
+
+export default graphql(gql`
+  {
+    allPagedaccueils {
+      header
+    }
+  }
+`)(AideFormContainer);
