460d3aed929ff719eb3f522e1661d2ab929fda83
yann <yann.boisselier@gmail.com>
2018-04-30 22:55:21 +0200

    move aideProvider to search module for now

--- react-app/src/modules/search/pages/SearchAidePage.js
+++ react-app/src/modules/search/pages/SearchAidePage.js
@@ -2,14 +2,14 @@ import React from "react";
 import Layout from "../../common/layouts/Layout";
 import AideList from "modules/search/presentationals/AideList";
 import SearchFilters from "modules/search/presentationals/SearchFilters";
-import AidesProvider from "modules/aide/decorators/AidesProvider";
+import AidesProvider from "modules/search/decorators/AidesProvider";
 import queryString from "query-string";
 import { graphql, compose } from "react-apollo";
 import gql from "graphql-tag";
 import AppLoader from "modules/ui-kit/AppLoader";
 import "./SearchAidePage.css";
 
-const SearchResultListWithData = AidesProvider(AideList);
+const Aides = AidesProvider(AideList);
 
 const SearchResultsPage = class extends React.Component {
   state = {
@@ -31,7 +31,7 @@ const SearchResultsPage = class extends React.Component {
               <SearchFilters onFiltersChange={this.handlFiltersChange} />
             </div>
             <div className="column">
-              <SearchResultListWithData
+              <Aides
                 perimetreApplicationType={"commune"}
                 perimetreApplicationCode={"44109"}
                 etape={this.state.etape}
