2e1437effb5a7486ae22bde4e131851ba5355371
yann <yann.boisselier@gmail.com>
2018-05-17 12:43:59 +0200

    fix admin css

--- react-app/src/modules/admin/layouts/AdminLayout.js
+++ react-app/src/modules/admin/layouts/AdminLayout.js
@@ -1,19 +1,44 @@
 import React from "react";
 import Navigation from "modules/common/presentationals/Navigation";
 import SideMenu from "../presentationals/SideMenu";
+import injectSheet from "react-jss";
+import classnames from "classnames";
 import "bulma/css/bulma.css";
-import "./AdminLayout.css";
 
-export default class DefaultLayout extends React.Component {
+const styles = {
+  column: {
+    padding: "2rem",
+    paddingTop: "4rem",
+    minHeight: "100%"
+  },
+  sideMenu: {
+    backgroundColor: "rgb(247, 249, 250)",
+    borderRight: "1px solid rgb(211, 220, 224)"
+  },
+  appMainMenu: {
+    boxShadow: "0px 0px 40px 0px rgba(0, 0, 0, 0.3)"
+  }
+};
+
+class DefaultLayout extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
       <div className="AdminLayout">
-        <Navigation />
+        <div className={classes.appMainMenu}>
+          <Navigation />
+        </div>
         <div className="columns">
-          <div className="column column-menu is-3">
+          <div
+            className={classnames(
+              "column is-3",
+              classes.column,
+              classes.sideMenu
+            )}
+          >
             <SideMenu />
           </div>
-          <div className="column column-content is-9">
+          <div className={classnames("column is-9", classes.column)}>
             {this.props.children}
           </div>
         </div>
@@ -21,3 +46,5 @@ export default class DefaultLayout extends React.Component {
     );
   }
 }
+
+export default injectSheet(styles)(DefaultLayout);
