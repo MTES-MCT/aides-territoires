fba4e9df0542fb6519bb5126739ac1c7aa6dbe8c
yann <yann.boisselier@gmail.com>
2018-05-30 16:02:49 +0200

    localiset une aide pour un perimetre donné avec les nouveaux filtres

--- server/src/services/aide.js
+++ server/src/services/aide.js
@@ -7,130 +7,131 @@ const getAide = id => {
 };
 
 /**
- *
  * @param {*} filters
  * @param {*} sort
  */
 const searchAides = async (filters, sort) => {
+  console.log(JSON.stringify(filters, 0, 2));
   const groupesDeResultats = [];
   let totalNombreAides = 0;
   let newFilters = {};
   let aides = {};
 
-  // si on a un seul type de perimetre d'application (ex : Commune)
-  // et un code insee (ex : 44000), on fait une recherche "localisée" en premier,
-  // c'est à dire qu'on va d'abord présenter les résultats liés à la commune 44 spécifiquement.
-  // Si on ne trouve rien, on va chercher des aides pour le département Loire Atlantique puis pour la région
-  // Pays de la Loire
-  //
-  // Après cela on affichera ls aides pour toutes les communes,
-  // puis tous les départements
-  // puis toutes les régions
-  // etc...
+  // si on a un code insee, on cherche d'abord des aides
+  // sur les territoires spécifiés et alentour
+  if (
+    filters.typePerimetreInitialDeRecherche &&
+    filters.codePerimetreInitialDeRecherche
+  ) {
+    // on créer un groupe de résultats pour les aides qu'on trouve
+    // pour le territoire demandé
+    const GroupeVosTerritoires = {
+      label: "Pour votre territoire",
+      type: "vos_territoires",
+      aidesParTypeDeTerritoires: []
+    };
 
-  // on essaie d'abord d'apporter les résultats le plus localisés si
-  // un code est précié..
-  // (code région, code département etc),
-  // on créer un premier groupe de résultats avec les aides correspondantes)
-  /*
-  if (filters.perimetreApplicationCode) {
-    if (filters.perimetreApplicationType === "commune") {
-      // chercher d'abord pour la commune
-      aides = await getAllAidesByTerritoire("commune", filters, true);
-      resultsGroups.push({
-        totalCount: Object.keys(aides).length,
-        type: `votre_commune`,
-        label: `pour votre commune`,
-        aides: aides
-      });
-      const result = await axios.get(
-        `https://geo.api.gouv.fr/departements/${
-          filters.perimetreApplicationCode
-        }`
-      );
-      console.log(result);
-      // chercher pour le département de la commune. Il faut récupérer son code insee
-    }
+    // toutes les aides communales
+    aides = await getAllAidesByTerritoire(
+      filters.typePerimetreInitialDeRecherche,
+      filters,
+      filters.codePerimetreInitialDeRecherche
+    );
+
+    GroupeVosTerritoires.aidesParTypeDeTerritoires.push({
+      nombreAides: Object.keys(aides).length,
+      type: "votre_territoire",
+      label: "Sur votre territoire",
+      aides: aides
+    });
+
+    groupesDeResultats.push(GroupeVosTerritoires);
   }
-  */
-
-  // Les résultats d'aide sur le territoire (quand on a un code insee précis)
-  const GroupeVosTerritoires = {
-    label: "Pour votre territoire",
-    type: "vos_territoires",
-    aidesParTypeDeTerritoires: []
-  };
-
-  // toutes les aides communales
-  aides = await getAllAidesByTerritoire("commune", filters);
-  GroupeVosTerritoires.aidesParTypeDeTerritoires.push({
-    nombreAides: Object.keys(aides).length,
-    type: "commune",
-    label: "Département",
-    aides: aides
-  });
-
+  /*
   // les aides pour tous le territoires
   const GroupeTousLesTerritoires = {
     label: "Pour tous les territoires",
     type: "tous_les_territoires",
     aidesParTypeDeTerritoires: []
   };
+  
+  // toutes les aides communales
+  aides = await getAllAidesByTerritoire("commune", filters);
+  if (aides.length > 0) {
+    GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
+      nombreAides: Object.keys(aides).length,
+      type: "commune",
+      label: "Département",
+      aides: aides
+    });
+  }
 
   // toutes les aides départementales
 
   aides = await getAllAidesByTerritoire("departement", filters);
-  GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
-    count: Object.keys(aides).length,
-    type: "departement",
-    label: "Département",
-    aides: aides
-  });
+  if (aides.length > 0) {
+    GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
+      count: Object.keys(aides).length,
+      type: "departement",
+      label: "Département",
+      aides: aides
+    });
+  }
 
   aides = await getAllAidesByTerritoire("region", filters);
-  GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
-    count: Object.keys(aides).length,
-    type: "region",
-    label: "Région",
-    aides: aides
-  });
-
+  if (aides.length > 0) {
+    GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
+      count: Object.keys(aides).length,
+      type: "region",
+      label: "Région",
+      aides: aides
+    });
+  }
   aides = await getAllAidesByTerritoire("outre_mer", filters);
-  GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
-    count: Object.keys(aides).length,
-    type: "outre_mer",
-    label: "Outre mer",
-    aides: aides
-  });
+  if (aides.length > 0) {
+    GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
+      count: Object.keys(aides).length,
+      type: "outre_mer",
+      label: "Outre mer",
+      aides: aides
+    });
+  }
 
-  aides = await getAllAidesByTerritoire("metropole", filters);
-  GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
-    count: Object.keys(aides).length,
-    type: "metropole",
-    label: "metropole",
-    aides: aides
-  });
+  if (aides.length > 0) {
+    aides = await getAllAidesByTerritoire("metropole", filters);
+    GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
+      count: Object.keys(aides).length,
+      type: "metropole",
+      label: "metropole",
+      aides: aides
+    });
+  }
 
   // metropole + outre_mer
 
   aides = await getAllAidesByTerritoire("france", filters);
-  GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
-    count: Object.keys(aides).length,
-    type: "france",
-    label: "France",
-    aides: aides
-  });
+  if (aides.length > 0) {
+    GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
+      count: Object.keys(aides).length,
+      type: "france",
+      label: "France",
+      aides: aides
+    });
+  }
 
   aides = await getAllAidesByTerritoire("europe", filters);
-  GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
-    count: Object.keys(aides).length,
-    type: "europe",
-    label: "Europe",
-    aides: aides
-  });
+  if (aides.length > 0) {
+    GroupeTousLesTerritoires.aidesParTypeDeTerritoires.push({
+      count: Object.keys(aides).length,
+      type: "europe",
+      label: "Europe",
+      aides: aides
+    });
+  }
+  
 
-  groupesDeResultats.push(GroupeVosTerritoires);
   groupesDeResultats.push(GroupeTousLesTerritoires);
+  */
 
   // toutes les aides régionales
   const response = {
@@ -150,17 +151,25 @@ function getTotalCountFromResultsGroups(groupesDeResultats) {
   return count;
 }
 
-const getAllAidesByTerritoire = async (
-  perimetreId,
-  filters,
-  restrictByCode = false
-) => {
+const getAllAidesByTerritoire = async (perimetreId, filters, code = null) => {
   const newFilters = { ...filters };
-  newFilters.perimetreApplicationType = perimetreId;
-  // enlever la localisation sur par code si ce n'est pas explicitement demandé
-  if (restrictByCode == false && newFilters.perimetreApplicationCode) {
+  delete newFilters.texte;
+
+  if (perimetreId) {
+    newFilters.perimetreApplicationType = perimetreId;
+    delete newFilters.typePerimetreInitialDeRecherche;
+  }
+  if (code) {
+    newFilters.perimetreApplicationCode = code;
+    delete newFilters.codePerimetreInitialDeRecherche;
+  }
+  // enlever la localisation sur par code si ce
+  // n'est n'est pas explicitement demandé
+  if (code === null && newFilters.perimetreApplicationCode) {
     delete newFilters.perimetreApplicationCode;
   }
+
+  console.log(JSON.stringify(newFilters, 0, 2));
   return await getAides(newFilters);
 };
 
