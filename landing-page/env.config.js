8f8a92e6b2759e7a87ea9aecad9d71cb580fd14d
yann <yann.boisselier@gmail.com>
2018-06-12 19:21:25 +0200

    remove landing page

--- landing-page/env.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Cette configuration est récupérable à la fois côté js client et serveur
- */
-module.exports = {
-  GRAPHQL_URL: "http://localhost:8100/graphql",
-  GRAPHCMS_API_URL:
-    "https://api.graphcms.com/simple/v1/cjfdxg4vd5bmo0176upepcub5"
-};
