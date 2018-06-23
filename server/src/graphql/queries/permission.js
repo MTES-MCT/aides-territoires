07a18b71fd776014c01c7541cf9d2418d825aaa3
yann <yann.boisselier@gmail.com>
2018-06-23 22:16:11 +0200

    clean permission and roles graphql types

--- server/src/graphql/queries/permission.js
+++ server/src/graphql/queries/permission.js
@@ -6,26 +6,24 @@ const {
   getAllPermissions
 } = require("../../services/user");
 
-const AllPermissionsEdgesType = new GraphQLObjectType({
-  name: "allPermissions",
-  fields: {
-    edges: {
-      type: new GraphQLList(
-        new GraphQLObjectType({
-          name: "allPermissionsEdge",
-          fields: {
-            node: { type: types.Permission }
-          }
-        })
-      )
-    }
-  }
-});
-
 // a simple query to test our graphql AP
 module.exports = {
   allPermissions: {
-    type: AllPermissionsEdgesType,
+    type: new GraphQLObjectType({
+      name: "allPermissions",
+      fields: {
+        edges: {
+          type: new GraphQLList(
+            new GraphQLObjectType({
+              name: "allPermissionsEdge",
+              fields: {
+                node: { type: types.Permission }
+              }
+            })
+          )
+        }
+      }
+    }),
     resolve: (_, args, context) => {
       if (!userHasPermission(context.user, "see_permissions_overview")) {
         permissionDenied();
--- server/src/graphql/queries/role.js
+++ server/src/graphql/queries/role.js
@@ -6,26 +6,23 @@ const {
   getAllRoles
 } = require("../../services/user");
 
-const AllRolesEdgesType = new GraphQLObjectType({
-  name: "allRoles",
-  fields: {
-    edges: {
-      type: new GraphQLList(
-        new GraphQLObjectType({
-          name: "allRolesEdge",
-          fields: {
-            node: { type: types.Role }
-          }
-        })
-      )
-    }
-  }
-});
-
-// a simple query to test our graphql AP
 module.exports = {
   allRoles: {
-    type: AllRolesEdgesType,
+    type: new GraphQLObjectType({
+      name: "allRoles",
+      fields: {
+        edges: {
+          type: new GraphQLList(
+            new GraphQLObjectType({
+              name: "allRolesEdge",
+              fields: {
+                node: { type: types.Role }
+              }
+            })
+          )
+        }
+      }
+    }),
     resolve: (_, args, context) => {
       if (!userHasPermission(context.user, "see_permissions_overview")) {
         permissionDenied();
