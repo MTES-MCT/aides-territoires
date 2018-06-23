da112573c837789e3cbdc30a9cd99af9183a822f
yann <yann.boisselier@gmail.com>
2018-06-23 21:39:49 +0200

    remove deprecated functions

--- server/src/services/enums.js
+++ server/src/services/enums.js
@@ -3,8 +3,6 @@ const logger = require("../services/logger");
 const allEnumerations = require("../config/enums");
 
 module.exports = {
-  formatEnumForMongoose,
-  formatEnumForGraphQL,
   getEnumByIdForMongoose,
   getEnumByIdForGraphQL,
   getAllEnums
@@ -55,31 +53,4 @@ function getEnumByIdForGraphQL(graphQLName, enumId) {
     result.values[value.id] = { value: value.id };
   });
   return new GraphQLEnumType(result);
-}
-
-function formatEnumForMongoose(enumType) {
-  return enumType.map(r => r.value);
-}
-
-function formatEnumForGraphQL(graphQLName, enumType, multiple = true) {
-  if (!enumType) {
-    logger.error(
-      "fonction formatEnumForGraphQL : L'énumération n'est pas définie pour " +
-        graphQLName
-    );
-  }
-  const graphQLType = {
-    name: graphQLName,
-    values: {}
-  };
-  enumType.map(data => {
-    graphQLType.values[data.value] = {
-      value: data.value,
-      departement: data.label
-    };
-  });
-  if (multiple) {
-    return new GraphQLList(new GraphQLEnumType(graphQLType));
-  }
-  return new GraphQLEnumType(graphQLType);
 }
