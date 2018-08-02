c91af8f3c75b925969a8fcf7afec10c12e2546dd
Thibault Jouannic <thibault@miximum.fr>
2018-08-02 15:10:55 +0200

    Generate locale files

--- src/core/settings/local.py
+++ src/core/settings/local.py
@@ -14,6 +14,7 @@ TEMPLATES = [
             'debug': DEBUG,
             'context_processors': [
                 'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
