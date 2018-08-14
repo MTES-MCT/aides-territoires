9d357a622e055f4b77ca91a5e4849071320ae933
Thibault Jouannic <thibault@miximum.fr>
2018-08-14 12:15:10 +0200

    Fix a tiny import typo

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
                                         BaseUserManager)
-from djanog.utils import timezone
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
