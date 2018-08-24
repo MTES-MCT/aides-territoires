42c06ba2c5051604272279d7803311c3e87336df
Thibault Jouannic <thibault@miximum.fr>
2018-08-24 16:25:52 +0200

    Install and configure Raven

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -1,7 +1,12 @@
 import environ
 
 from .base import *  # noqa
+from .base import INSTALLED_APPS
 
+
+INSTALLED_APPS += [
+    'raven.contrib.django.raven_compat',
+]
 
 # Create a .env.production file in django's root
 environ.Env.read_env('.env.production')
@@ -16,3 +21,7 @@ DATABASES = {
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 INTERNAL_IPS = env.list('INTERNAL_IPS')
+
+RAVEN_CONFIG = {
+    'dsn': env.url('RAVEN_URL'),
+}
