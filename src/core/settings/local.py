c51906df03be2c728e7b66315ef59340013248f9
Thibault Jouannic <thibault@miximum.fr>
2018-08-28 10:48:44 +0200

    Make `COMPRESS_OFFLINE` locally overridable

--- src/core/settings/local.py
+++ src/core/settings/local.py
@@ -32,3 +32,5 @@ DATABASES = {
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['aidesterritoires.local'])
 
 INTERNAL_IPS = env.list('INTERNAL_IPS', default=['127.0.0.1'])
+
+COMPRESS_OFFLINE = env.bool('COMPRESS_OFFLINE', False)
