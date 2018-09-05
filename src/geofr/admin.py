ce2eeba858e7f01ad94b63b4d061f19449d4632d
Thibault Jouannic <thibault@miximum.fr>
2018-09-05 11:58:46 +0200

    Add sorting and filtering to perimeter admin

--- src/geofr/admin.py
+++ src/geofr/admin.py
@@ -6,7 +6,11 @@ from geofr.models import Perimeter
 class PerimeterAdmin(admin.ModelAdmin):
     """Admin module for perimeters."""
 
-    list_display = ('scale', 'name', 'code')
+    list_display = ('scale', 'name', 'code', 'region', 'department', 'epci',
+                    'zipcodes')
+    search_fields = ['name', 'code']
+    list_filter = ('scale',)
+    ordering = ('-scale', 'name')
 
 
 admin.site.register(Perimeter, PerimeterAdmin)
