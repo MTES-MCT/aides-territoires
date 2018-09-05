cae79599963da0f9f636ffa060cc62ce0451f662
Thibault Jouannic <thibault@miximum.fr>
2018-09-05 11:59:19 +0200

    Import all perimeters using the geo api

---
+++ src/geofr/management/commands/import_countries.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from geofr.models import Perimeter
+
+
+class Command(BaseCommand):
+    """Import the list of all regions."""
+
+    def handle(self, *args, **options):
+
+        Perimeter.objects.create(
+            scale=Perimeter.TYPES.country,
+            code='FRA',
+            name='France')
+        Perimeter.objects.create(
+            scale=Perimeter.TYPES.continent,
+            code='EU',
+            name='Europe')
+
