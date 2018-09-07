f231246c61a546c034cd6186910ae86c0b6659e2
Thibault Jouannic <thibault@miximum.fr>
2018-09-07 12:27:15 +0200

    Update api to match select2 exepcted results

--- src/geofr/api/serializers.py
+++ src/geofr/api/serializers.py
@@ -6,8 +6,8 @@ from geofr.models import Perimeter
 class PerimeterSerializer(serializers.ModelSerializer):
 
     scale = serializers.CharField(source='get_scale_display')
-    _str = serializers.CharField(source='__str__')
+    text = serializers.CharField(source='__str__')
 
     class Meta:
         model = Perimeter
-        fields = ('name', 'scale', '_str')
+        fields = ('id', 'name', 'scale', 'text')
