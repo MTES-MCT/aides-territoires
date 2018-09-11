13e4cf2f913e5c999b26bdd9c9416a47b05f9478
Thibault Jouannic <thibault@miximum.fr>
2018-09-11 10:06:21 +0200

    Add missing parameters in admin functions

--- src/geofr/admin.py
+++ src/geofr/admin.py
@@ -15,10 +15,10 @@ class PerimeterAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_change_permission(self, request):
+    def has_change_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request):
+    def has_delete_permission(self, request, obj=None):
         return False
 
 
