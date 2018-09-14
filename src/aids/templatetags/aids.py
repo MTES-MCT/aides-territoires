e371f22b386f845ede04ff94c53d453ce18afab8
Thibault Jouannic <thibault@miximum.fr>
2018-09-14 14:41:52 +0200

    Fix issue with aid type display

--- src/aids/templatetags/aids.py
+++ src/aids/templatetags/aids.py
@@ -16,3 +16,18 @@ def choices_display(obj, field):
     keys = getattr(obj, field)
     values = [force_text(choices_dict[key]) for key in keys]
     return ', '.join(values)
+
+
+@register.simple_tag
+def grouped_choices_display(obj, field):
+    """Correct rendering of `ChoiceArrayField` values.."""
+
+    choices = obj._meta.get_field(field).base_field.choices
+    flat_values = []
+    for group, values in choices:
+        flat_values += values
+    choices_dict = dict(flat_values)
+
+    keys = set(getattr(obj, field))
+    values = [force_text(choices_dict[key]) for key in keys]
+    return ', '.join(values)
