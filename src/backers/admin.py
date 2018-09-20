606dc81d14abb14c632078d7de53c55c419948c6
Thibault Jouannic <thibault@miximum.fr>
2018-09-20 15:21:13 +0200

    Add tools in admin to find aid by backers

--- src/backers/admin.py
+++ src/backers/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from backers.models import Backer
+
+
+class AidInline(admin.TabularInline):
+    model = Backer.aids.through
+    extra = 0
 
 
 class BackerAdmin(admin.ModelAdmin):
     """Admin module for aid backers."""
 
-    list_display = ['name']
+    list_display = ['name', 'nb_aids']
     search_fields = ['name']
+    inlines = [AidInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(nb_aids=Count('aids'))
+        return qs
+
+    def nb_aids(self, obj):
+        return obj.nb_aids
 
 
 admin.site.register(Backer, BackerAdmin)
