a1b33ee862c1896fccdc355108fb63dd923c1625
Thibault Jouannic <thibault@miximum.fr>
2018-09-25 10:55:24 +0200

    Create custom field to select perimeters

--- src/geofr/forms/fields.py
+++ src/geofr/forms/fields.py
@@ -1,6 +1,8 @@
 from django import forms
 
+from core.forms.widgets import AutocompleteSelect
 from geofr.constants import REGIONS, DEPARTMENTS
+from geofr.models import Perimeter
 
 
 class RegionField(forms.ChoiceField):
@@ -19,3 +21,25 @@ class DepartmentField(forms.ChoiceField):
 
         kwargs['choices'] = DEPARTMENTS
         super().__init__(**kwargs)
+
+
+class PerimeterChoiceField(forms.ModelChoiceField):
+    """Perimeter selection form field.
+
+    Custom field to work with an autocomplete widget.
+    """
+    def __init__(self, *args, widget=None, **kwargs):  # noqa
+        queryset = Perimeter.objects.all()
+        if widget is None:
+            widget = AutocompleteSelect
+
+        return super().__init__(queryset, *args, widget=widget, **kwargs)
+
+    def to_python(self, value):
+        perimeter_id = value.split('-')[0]
+        return super().to_python(perimeter_id)
+
+    def prepare_value(self, value):
+        if isinstance(value, str):
+            value = value.split('-')[0]
+        return value
