e6bb0b1930755f52a0646d0dcd807cb5a16ab9dd
Thibault Jouannic <thibault@miximum.fr>
2018-09-26 09:52:54 +0200

    Translate add creation url

--- src/aids/urls.py
+++ src/aids/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.utils.translation import ugettext_lazy as _
 
 from aids.views import SearchView, AidCreateView
 
 urlpatterns = [
     path('', SearchView.as_view(), name='search_view'),
-    path('create', AidCreateView.as_view(), name='aid_create_view'),
+    path(_('add/'), AidCreateView.as_view(), name='aid_create_view'),
 ]
