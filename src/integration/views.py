4ab67c3fcba40b3d6f8ffedf1fd7508821e63c8d
Thibault Jouannic <thibault@miximum.fr>
2018-09-27 15:20:41 +0200

    Add example with Somme site

--- src/integration/views.py
+++ src/integration/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class GuyaneGouvView(TemplateView):
     template_name = 'integration/guyane.html'
+
+
+class SommeView(TemplateView):
+    template_name = 'integration/somme.html'
