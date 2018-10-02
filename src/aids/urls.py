52402f3d40d2813dfcc4307dc554ff7a67b84198
Thibault Jouannic <thibault@miximum.fr>
2018-10-02 14:08:58 +0200

    Link to aid detail view in results

--- src/aids/urls.py
+++ src/aids/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.utils.translation import ugettext_lazy as _
 
-from aids.views import SearchView, AidCreateView
+from aids.views import SearchView, AidDetailView, AidCreateView
 
 urlpatterns = [
     path('', SearchView.as_view(), name='search_view'),
     path(_('add/'), AidCreateView.as_view(), name='aid_create_view'),
+    path('<slug:slug>/', AidDetailView.as_view(), name='aid_detail_view'),
 ]
