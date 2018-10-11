7f4854de79e521637e015d70652653d5a2a6b530
Thibault Jouannic <thibault@miximum.fr>
2018-10-11 16:30:30 +0200

    Create bookmark button

--- src/static/js/aids/bookmark_results.js
+++ src/static/js/aids/bookmark_results.js
@@ -1,19 +1,61 @@
 /**
  * Define features for the bookmark modal.
  */
-(function (exports) {
+(function (exports, catalog) {
+
+    exports.fillCurrentUrlField = function (event) {
+        var window = exports;
+        var currentUrl = window.document.location;
+        var input = event.data.clipboardInput;
+
+        input.attr('value', currentUrl);
+    };
 
     exports.copyUrlToClipboard = function (event) {
         event.preventDefault();
 
         var window = exports;
-        var url = window.document.location;
+        var navigator = window.navigator;
+        var document = window.document;
+        var $input = event.data.clipboardInput;
+        var $copyBtn = $(event.currentTarget);
+        var currentUrl = $input.attr('value');
 
+        $input.focus();
+        $input.select();
+
+        var onCopyToClipboardSuccess = function () {
+            $copyBtn.tooltip({
+                placement: 'bottom',
+                title: catalog.copy_to_clipboard_success,
+            }).tooltip('show');
+        };
+
+        var onCopyToClipboardError = function () {
+            $copyBtn.tooltip({
+                placement: 'bottom',
+                title: catalog.copy_to_clipboard_error,
+            }).tooltip('show');
+        };
+
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(currentUrl).then(onCopyToClipboardSuccess, onCopyToClipboardError);
+        } else {
+            if (document.execCommand('copy')) {
+                onCopyToClipboardSuccess();
+            } else {
+                onCopyToClipboardError();
+            }
+        }
     };
 
-})(this);
+})(this, catalog);
 
 $(document).ready(function () {
     var clipboardBtn = $('button#clipboard-btn');
-    clipboardBtn.click(copyUrlToClipboard);
+    var clipboardModal = $('div#bookmark-modal');
+    var clipboardInput = clipboardModal.find('input[name=current-url]');
+
+    clipboardModal.on('show.bs.modal', { clipboardInput: clipboardInput }, fillCurrentUrlField);
+    clipboardBtn.on('click', { clipboardInput: clipboardInput }, copyUrlToClipboard);
 });
