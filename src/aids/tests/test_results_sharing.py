2a7dbe5946961d7673fd6a53dd3d0662fb496c70
Thibault Jouannic <thibault@miximum.fr>
2018-10-12 11:41:34 +0200

    Display button to send results by email

---
+++ src/aids/tests/test_results_sharing.py
@@ -0,0 +1,33 @@
+"""Test methods for search results sharing."""
+
+import pytest
+from django.urls import reverse
+
+from aids.factories import AidFactory
+
+pytestmark = pytest.mark.django_db
+
+
+def test_share_button_is_hidden_for_anonymous_users(client):
+
+    AidFactory.create_batch(3)
+    url = reverse('search_view')
+    res = client.get(url)
+
+    assert res.status_code == 200
+    content = res.content.decode('utf-8')
+    assert 'envoyer ces résultats par e-mail' not in content
+
+
+def test_share_button_is_displayed_for_logged_users(client, user):
+
+    AidFactory.create_batch(3)
+    client.force_login(user)
+    assert user.is_authenticated
+
+    url = reverse('search_view')
+    res = client.get(url)
+
+    assert res.status_code == 200
+    content = res.content.decode('utf-8')
+    assert 'envoyer ces résultats par e-mail' in content
