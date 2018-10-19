8bac02b4867c48f1d2dce4b5c4c3042c5e5708a3
Thibault Jouannic <thibault@miximum.fr>
2018-10-19 15:42:59 +0200

    Test aid forms

--- src/aids/tests/test_forms.py
+++ src/aids/tests/test_forms.py
@@ -28,26 +28,6 @@ def aid_admin_form_class():
 
 
 @pytest.fixture
-def aid_form_data(user, backer, perimeter):
-    """Returns valid data to create an Aid object."""
-
-    return {
-        'name': 'Test aid',
-        'author': user.id,
-        'backers': [backer.id],
-        'description': 'My aid description',
-        'eligibility': 'Aid eligibility info',
-        'perimeter': perimeter.id,
-        'mobilization_steps': ['preop'],
-        'targeted_audiances': ['department'],
-        'aid_types': ['grant', 'loan'],
-        'destinations': ['supply'],
-        'publication_status': 'open',
-        'status': 'published',
-    }
-
-
-@pytest.fixture
 def aids(user, backer):
     """Generates a few aids and return the corresponding queryset."""
 
