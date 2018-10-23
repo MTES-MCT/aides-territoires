4eab266daba2c5a7d19be92a7ebbfe78aaaa1ecd
Thibault Jouannic <thibault@miximum.fr>
2018-10-23 15:46:30 +0200

    Use domain vocabulary: published aids

--- src/aids/urls.py
+++ src/aids/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path(_('results/receive/'), ResultsReceiveView.as_view(),
          name='results_receive_view'),
     path(_('add/'), AidCreateView.as_view(), name='aid_create_view'),
-    path(_('drafts/'), AidDraftListView.as_view(),
+    path(_('published/'), AidDraftListView.as_view(),
          name='aid_draft_list_view'),
-    path(_('drafts/<slug:slug>'), AidEditView.as_view(),
+    path(_('published/<slug:slug>'), AidEditView.as_view(),
          name='aid_edit_view'),
     path('<slug:slug>/', AidDetailView.as_view(), name='aid_detail_view'),
 ]
