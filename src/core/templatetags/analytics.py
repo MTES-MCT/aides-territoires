cbf15dfc8570b4051c19068f73a31c21e41bb932
Thibault Jouannic <thibault@miximum.fr>
2018-11-06 09:35:41 +0100

    Fix analytics tag compression issue

--- src/core/templatetags/analytics.py
+++ src/core/templatetags/analytics.py
@@ -1,6 +1,5 @@
 from django import template
 from django.conf import settings
-from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -16,21 +15,3 @@ def analytics_enabled():
 def analytics_siteid():
 
     return settings.ANALYTICS_SITEID
-
-
-@register.simple_tag
-def analytics_setup():
-    """"Embed the js tracking code setup."""
-
-    site_id = settings.ANALYTICS_SITEID
-    tag = '''
-    <script type="text/javascript">
-      var _paq = _paq || [];
-      _paq.push(["setDomains", ["*.aides-territoires.beta.gouv.fr"]]);
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      _paq.push(['setTrackerUrl', '//stats.data.gouv.fr/piwik.php']);
-      _paq.push(['setSiteId', '{siteid}']);
-    </script>
-    '''.format(siteid=site_id)
-    return mark_safe(tag)
