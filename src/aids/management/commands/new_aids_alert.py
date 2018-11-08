608b858f871e883eb1935f74ca11c1e21b9a30b1
Thibault Jouannic <thibault@miximum.fr>
2018-11-08 10:35:38 +0100

    Update "new aid alerts" email content

--- src/aids/management/commands/new_aids_alert.py
+++ src/aids/management/commands/new_aids_alert.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.core.management.base import BaseCommand
 from django.template.loader import render_to_string
 from django.core.mail import send_mail
+from django.contrib.sites.models import Site
 from django.conf import settings
 
 from aids.models import Aid
@@ -19,14 +20,18 @@ class Command(BaseCommand):
             .order_by('author') \
             .select_related('author')
 
-        if new_aids.count() == 0:
+        nb_aids = new_aids.count()
+        if nb_aids == 0:
             self.stdout.write('We could not find any new aids.')
             return
 
+        site = Site.objects.get_current()
         email_body = render_to_string('emails/new_aids_alert_body.txt', {
-            'new_aids': new_aids
+            'new_aids': new_aids,
+            'domain': site.domain,
         })
-        email_subject = 'Nouvelles aides au {:%d/%m/%Y}'.format(yesterday)
+        email_subject = '{} nouvelles aides au {:%d/%m/%Y}'.format(
+            nb_aids, yesterday)
         email_from = settings.DEFAULT_FROM_EMAIL
         email_to = [settings.CONTACT_EMAIL]
 
