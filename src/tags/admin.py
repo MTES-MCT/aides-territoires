fd38dfc9b31b5645dd17f554038b81e1f2c83c52
Thibault Jouannic <thibault@miximum.fr>
2018-11-14 15:51:53 +0100

    Tag admin form saves tags as slugs

--- src/tags/admin.py
+++ src/tags/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.utils.text import slugify
 
 from tags.models import Tag
 
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ['name']
+
+    def save_model(self, request, obj, form, change):
+        """Make sure the tag name is a slug."""
+        obj.name = slugify(obj.name)
+        return super().save_model(request, obj, form, change)
 
 
 admin.site.register(Tag, TagAdmin)
