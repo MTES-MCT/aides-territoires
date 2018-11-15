2c807d3c521ce9f0cbea29b32c1b1d2ca70c48c8
Thibault Jouannic <thibault@miximum.fr>
2018-11-15 10:41:17 +0100

    Add index to tag search

--- src/tags/models.py
+++ src/tags/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from core.indexes import GinTrigramIndex
+
 
 class Tag(models.Model):
     name = models.CharField(
@@ -11,6 +13,9 @@ class Tag(models.Model):
     class Meta:
         verbose_name = _('Tag')
         verbose_name_plural = _('Tags')
+        indexes = [
+            GinTrigramIndex(fields=['name']),
+        ]
 
     def __str__(self):
         return self.name
