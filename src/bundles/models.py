04e61bdaa5c4464fa0fe5711694af401f235c835
Thibault Jouannic <thibault@miximum.fr>
2018-11-20 16:06:52 +0100

    Make bundle name unique per user

--- src/bundles/models.py
+++ src/bundles/models.py
@@ -33,6 +33,7 @@ class Bundle(models.Model):
     class Meta:
         verbose_name = _('Bundle')
         verbose_name_plural = _('Bundles')
+        unique_together = ['owner', 'name']
 
     def save(self, *args, **kwargs):
         """Populate the slug field.
