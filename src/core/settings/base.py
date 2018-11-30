4a1ffaeab01c21e7583e3ef6b16fb1e440242334
Thibault Jouannic <thibault@miximum.fr>
2018-11-30 15:05:17 +0100

    Disable x-frame-options middleware

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
