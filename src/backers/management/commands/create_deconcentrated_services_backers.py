03b5921315c403d952eaf637f2dc8f73fc3193c4
Thibault Jouannic <thibault@miximum.fr>
2019-03-07 14:43:02 +0100

    Create deconcentrated services backers task

---
+++ src/backers/management/commands/create_deconcentrated_services_backers.py
@@ -0,0 +1,47 @@
+# flake8: noqa
+from django.core.management.base import BaseCommand
+
+from backers.models import Backer
+from geofr.constants import OVERSEAS_REGIONS
+from geofr.models import Perimeter
+
+
+SERVICES = [
+    'Ademe — Direction régionale',
+    'Banque des Territoires — Direction régionale',
+    'Préfecture de région',
+    'Direction régionale de l\'Alimentation, de l\'Agriculture et de la Forêt (DRAAF)',
+    'Direction régionale des Affaires culturelles (DRAC)',
+    "Direction régionale des Entreprises, de la Concurrence, de la Consommation, du Travail et de l’Emploi (DIRECCTE)",
+    "Direction régionale de l’environnement, de l’aménagement et du logement (DREAL)",
+    "Direction régionale des Finances publiques (DRFiP)",
+    "Région académique",
+    "Direction régionale de la Jeunesse, des Sports et de la Cohésion sociale (DRJSCS)",
+    "Délégation régionale à la recherche et à la technologie (DRRT)",
+    "Agence régionale de santé (ARS)",
+]
+
+
+class Command(BaseCommand):
+    """Create backers entries for regional deconcentrated services."""
+
+    def handle(self, *args, **options):
+        confirm = input('This command is meant to run in production only '
+                        'once.\nAre you sure you know what you\'re doing?\n')
+        if confirm != 'yes':
+            return
+
+        regions = Perimeter.objects \
+            .filter(scale=Perimeter.TYPES.region) \
+            .exclude(code__in=OVERSEAS_REGIONS)
+
+        backers = []
+        for region in regions:
+            for service in SERVICES:
+                backers.append(Backer(
+                    name='{} — {}'.format(service, region.name)))
+
+        results = Backer.objects.bulk_create(backers)
+        self.stdout.write(self.style.SUCCESS(
+            '{} backers created'.format(len(results))
+        ))
