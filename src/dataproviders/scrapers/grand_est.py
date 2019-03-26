ef01c4422e60b13ec45a1add68e909324b6a4288
Thibault Jouannic <thibault@miximum.fr>
2019-03-26 10:06:27 +0100

    Only import GrandEst aids targeted at epcis

--- src/dataproviders/scrapers/grand_est.py
+++ src/dataproviders/scrapers/grand_est.py
@@ -2,12 +2,19 @@ import scrapy
 from dataproviders.utils import content_prettify
 
 
+# This is a reference specific to the grandest.fr site.
+# When you click on the filter form "filtrer par bénéficiaires" field and
+# choose "collectivités", you get a new parameter ?beneficiaire=63 in the url.
+EPCI_AUDIANCE_CODE = '63'
+
+
 class GrandEstSpider(scrapy.Spider):
     name = 'grand_est'
 
     BASE_URL = 'https://www.grandest.fr/'
     start_urls = [
-        'https://www.grandest.fr/aides/',
+        'https://www.grandest.fr/aides/?beneficiaire={}'.format(
+            EPCI_AUDIANCE_CODE),
     ]
 
     def parse(self, response):
@@ -21,8 +28,9 @@ class GrandEstSpider(scrapy.Spider):
         next = response.css('a[rel=next]::attr("href")').get()
         if next:
             page_number = next.split('/')[-2]
-            real_next_url = '{base_url}page/{page}/?pg={page}'.format(
+            real_next_url = '{base_url}page/{page}/?beneficiaire={beneficiaire}&pg={page}'.format(  # noqa
                 base_url=self.start_urls[0],
+                beneficiaire=EPCI_AUDIANCE_CODE,
                 page=page_number)
             yield scrapy.Request(real_next_url, callback=self.parse)
 
