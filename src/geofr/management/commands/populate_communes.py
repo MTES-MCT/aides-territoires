98f3a635fc335c63c2a753e80b3ef022b06c2083
Thibault Jouannic <thibault@miximum.fr>
2019-03-27 10:02:51 +0100

    Rename commune population script

--- src/geofr/management/commands/populate_communes.py
+++ src/geofr/management/commands/populate_communes.py
@@ -1,4 +1,5 @@
 import requests
+from operator import attrgetter
 from django.core.management.base import BaseCommand, CommandError
 
 from geofr.models import Perimeter
@@ -8,15 +9,28 @@ from geofr.constants import OVERSEAS_REGIONS
 API_URL = 'https://geo.api.gouv.fr/departements/{}/communes/'
 
 
+FORMAT_STR = '{c.name};{c.code};{c.departments};{c.zipcodes}\n'
+
+
 class Command(BaseCommand):
     """Import the list of all communes."""
 
     def handle(self, *args, **options):
 
+        # Let's serialize and dump existing communes in a file
+        communes = Perimeter.objects \
+            .filter(scale=Perimeter.TYPES.commune) \
+            .order_by('name')
+        list_communes = list(communes)
+        list_communes.sort(key=attrgetter('name', 'code'))
+        with open('/tmp/existing_communes.dump', 'w') as f:
+            for commune in list_communes:
+                f.write(FORMAT_STR.format(c=commune))
+
+        # Now, let's fetch updated data, and dump it with the same format
         departments = Perimeter.objects.filter(
             scale=Perimeter.TYPES.department)
-
-        communes = []
+        new_communes = []
         for department in departments:
             self.stdout.write(
                 'Importing communes for {}.'.format(department.name))
@@ -35,8 +49,12 @@ class Command(BaseCommand):
                     regions=[entry['codeRegion']],
                     zipcodes=entry['codesPostaux'],
                     is_overseas=(entry['codeRegion'] in OVERSEAS_REGIONS))
-                communes.append(commune)
+                new_communes.append(commune)
 
-        results = Perimeter.objects.bulk_create(communes)
-        self.stdout.write(self.style.SUCCESS(
-            '%d communes imported.' % len(results)))
+        new_communes.sort(key=attrgetter('name', 'code'))
+        with open('/tmp/new_communes.dump', 'w') as f:
+            for commune in new_communes:
+                f.write(FORMAT_STR.format(c=commune))
+
+        print('Existing communes: {}'.format(len(list_communes)))
+        print('New communes: {}'.format(len(new_communes)))
