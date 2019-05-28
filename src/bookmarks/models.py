65d878457dc6e2958f6d266cc97d7360b1fbf3bb
Thibault Jouannic <thibault@miximum.fr>
2019-05-28 15:34:19 +0200

    Create Bookmark model

---
+++ src/bookmarks/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
+
+class Bookmark(models.Model):
+    """A bookmarked search query."""
+
+    owner = models.ForeignKey(
+        'accounts.User',
+        verbose_name=_('Owner'),
+        on_delete=models.CASCADE)
+    querystring = models.TextField(
+        _('Querystring'))
+    date_created = models.DateTimeField(
+        _('Date created'),
+        default=timezone.now)
+    date_updated = models.DateTimeField(
+        _('Date updated'),
+        auto_now=True)
+
+    class Meta:
+        verbose_name = _('Bookmark')
+        verbose_name_plural = _('Bookmarks')
