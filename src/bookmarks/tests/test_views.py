276be2ca7e8510aaf4f705e06c4645735d340214
Thibault Jouannic <thibault@miximum.fr>
2019-05-28 15:42:11 +0200

    Write test for bookmark creation view

--- src/bookmarks/tests/test_views.py
+++ src/bookmarks/tests/test_views.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from bookmarks.models import Bookmark
+
 
 pytestmark = pytest.mark.django_db
 
@@ -15,3 +17,21 @@ def test_bookmark_create_is_for_logged_in_users_only(client):
     url = reverse('bookmark_create_view')
     res = client.post(url, data={}, follow=True)
     assert res.redirect_chain[0][0].startswith('/comptes/connexion/')
+
+
+def test_bookmark_create_view(user, client):
+    bookmarks = Bookmark.objects.all()
+    assert bookmarks.count() == 0
+
+    url = reverse('bookmark_create_view')
+    client.force_login(user)
+    res = client.post(url, data={
+        'text': 'Ademe',
+        'call_for_projects_only': 'on',
+    })
+    assert res.status_code == 302
+    assert bookmarks.count() == 1
+
+    bookmark = bookmarks[0]
+    assert bookmark.owner == user
+    assert bookmark.querystring == 'text=Ademe&call_for_projects_only=on'
