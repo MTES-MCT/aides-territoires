595cc551bcef4916bfade5de0f057d6c6c715b1e
Thibault Jouannic <thibault@miximum.fr>
2019-05-28 15:49:06 +0200

    List bookmarks in admin

--- src/bookmarks/admin.py
+++ src/bookmarks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from bookmarks.models import Bookmark
+
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'date_created', 'date_updated']
+
+
+admin.site.register(Bookmark, BookmarkAdmin)
