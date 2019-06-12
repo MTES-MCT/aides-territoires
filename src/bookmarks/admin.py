70f74e8c95765ed3f1c2cdc5a45b079dcab7586f
Thibault Jouannic <thibault@miximum.fr>
2019-06-12 15:02:57 +0200

    Display alert settings in bm admin

--- src/bookmarks/admin.py
+++ src/bookmarks/admin.py
@@ -4,7 +4,9 @@ from bookmarks.models import Bookmark
 
 
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ['owner', 'date_created', 'date_updated']
+    list_display = [
+        'owner', 'title', 'send_email_alert', 'date_created', 'date_updated'
+    ]
 
 
 admin.site.register(Bookmark, BookmarkAdmin)
