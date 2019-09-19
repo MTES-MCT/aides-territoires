076d33d8212ad399ff7b60b6712a1fc322550c58
Thibault Jouannic <thibault@miximum.fr>
2019-09-19 15:30:39 +0200

    Update search engine tests

--- src/aids/tests/test_search_engine.py
+++ src/aids/tests/test_search_engine.py
@@ -15,100 +15,133 @@ pytestmark = pytest.mark.django_db
 
 @pytest.fixture
 def perimeters():
+
+    europe = PerimeterFactory(
+        scale=Perimeter.TYPES.continent,
+        name='Europe',
+        code='EU')
+    france = PerimeterFactory(
+        scale=Perimeter.TYPES.country,
+        contained_in=[europe],
+        name='France',
+        code='FRA')
+    metropole = PerimeterFactory(
+        scale=Perimeter.TYPES.mainland,
+        contained_in=[europe, france],
+        name='Métropole',
+        code='FRA-MET')
+    outre_mer = PerimeterFactory(
+        scale=Perimeter.TYPES.overseas,
+        contained_in=[europe, france],
+        name='Outre-mer',
+        code='FRA-OM')
+    rhonemed = PerimeterFactory(
+        scale=Perimeter.TYPES.basin,
+        contained_in=[europe, france],
+        is_overseas=False,
+        name='Rhône-Méditerannée',
+        country='FRA',
+        code='FR000006')
+    adour_garonne = PerimeterFactory(
+        scale=Perimeter.TYPES.basin,
+        contained_in=[europe, france],
+        is_overseas=False,
+        name='Adour-Garonne',
+        code='FR000005')
+    occitanie = PerimeterFactory(
+        scale=Perimeter.TYPES.region,
+        contained_in=[europe, france],
+        is_overseas=False,
+        name='Occitanie',
+        code='76')
+    herault = PerimeterFactory(
+        scale=Perimeter.TYPES.department,
+        contained_in=[europe, france, occitanie],
+        is_overseas=False,
+        name='Hérault',
+        code='34',
+        regions=['76'])
+    montpellier = PerimeterFactory(
+        scale=Perimeter.TYPES.commune,
+        contained_in=[europe, france, occitanie, herault, rhonemed],
+        is_overseas=False,
+        name='Montpellier',
+        code='34172',
+        regions=['76'],
+        departments=['34'],
+        basin='FR000006')
+    vic = PerimeterFactory(
+        scale=Perimeter.TYPES.commune,
+        contained_in=[europe, france, occitanie, herault, rhonemed],
+        is_overseas=False,
+        name='Vic-la-Gardiole',
+        code='34333',
+        regions=['76'],
+        departments=['34'],
+        basin='FR000006')
+    aveyron = PerimeterFactory(
+        scale=Perimeter.TYPES.department,
+        contained_in=[europe, france, occitanie],
+        is_overseas=False,
+        name='Aveyron',
+        code='12',
+        regions=['76'])
+    rodez = PerimeterFactory(
+        scale=Perimeter.TYPES.commune,
+        contained_in=[europe, france, occitanie, aveyron, adour_garonne],
+        is_overseas=False,
+        name='Rodez',
+        code='12202',
+        regions=['76'],
+        departments=['12'],
+        basin='FR000005')
+    normandie = PerimeterFactory(
+        scale=Perimeter.TYPES.region,
+        contained_in=[europe, france],
+        is_overseas=False,
+        name='Normandie',
+        code='28')
+    eure = PerimeterFactory(
+        scale=Perimeter.TYPES.department,
+        contained_in=[europe, france, normandie],
+        is_overseas=False,
+        name='Eure',
+        code='28',
+        regions=['28'])
+    st_cyr = PerimeterFactory(
+        scale=Perimeter.TYPES.commune,
+        contained_in=[europe, france, normandie, eure],
+        is_overseas=False,
+        name='Saint-Cyr-la-Campagne',
+        code='27529',
+        regions=['28'],
+        departments=['27'])
+    fort_de_france = PerimeterFactory(
+        scale=Perimeter.TYPES.commune,
+        contained_in=[europe, france],
+        is_overseas=True,
+        name='Fort-de-France',
+        code='97209',
+        regions=['02'],
+        departments=['972'])
+
     perimeters = {
-        'europe': PerimeterFactory(
-            scale=Perimeter.TYPES.continent,
-            name='Europe',
-            code='EU'),
-        'france': PerimeterFactory(
-            scale=Perimeter.TYPES.country,
-            name='France',
-            code='FRA'),
-        'métropole': PerimeterFactory(
-            scale=Perimeter.TYPES.mainland,
-            name='Métropole',
-            code='FRA-MET'),
-        'outre-mer': PerimeterFactory(
-            scale=Perimeter.TYPES.overseas,
-            name='Outre-mer',
-            code='FRA-OM'),
-        'occitanie': PerimeterFactory(
-            scale=Perimeter.TYPES.region,
-            is_overseas=False,
-            name='Occitanie',
-            code='76'),
-        'herault': PerimeterFactory(
-            scale=Perimeter.TYPES.department,
-            is_overseas=False,
-            name='Hérault',
-            code='34',
-            regions=['76']),
-        'montpellier': PerimeterFactory(
-            scale=Perimeter.TYPES.commune,
-            is_overseas=False,
-            name='Montpellier',
-            code='34172',
-            regions=['76'],
-            departments=['34'],
-            basin='FR000006'),
-        'vic': PerimeterFactory(
-            scale=Perimeter.TYPES.commune,
-            is_overseas=False,
-            name='Vic-la-Gardiole',
-            code='34333',
-            regions=['76'],
-            departments=['34'],
-            basin='FR000006'),
-        'aveyron': PerimeterFactory(
-            scale=Perimeter.TYPES.department,
-            is_overseas=False,
-            name='Aveyron',
-            code='12',
-            regions=['76']),
-        'rodez': PerimeterFactory(
-            scale=Perimeter.TYPES.commune,
-            is_overseas=False,
-            name='Rodez',
-            code='12202',
-            regions=['76'],
-            departments=['12'],
-            basin='FR000005'),
-        'normandie': PerimeterFactory(
-            scale=Perimeter.TYPES.region,
-            is_overseas=False,
-            name='Normandie',
-            code='28'),
-        'eure': PerimeterFactory(
-            scale=Perimeter.TYPES.department,
-            is_overseas=False,
-            name='Eure',
-            code='28',
-            regions=['28']),
-        'st-cyr': PerimeterFactory(
-            scale=Perimeter.TYPES.commune,
-            is_overseas=False,
-            name='Saint-Cyr-la-Campagne',
-            code='27529',
-            regions=['28'],
-            departments=['27']),
-        'rhone-mediterannee': PerimeterFactory(
-            scale=Perimeter.TYPES.basin,
-            is_overseas=False,
-            name='Rhône-Méditerannée',
-            country='FRA',
-            code='FR000006'),
-        'adour-garonne': PerimeterFactory(
-            scale=Perimeter.TYPES.basin,
-            is_overseas=False,
-            name='Adour-Garonne',
-            code='FR000005'),
-        'fort-de-france': PerimeterFactory(
-            scale=Perimeter.TYPES.commune,
-            is_overseas=True,
-            name='Fort-de-France',
-            code='97209',
-            regions=['02'],
-            departments=['972']),
+        'europe': europe,
+        'france': france,
+        'métropole': metropole,
+        'outre-mer': outre_mer,
+        'occitanie': occitanie,
+        'herault': herault,
+        'montpellier': montpellier,
+        'vic': vic,
+        'aveyron': aveyron,
+        'rodez': rodez,
+        'normandie': normandie,
+        'eure': eure,
+        'st-cyr': st_cyr,
+        'rhone-mediterannee': rhonemed,
+        'adour-garonne': adour_garonne,
+        'fort-de-france': fort_de_france,
     }
     return perimeters
 
