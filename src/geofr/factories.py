3a54940f1a678edd1687645220de41d168f366ef
Thibault Jouannic <thibault@miximum.fr>
2019-09-19 15:30:25 +0200

    Handle links between perimeters in factory

--- src/geofr/factories.py
+++ src/geofr/factories.py
@@ -14,3 +14,14 @@ class PerimeterFactory(DjangoModelFactory):
     scale = Perimeter.TYPES.region
     code = factory.Sequence(lambda n: '%08d' % n)
     name = factory.Faker('company')
+
+    @factory.post_generation
+    def contained_in(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for perimeter in extracted:
+                self.contained_in.add(perimeter)
