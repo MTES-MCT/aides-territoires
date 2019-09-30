80353b5ecceeac5b76b0293546755a9eac2317de
Thibault Jouannic <thibault@miximum.fr>
2019-09-30 09:44:15 +0200

    Create views to display program details

---
+++ src/programs/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from programs.views import ProgramList, ProgramDetail
+
+
+urlpatterns = [
+    path('', ProgramList.as_view(), name='program_list'),
+    path('<slug:slug>/', ProgramDetail.as_view(), name='program_detail'),
+]
--- src/programs/views.py
+++ src/programs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from programs.models import Program
+
+
+class ProgramList(ListView):
+    template_name = 'programs/list.html'
+    context_object_name = 'programs'
+
+    def get_queryset(self):
+        qs = Program.objects.all()
+        return qs
+
+
+class ProgramDetail(DetailView):
+    template_name = 'programs/detail.html'
+    context_object_name = 'program'
+
+    def get_queryset(self):
+        qs = Program.objects.all()
+        return qs
