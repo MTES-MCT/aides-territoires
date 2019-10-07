084e7dd1c7e9c9b023591d0f935e364bf6d857b1
Thibault Jouannic <thibault@miximum.fr>
2019-10-07 10:16:55 +0200

    Fix pep8 error

--- src/aids/views.py
+++ src/aids/views.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
 from django.db.models import Q
