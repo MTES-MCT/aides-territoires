a07bdf46fb1fa0b3c1bc74300180ccdae7517b70
Thibault Jouannic <thibault@miximum.fr>
2019-10-25 09:45:58 +0200

    Bookmark form for anonymous users

--- src/bookmarks/views.py
+++ src/bookmarks/views.py
@@ -1,13 +1,17 @@
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
-from django.utils.translation import ugettext_lazy as _, ugettext
+from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse, reverse_lazy
+from django.db import transaction
 from django.http import HttpResponse, HttpResponseRedirect
 from braces.views import MessageMixin
 
-from bookmarks.forms import BookmarkAlertForm, UserBookmarkForm
+from accounts.forms import RegisterForm
+from accounts.tasks import send_connection_email
+from bookmarks.forms import (BookmarkAlertForm, UserBookmarkForm,
+                             AnonymousBookmarkForm)
 from bookmarks.models import Bookmark
 
 
@@ -26,21 +30,58 @@ class BookmarkList(LoginRequiredMixin, BookmarkMixin, ListView):
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class BookmarkCreate(LoginRequiredMixin, MessageMixin, BookmarkMixin,
-                     CreateView):
+class BookmarkCreate(MessageMixin, BookmarkMixin, CreateView):
     """Create a bookmark by saving a search view querystring.
 
     Note: the search form, by default, uses the GET method. Hence, we
     don't pass the form a csrf token and that's why we had to exempt this
     view from csrf protection.
+
+    This view has to handle two cases:
+
+     1. the user creating the bookmark is already connected.
+     2. the user is just an anonymous visitor and it's email is not validated.
+
     """
 
     http_method_names = ['post']
 
     def get_form(self):
-        return UserBookmarkForm(self.request.POST)
+        if self.request.user.is_authenticated:
+            BookmarkForm = UserBookmarkForm
+        else:
+            BookmarkForm = AnonymousBookmarkForm
 
+        return BookmarkForm(self.request.POST)
+
+    @transaction.atomic
     def form_valid(self, form):
+
+        if self.request.user.is_authenticated:
+            owner = self.request.user
+            send_alert = form.cleaned_data['send_email_alert']
+            bookmark = self.create_bookmark(form, owner, send_alert)
+            bookmarks_url = reverse('bookmark_list_view')
+            message = _('Your new bookmark was successfully created. '
+                        '<a href="%(url)s">You will find in in your bookmark '
+                        'list.</a>') % {'url': bookmarks_url}
+
+        else:
+            owner = self.create_account(form)
+            send_alert = True
+            bookmark = self.create_bookmark(form, owner, send_alert)
+            send_connection_email.delay(
+                owner.email,
+                body_template='emails/bookmark_login.txt')
+            message = _('We just sent you an email to validate your address.')
+
+        self.messages.success(message)
+        redirect_url = reverse('search_view')
+        return HttpResponseRedirect('{}?{}'.format(
+            redirect_url, bookmark.querystring))
+
+    def create_bookmark(self, form, owner, send_alert):
+        """Create a new bookmark."""
 
         # Extract search parameters and exclude the bookmark
         # configuration fields.
@@ -50,22 +91,33 @@ class BookmarkCreate(LoginRequiredMixin, MessageMixin, BookmarkMixin,
         querystring = post_data.urlencode()
 
         # Create the bookmark object
-        Bookmark.objects.create(
-            owner=self.request.user,
+        bookmark = Bookmark.objects.create(
+            owner=owner,
             title=form.cleaned_data['title'],
-            send_email_alert=form.cleaned_data['send_email_alert'],
+            send_email_alert=send_alert,
             querystring=querystring)
 
-        bookmarks_url = reverse('bookmark_list_view')
-        message = _('Your new bookmark was successfully created. '
-                    '<a href="%(url)s">You will find in in your bookmark '
-                    'list.</a>') % {'url': bookmarks_url}
-        self.messages.success(message)
-        redirect_url = reverse('search_view')
-        return HttpResponseRedirect('{}?{}'.format(redirect_url, querystring))
+        return bookmark
+
+    def create_account(self, form):
+        """Create a tmp account to attach the bookmark to."""
+
+        register_form = RegisterForm({
+            'email': form.cleaned_data['email'],
+            'full_name': form.cleaned_data['email'],
+            'ml_consent': False})
+        user = register_form.save()
+        return user
 
     def form_invalid(self, form):
-        self.messages.error(_('Something went wrong. Please try again.'))
+        if form.has_error('email', 'unique'):
+            msg = _('An account with this address already exists. If this is '
+                    'your account, you might want to login first.')
+        else:
+            msg = _('We could not create your bookmark because of those '
+                    'errors: {}').format(form.errors.as_text())
+
+        self.messages.error(msg)
         redirect_url = reverse('search_view')
         return HttpResponseRedirect(redirect_url)
 
