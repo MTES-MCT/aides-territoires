96dff15dd8ea313c1550c1c48908e217cba2fb15
Thibault Jouannic <thibault@miximum.fr>
2019-10-28 11:16:10 +0100

    Prefill the "alert title" field

--- src/static/js/bookmarks/bookmark_search_form.js
+++ src/static/js/bookmarks/bookmark_search_form.js
@@ -4,19 +4,11 @@
 (function (exports, catalog) {
     'use strict';
 
-    /**
-     * See https://www.quirksmode.org/js/cookies.html
-     */
-    var readCookie = function (name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
+    var searchForm = $('form#search-form');
+
+    var queryField = $('form#search-form input#id_text');
+    var perimeterField = $('form#search-form select#id_perimeter');
+    var bookmarkNameField = $('form#bookmark-form input#id_title');
 
     /**
      * Prepend current search parameters to the form data.
@@ -24,23 +16,41 @@
      * The "alert creation form" does not actually holds the fields
      * for the current search. Hence, we have to append the search fields
      * (that are held in a different form) to the currently posted data.
-     *
-     * The easiest way to get that data is to get it from the cookie that
-     * is updated everytime a search is performed.
      */
     exports.onBookmarkFormSubmit = function (event) {
-        var searchCookieName = catalog.SEARCH_COOKIE_NAME;
-        var cookieValue = readCookie(searchCookieName);
+        var querystring = searchForm.serialize();
         var input = $('<input />');
         input.attr('type', 'hidden');
         input.attr('name', 'querystring');
-        input.attr('value', cookieValue);
+        input.attr('value', querystring);
         input.appendTo(this);
         return true;
+    };
+
+
+    /**
+     * Prefill the "alert title" field upon aid modal opening.
+     */
+    exports.onModalShow = function (event) {
+        var nameSuggestion;
+
+        var query = queryField.val();
+        var perimeter = perimeterField.children('option:selected').text();
+        var perimeterName = perimeter.split(' (')[0];
+
+
+        if (query !== '' && perimeterName !== '') {
+            nameSuggestion = perimeterName + ' — ' + query;
+        } else {
+            nameSuggestion = perimeterName + ' ' + query;
+        }
+
+        bookmarkNameField.val(nameSuggestion.trim());
     };
 
 })(this, catalog);
 
 $(document).ready(function () {
     $('form#bookmark-form').on('submit', onBookmarkFormSubmit);
+    $('div#bookmark-search-modal').on('show.bs.modal', onModalShow);
 });
