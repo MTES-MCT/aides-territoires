d58d8019c20e14129b784a87cd22e87085e9a62e
Thibault Jouannic <thibault@miximum.fr>
2019-11-05 10:04:10 +0100

    Don't hide fields on form error

--- src/static/js/aids/toggle_subvention_related_fields.js
+++ src/static/js/aids/toggle_subvention_related_fields.js
@@ -1,29 +1,39 @@
 (function (exports) {
 
+    /**
+     * Enable the subvention related fields toggling.
+     *
+     * Note: if some subvention related fields have errors (because the form
+     * was submitted before), we always display them so the error message
+     * will never be hidden.
+     */
     exports.toggleSubventionFields = function(form, div) {
 
         var checkbox = form.find('input[value=grant]');
         var checked = checkbox.prop('checked');
+        var hasErrors = div.find('p.error').length > 0;
 
-        if (checked) {
+        if (checked || hasErrors) {
             div.addClass('collapse show');
         } else {
             div.addClass('collapse');
         }
 
-        form.on('change', function() {
-            var collapse = checkbox.prop('checked') ? 'show' : 'hide';
-            div.collapse(collapse);
-        });
+        if (!hasErrors) {
+            form.on('change', function() {
+                var collapse = checkbox.prop('checked') ? 'show' : 'hide';
+                div.collapse(collapse);
+            });
+        }
     };
 
 })(this);
 
 $(document).ready(function () {
-    // Prevent status update when edit form was modified
-    // to prevent data loss.
+
+    // Only display subvention related fields when the `subvention`
+    // checkbox is checked.
     var aidEditForm = $('form.main-form');
     var subventionFieldsDiv = $('div#subvention-fields');
-
     toggleSubventionFields(aidEditForm, subventionFieldsDiv);
 });
