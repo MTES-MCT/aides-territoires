17dbc754198d53934abfad30e8b3f8a666e0e38b
Thibault Jouannic <thibault@miximum.fr>
2020-01-13 10:59:18 +0100

    Make the country import script idempotent

--- src/geofr/management/commands/populate_countries.py
+++ src/geofr/management/commands/populate_countries.py
@@ -8,11 +8,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        Perimeter.objects.create(
+        france, _ = Perimeter.objects.update_or_create(
             scale=Perimeter.TYPES.country,
             code='FRA',
             name='France')
-        Perimeter.objects.create(
+        europe, _ = Perimeter.objects.update_or_create(
             scale=Perimeter.TYPES.continent,
             code='EU',
             name='Europe')
+
+        PerimeterContainedIn = Perimeter.contained_in.through
+        PerimeterContainedIn.objects.update_or_create(
+            from_perimeter_id=france.id,
+            to_perimeter_id=europe.id)
