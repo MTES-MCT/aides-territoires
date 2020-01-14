91d7bbf1cd5f076d087fe711428ee16b9d84218b
Thibault Jouannic <thibault@miximum.fr>
2020-01-14 11:17:29 +0100

    Make the commune import task idempotent.
    Also, import communes from dom-tom

--- src/geofr/management/commands/populate_communes.py
+++ src/geofr/management/commands/populate_communes.py
@@ -1,60 +1,132 @@
-import requests
-from operator import attrgetter
-from django.core.management.base import BaseCommand, CommandError
+import json
+
+from django.db import transaction
+from django.core.management.base import BaseCommand
+from django.conf import settings
 
 from geofr.models import Perimeter
 from geofr.constants import OVERSEAS_REGIONS
 
 
-API_URL = 'https://geo.api.gouv.fr/departements/{}/communes/'
-
-
-FORMAT_STR = '{c.name};{c.code};{c.departments};{c.zipcodes}\n'
+DATA_PATH = '/node_modules/@etalab/decoupage-administratif/data/communes.json'  # noqa
 
 
 class Command(BaseCommand):
     """Import the list of all communes."""
 
+    @transaction.atomic()
     def handle(self, *args, **options):
 
-        # Let's serialize and dump existing communes in a file
-        communes = Perimeter.objects \
-            .filter(scale=Perimeter.TYPES.commune) \
-            .order_by('name')
-        list_communes = list(communes)
-        list_communes.sort(key=attrgetter('name', 'code'))
-        with open('/tmp/existing_communes.dump', 'w') as f:
-            for commune in list_communes:
-                f.write(FORMAT_STR.format(c=commune))
+        france = Perimeter.objects.get(
+            scale=Perimeter.TYPES.country,
+            code='FRA')
+        europe = Perimeter.objects.get(
+            scale=Perimeter.TYPES.continent,
+            code='EU')
+        mainland = Perimeter.objects.get(
+            scale=Perimeter.TYPES.adhoc,
+            code='FRA-MET')
+        overseas = Perimeter.objects.get(
+            scale=Perimeter.TYPES.adhoc,
+            code='FRA-OM')
+        regions_qs = Perimeter.objects \
+            .filter(scale=Perimeter.TYPES.region) \
+            .values_list('code', 'id')
+        regions = dict(regions_qs)
+        departments_qs = Perimeter.objects \
+            .filter(scale=Perimeter.TYPES.department) \
+            .values_list('code', 'id')
+        departments = dict(departments_qs)
+        adhoc_qs = Perimeter.objects \
+            .filter(scale=Perimeter.TYPES.adhoc) \
+            .values_list('code', 'id')
+        adhoc = dict(adhoc_qs)
 
-        # Now, let's fetch updated data, and dump it with the same format
-        departments = Perimeter.objects.filter(
-            scale=Perimeter.TYPES.department)
-        new_communes = []
-        for department in departments:
-            self.stdout.write(
-                'Importing communes for {}.'.format(department.name))
-            api_result = requests.get(API_URL.format(department.code))
-            if api_result.status_code != 200:
-                raise CommandError('Failed to reach geo api.')
+        PerimeterContainedIn = Perimeter.contained_in.through
+        perimeter_links = []
 
-            data = api_result.json()
+        data_file = settings.DJANGO_ROOT + DATA_PATH
+        data = json.loads(data_file.read_file())
+        nb_created = 0
+        nb_updated = 0
 
-            for entry in data:
-                commune = Perimeter(
-                    scale=Perimeter.TYPES.commune,
-                    code=entry['code'],
-                    name=entry['nom'],
-                    departments=[entry['codeDepartement']],
-                    regions=[entry['codeRegion']],
-                    zipcodes=entry['codesPostaux'],
-                    is_overseas=(entry['codeRegion'] in OVERSEAS_REGIONS))
-                new_communes.append(commune)
+        for entry in data:
 
-        new_communes.sort(key=attrgetter('name', 'code'))
-        with open('/tmp/new_communes.dump', 'w') as f:
-            for commune in new_communes:
-                f.write(FORMAT_STR.format(c=commune))
+            # There are several types of entries in the file:
+            #  - communes
+            #  - communes déléguées
+            #  - arrondissements municipaux
+            # At this stage, we only handle communes
+            if entry['type'] != 'commune-actuelle':
+                continue
 
-        print('Existing communes: {}'.format(len(list_communes)))
-        print('New communes: {}'.format(len(new_communes)))
+            # In the files, actual communes can be of two types:
+            # 1. Communes that belong in a region / department
+            # 2. Communes from "collectivités d'Outre-mer"
+
+            if 'region' in entry:
+                data = {
+                    'regions': [entry['region']],
+                    'departments': [entry['departement']],
+                    'zipcodes': entry['codesPostaux'],
+                    'is_overseas': (entry['region'] in OVERSEAS_REGIONS)
+                }
+            else:
+                data = {
+                    'is_overseas': True
+                }
+
+            defaults = {
+                'name': entry['nom']
+            }
+            defaults.update(data)
+
+            # Create or update the commune perimeter
+            commune, created = Perimeter.objects.update_or_create(
+                scale=Perimeter.TYPES.commune,
+                code=entry['code'],
+                defaults=defaults)
+            if created:
+                nb_created += 1
+            else:
+                nb_updated += 1
+
+            # Link perimeter to france, europe, regions, departements,
+            # "collectivités d'outre-mer", mainland / overseas, etc.
+            perimeter_links.append(PerimeterContainedIn(
+                from_perimeter_id=commune.id,
+                to_perimeter_id=europe.id))
+            perimeter_links.append(PerimeterContainedIn(
+                from_perimeter_id=commune.id,
+                to_perimeter_id=france.id))
+
+            for region_code in commune.regions:
+                perimeter_links.append(PerimeterContainedIn(
+                    from_perimeter_id=commune.id,
+                    to_perimeter_id=regions[region_code]))
+            for department_code in commune.departments:
+                perimeter_links.append(PerimeterContainedIn(
+                    from_perimeter_id=commune.id,
+                    to_perimeter_id=departments[department_code]))
+
+            if commune.is_overseas:
+                perimeter_links.append(PerimeterContainedIn(
+                    from_perimeter_id=commune.id,
+                    to_perimeter_id=overseas.id))
+            else:
+                perimeter_links.append(PerimeterContainedIn(
+                    from_perimeter_id=commune.id,
+                    to_perimeter_id=mainland.id))
+
+            if 'collectiviteOutremer' in entry:
+                code = entry['collectiviteOutremer']['code']
+                perimeter_links.append(PerimeterContainedIn(
+                    from_perimeter_id=commune.id,
+                    to_perimeter_id=adhoc[code]))
+
+        # Create the links between the perimeters
+        PerimeterContainedIn.objects.bulk_create(
+            perimeter_links, ignore_conflicts=True)
+
+        self.stdout.write(self.style.SUCCESS(
+            '%d communes created, %d updated.' % (nb_created, nb_updated)))
