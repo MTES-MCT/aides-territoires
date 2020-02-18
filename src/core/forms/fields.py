400b68f7b306204f1d15bbdc8e5b5dec22e3a073
Thibault Jouannic <thibault@miximum.fr>
2020-02-18 11:04:30 +0100

    Add custom groupable model choice field

--- src/core/forms/fields.py
+++ src/core/forms/fields.py
@@ -1,3 +1,6 @@
+from functools import partial
+from itertools import groupby
+from operator import attrgetter
 from django import forms
 from dataproviders.utils import content_prettify
 
@@ -30,3 +33,37 @@ class RichTextField(forms.CharField):
 
         cleaned = super().clean(value)
         return content_prettify(cleaned, more_allowed_tags=['a']).strip()
+
+
+class GroupedModelChoiceIterator(forms.models.ModelChoiceIterator):
+    def __init__(self, field, groupby):
+        self.groupby = groupby
+        super().__init__(field)
+
+    def __iter__(self):
+        if self.field.empty_label is not None:
+            yield ("", self.field.empty_label)
+        queryset = self.queryset
+        # Can't use iterator() when queryset uses prefetch_related()
+        if not queryset._prefetch_related_lookups:
+            queryset = queryset.iterator()
+        for group, objs in groupby(queryset, self.groupby):
+            yield (group, [self.choice(obj) for obj in objs])
+
+
+class GroupedModelChoiceField(forms.ModelChoiceField):
+    """A model choice field with the ability to generate optgroups.
+
+    Shamelessly stolen from here:
+    https://code.djangoproject.com/ticket/27331#comment:7
+    """
+    def __init__(self, *args, choices_groupby, **kwargs):
+        if isinstance(choices_groupby, str):
+            choices_groupby = attrgetter(choices_groupby)
+        elif not callable(choices_groupby):
+            msg = 'choices_groupby must either be a str or a callable ' \
+                  'accepting a single argument'
+            raise TypeError(msg)
+        self.iterator = partial(
+            GroupedModelChoiceIterator, groupby=choices_groupby)
+        super().__init__(*args, **kwargs)
