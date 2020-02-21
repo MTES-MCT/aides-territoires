ac04c97452ebeb2ebfa3c9d6fae3d1b2446df6aa
Thibault Jouannic <thibault@miximum.fr>
2020-02-21 10:06:53 +0100

    Render the category selection step with aid facets

--- src/search/forms.py
+++ src/search/forms.py
@@ -1,8 +1,10 @@
+from itertools import groupby
+from operator import itemgetter
+
 from django import forms
 from django.db.models import Count
 from django.utils.translation import ugettext_lazy as _
 
-from core.forms import GroupedModelChoiceField
 from categories.models import Theme, Category
 from aids.models import Aid
 from aids.forms import AidSearchForm
@@ -45,14 +47,14 @@ class PerimeterSearchForm(forms.Form):
         required=False)
 
 
-class ThemeWidget(forms.widgets.ChoiceWidget):
-    """Custom widget to select themes."""
-
-    allow_multiple_selected = True
-    template_name = 'search/forms/widgets/theme_widget.html'
-
-
 class ThemeChoiceIterator(forms.models.ModelChoiceIterator):
+    """Custom iterator for the `Theme` queryset.
+
+    This class generates the list of choices to be rendered by the widget.
+
+    We need to create a custom iterator because the theme queryset is
+    a bit peculiar. See below.
+    """
     def choice(self, obj):
         return (
             obj['categories__theme__slug'],
@@ -61,12 +63,23 @@ class ThemeChoiceIterator(forms.models.ModelChoiceIterator):
 
 
 class ThemeChoiceField(forms.ModelMultipleChoiceField):
+    """Custom choice field to select Themes.
 
+    We override the iterator and display the number of matching aids
+    in the widget.
+    """
     iterator = ThemeChoiceIterator
 
     def label_from_instance(self, obj):
         return '{} ({})'.format(
             obj['categories__theme__name'], obj['nb_aids'])
+
+
+class ThemeWidget(forms.widgets.ChoiceWidget):
+    """Custom widget to select themes."""
+
+    allow_multiple_selected = True
+    template_name = 'search/forms/widgets/theme_widget.html'
 
 
 class ThemeSearchForm(forms.Form):
@@ -101,6 +114,40 @@ class ThemeSearchForm(forms.Form):
         self.fields['theme'].queryset = themes_with_aid_count
 
 
+class CategoryIterator(forms.models.ModelChoiceIterator):
+    """Custom iterator for the category list.
+
+    We do several custom things here:
+      - handle the peculiar queryset that was used to select categories
+      - group the categories by the associated theme
+      - pass the correct parameters to the widget renderer.
+    """
+    groupby = 'categories__theme__name'  # as selected in the queryset
+
+    def __iter__(self):
+
+        for group, objs in groupby(self.queryset, itemgetter(self.groupby)):
+            yield (group, [self.choice(obj) for obj in objs])
+
+    def choice(self, obj):
+        return (
+            obj['categories__slug'],
+            self.field.label_from_instance(obj),
+        )
+
+
+class CategoryChoiceField(forms.ModelMultipleChoiceField):
+    """Custom choice field for the category list.
+
+    We override the iterator and pass the aid count to the widget label.
+    """
+    iterator = CategoryIterator
+
+    def label_from_instance(self, obj):
+        return '{} ({})'.format(
+            obj['categories__name'], obj['nb_aids'])
+
+
 class CategoryWidget(forms.widgets.ChoiceWidget):
     """Custom widget to select categories grouped by themes."""
 
@@ -117,18 +164,26 @@ class CategorySearchForm(forms.Form):
         queryset=Theme.objects.all(),
         to_field_name='slug',
         widget=forms.widgets.MultipleHiddenInput)
-    category = GroupedModelChoiceField(
+    category = CategoryChoiceField(
         queryset=Category.objects.all(),
-        choices_groupby='theme',
-        empty_label=None,
         to_field_name='slug',
         widget=CategoryWidget)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # See `ThemeSearchForm` for explanation about the following lines.
         themes = self['theme'].value()
-        self.fields['category'].queryset = Category.objects \
-            .filter(theme__slug__in=themes) \
-            .select_related('theme') \
-            .order_by('theme__name', 'name')
+        aids = Aid.objects \
+            .published() \
+            .open()
+        filter_form = AidSearchForm(self.data)
+        categories_with_aid_count = filter_form.filter_queryset(aids) \
+            .filter(categories__theme__slug__in=themes) \
+            .values(
+                'categories__theme__name',
+                'categories__name',
+                'categories__slug') \
+            .annotate(nb_aids=Count('id', distinct=True)) \
+            .order_by('categories__theme__name', 'categories__name')
+        self.fields['category'].queryset = categories_with_aid_count
