599b8889e81a9ba0e500ce3d6b01e1c8817c876e
Thibault Jouannic <thibault@miximum.fr>
2020-02-21 09:24:11 +0100

    Implement faceted search for theme search

--- src/search/forms.py
+++ src/search/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.db.models import Count
 from django.utils.translation import ugettext_lazy as _
 
 from core.forms import GroupedModelChoiceField
 from categories.models import Theme, Category
+from aids.models import Aid
+from aids.forms import AidSearchForm
 from geofr.forms.fields import PerimeterChoiceField
 
 
@@ -49,15 +52,53 @@ class ThemeWidget(forms.widgets.ChoiceWidget):
     template_name = 'search/forms/widgets/theme_widget.html'
 
 
+class ThemeChoiceIterator(forms.models.ModelChoiceIterator):
+    def choice(self, obj):
+        return (
+            obj['categories__theme__slug'],
+            self.field.label_from_instance(obj),
+        )
+
+
+class ThemeChoiceField(forms.ModelMultipleChoiceField):
+
+    iterator = ThemeChoiceIterator
+
+    def label_from_instance(self, obj):
+        return '{} ({})'.format(
+            obj['categories__theme__name'], obj['nb_aids'])
+
+
 class ThemeSearchForm(forms.Form):
     targeted_audiances = forms.CharField(
         widget=forms.widgets.HiddenInput)
     perimeter = forms.CharField(
         widget=forms.widgets.HiddenInput)
-    theme = forms.ModelMultipleChoiceField(
+    theme = ThemeChoiceField(
         queryset=Theme.objects.order_by('name'),
         to_field_name='slug',
         widget=ThemeWidget)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # We have a problem here.
+        # We want to return the list of existing themes, but we also want to
+        # count the number of aids **matching the current search** for each
+        # theme.
+        #
+        # Since the search query is already quite complex to generate, we
+        # use it as a base and then we do a join on categories and themes,
+        # then we group by theme and count.
+        aids = Aid.objects \
+            .published() \
+            .open()
+        filter_form = AidSearchForm(self.data)
+        themes_with_aid_count = filter_form.filter_queryset(aids) \
+            .values('categories__theme__slug', 'categories__theme__name') \
+            .annotate(nb_aids=Count('id', distinct=True)) \
+            .order_by('categories__theme__name')
+        self.fields['theme'].queryset = themes_with_aid_count
 
 
 class CategoryWidget(forms.widgets.ChoiceWidget):
