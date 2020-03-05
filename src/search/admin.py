e8be222782a8d3b5e1398040a694adf5980bbeb2
Thibault Jouannic <thibault@miximum.fr>
2020-03-05 09:57:57 +0100

    Create the search page admin module

---
+++ src/search/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from search.models import SearchPage
+from search.forms import SearchPageAdminForm
+
+
+class SearchPageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'meta_description']
+    prepopulated_fields = {'slug': ('title',)}
+    form = SearchPageAdminForm
+
+    class Media:
+        css = {
+            'all': (
+                '/static/css/admin.css',
+                '/static/trumbowyg/dist/ui/trumbowyg.css',
+            )
+        }
+        js = [
+            'admin/js/jquery.init.js',
+            '/static/js/plugins/softmaxlength.js',
+            '/static/js/search/enable_softmaxlength.js',
+            '/static/trumbowyg/dist/trumbowyg.js',
+            '/static/trumbowyg/dist/langs/fr.js',
+            '/static/js/enable_rich_text_editor.js',
+        ]
+
+
+admin.site.register(SearchPage, SearchPageAdmin)
---
+++ src/static/js/search/enable_softmaxlength.js
@@ -0,0 +1,9 @@
+(function ($) {
+    'use strict';
+
+    $(document).ready(function () {
+        $('form #id_title').softmaxlength();
+        $('form #id_meta_title').softmaxlength();
+        $('form #id_meta_description').softmaxlength();
+    });
+}($ || django.jQuery));
