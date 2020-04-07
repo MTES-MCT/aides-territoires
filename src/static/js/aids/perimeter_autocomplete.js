9e1a722a7a14ecec33b68ca580512ae7b5c7fe96
Thibault Jouannic <thibault@miximum.fr>
2020-04-07 10:08:48 +0200

    Update field placeholder

--- src/static/js/aids/perimeter_autocomplete.js
+++ src/static/js/aids/perimeter_autocomplete.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     });
 
     $('select#id_categories').select2({
-        placeholder: catalog.perimeter_placeholder,
+        placeholder: catalog.autocomplete_placeholder,
         language: 'fr',
         theme: 'bootstrap4',
         width: '',
