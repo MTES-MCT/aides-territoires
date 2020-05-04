c1f9aa5610c33e8917388896f83647db54bf301d
Thibault Jouannic <thibault@miximum.fr>
2020-05-04 14:56:06 +0200

    Use rich text editor for page content

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -1,15 +1,40 @@
 from django.contrib import admin
 from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.forms import FlatpageForm
 from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import ugettext_lazy as _
 
+from core.forms import RichTextField
 from pages.models import Page
 
 
-HELP = _("Note: it's not possible to change url of pages in the main menu.")
+class PageForm(FlatpageForm):
+    content = RichTextField(label=_('Content'))
+
+    class Meta:
+        model = Page
+        fields = '__all__'
 
 
 class PageAdmin(FlatPageAdmin):
+
+    HELP = _("WARNING! DON'T CHANGE url of pages in the main menu.")
+
+    class Media:
+        css = {
+            'all': (
+                '/static/css/admin.css',
+                '/static/trumbowyg/dist/ui/trumbowyg.css',
+            )
+        }
+        js = [
+            'admin/js/jquery.init.js',
+            '/static/trumbowyg/dist/trumbowyg.js',
+            '/static/trumbowyg/dist/langs/fr.js',
+            '/static/js/enable_rich_text_editor.js',
+        ]
+
+    form = PageForm
     fieldsets = (
         (None, {
             'fields': ('url', 'title', 'content'),
