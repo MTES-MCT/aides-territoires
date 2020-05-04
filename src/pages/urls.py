20efbdb75745c204deae181775c1bef40ee1c7dd
Thibault Jouannic <thibault@miximum.fr>
2020-05-04 11:09:31 +0200

    Create flatpage template

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,22 +1,6 @@
 from django.urls import path, include
-from django.contrib.flatpages import views
 
 
 urlpatterns = [
-    path(
-        'plateforme-aides-territoires/',
-        views.flatpage,
-        {'url': '/plateforme-aides-territoires/'},
-        name='page_about_us'),
-    path(
-        'porteurs-aides/',
-        views.flatpage,
-        {'url': '/porteurs-aides/'},
-        name='page_backers'),
-    path(
-        'aides-collectivites/',
-        views.flatpage,
-        {'url': '/aides-collectivites/'},
-        name='page_communities'),
     path('', include('django.contrib.flatpages.urls')),
 ]
