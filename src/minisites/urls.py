437a01111279501e10e60b8d1396cd4c0206dc8f
Thibault Jouannic <thibault@miximum.fr>
2020-06-29 11:18:37 +0200

    Fix issues with api urls

--- src/minisites/urls.py
+++ src/minisites/urls.py
@@ -1,23 +1,31 @@
+from rest_framework import routers
 from django.urls import path, include
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.conf.urls.static import static
 
 from minisites.views import Home, Search, Aid, Alert
-from geofr.api.urls import router
 
+
+router = routers.DefaultRouter()
+
+api_patterns = [
+    path('', include(router.urls)),
+    path('perimeters/', include('geofr.api.urls')),
+    path('backers/', include('backers.api.urls')),
+]
 
 urlpatterns = [
     path('', Home.as_view(),  name='home'),
-    path(_('search/'), Search.as_view(),
-         name='advanced_search_view'),
+    path(_('search/'), Search.as_view(), name='advanced_search_view'),
     path('<slug:slug>/', include([
-        path('', Aid.as_view(), name='aid_detail_view'),
-    ])),
+        path('', Aid.as_view(), name='aid_detail_view')])),
     path(_('alert/'), Alert.as_view(), name='alert_create_view'),
+
+    # Api related routes
+    path('api/', include(api_patterns)),
 ]
 
-urlpatterns += router.urls
 
 if settings.DEBUG:
     import debug_toolbar
