c02e8d435fcbeab7c5c268a53dea7581cd0e468d
Thibault Jouannic <thibault@miximum.fr>
2020-07-06 11:42:35 +0200

    Add missing tests for minisite views

---
+++ src/minisites/factories.py
@@ -0,0 +1,13 @@
+import factory
+from search.factories import SearchPageFactory
+
+
+class MinisiteFactory(SearchPageFactory):
+
+    color_1 = '#cccccc'
+    color_2 = '#cccccc'
+    color_3 = '#cccccc'
+    color_4 = '#cccccc'
+    color_5 = '#cccccc'
+    logo = factory.Faker('file_path')
+    logo_link = factory.Faker('url')
