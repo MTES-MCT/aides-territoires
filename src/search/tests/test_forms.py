f7dc222fd04a87fc5ecf11ceee511aab89004f69
Thibault Jouannic <thibault@miximum.fr>
2020-09-10 16:41:49 +0200

    Prevent adding more than 3 search filters

--- src/search/tests/test_forms.py
+++ src/search/tests/test_forms.py
@@ -20,7 +20,9 @@ def test_valid_search_page_form():
         'search_querystring': 'text=',
         'show_perimeter_field': True,
         'show_audiance_field': True,
-        'show_categories_field': True})
+        'show_categories_field': True,
+        'show_mobilization_step_field': False,
+    })
 
     assert form.is_valid()
 
@@ -36,12 +38,14 @@ def test_search_form_field_customizations():
         'search_querystring': 'text=',
         'show_perimeter_field': True,
         'show_audiance_field': False,
-        'show_categories_field': False})
+        'show_categories_field': False,
+        'show_mobilization_step_field': False,
+    })
 
     assert form.is_valid()
 
 
-def test_search_form_bad_customization():
+def test_search_form_not_enough_filters():
     """It's impossible to hide *all* search form fields."""
 
     form = DummySearchPageAdminForm({
@@ -52,7 +56,28 @@ def test_search_form_bad_customization():
         'search_querystring': 'text=',
         'show_perimeter_field': False,
         'show_audiance_field': False,
-        'show_categories_field': False})
+        'show_categories_field': False,
+        'show_mobilization_step_field': False,
+    })
 
     assert not form.is_valid()
-    assert form.errors.as_data()['__all__'][0].code == 'missing_filter'
+    assert form.errors.as_data()['__all__'][0].code == 'not_enough_filters'
+
+
+def test_search_form_too_many_filters():
+    """It's impossible to select more than three search form fields."""
+
+    form = DummySearchPageAdminForm({
+        'title': 'Test title',
+        'slug': 'test-title',
+        'content': 'Test content',
+        'more_content': 'Test content',
+        'search_querystring': 'text=',
+        'show_perimeter_field': True,
+        'show_audiance_field': True,
+        'show_categories_field': True,
+        'show_mobilization_step_field': True,
+    })
+
+    assert not form.is_valid()
+    assert form.errors.as_data()['__all__'][0].code == 'too_many_filters'
