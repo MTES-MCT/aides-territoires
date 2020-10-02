554731f629724a9aaf767bc278f2620bfaf56c89
Sylvain <sylvain@lazybird.co>
2020-10-02 17:01:02 +0200

    In backers admin: Added link to aid admin and order related aids by name.

--- src/backers/admin.py
+++ src/backers/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.urls import reverse
 from django.utils.html import escape, mark_safe
 from django.utils.translation import ugettext_lazy as _
+
 
 
 from backers.models import Backer
@@ -36,10 +38,14 @@ class BackerAdmin(admin.ModelAdmin):
     nb_instructed_aids.admin_order_field = 'nb_instructed_aids'
 
     def display_related_aids(self, obj):
-        related_aid_html = ''
-        for aid in obj.financed_aids.all():
-            related_aid_html += '<br/> * '
+        related_aid_html = '<ul>'
+        for aid in obj.financed_aids.all().order_by('name'):
+            url = reverse("admin:aids_aid_change", args=(aid.pk,))
+            related_aid_html += f'<li><a href="{url}">'
             related_aid_html += escape(aid.name)
+            related_aid_html += f' (ID : {aid.pk})'
+            related_aid_html += '</a></li>'
+        related_aid_html += '</ul>'
         return mark_safe(related_aid_html)
     display_related_aids.short_description = _('Related aids')
 
