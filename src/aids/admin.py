500aaadb95c5c64d30b9ebd19570a4463f2a7dfe
Raphael Odini <raphael.odini@protonmail.com>
2020-11-03 14:39:42 +0100

    Import Aid export in the admin: translate columns content

--- src/aids/admin.py
+++ src/aids/admin.py
@@ -127,6 +127,41 @@ class AidResource(resources.ModelResource):
         # adding custom widgets breaks the usual order
         export_order = [field.name for field in Aid._meta.fields]
 
+    def get_export_headers(self):
+        """override get_export_headers() to translate field names."""
+        headers = []
+        for field in self.get_export_fields():
+            field_model = self.Meta.model._meta.get_field(field.column_name)
+            headers.append(field_model.verbose_name)
+        return headers
+
+    def export_field(self, field, obj):
+        """override export_field() to translate field values."""
+        field_name = self.get_field_name(field)
+        method = getattr(self, 'dehydrate_%s' % field_name, None)
+        if method is not None:
+            return method(obj)
+
+        field_model = self.Meta.model._meta.get_field(field.column_name)
+        if field_model.serialize:
+            # simple fields with choices: use get_FOO_display to translate
+            if field_model.choices:
+                value = getattr(obj, f'get_{field.column_name}_display')()
+                return field.widget.render(value, obj)
+            # ChoiceArrayField fields: need to translate a list
+            elif hasattr(field_model, 'base_field'):
+                value_raw = field.get_value(obj)
+                if value_raw:
+                    # translate each dict choice
+                    value = [dict(field_model.base_field.choices).get(value, value) for value in value_raw]  # noqa
+                    return field.widget.render(value, obj)
+            # BooleanField fields: avoid returning 1 (True) and 0 (False)
+            elif field_model.get_internal_type() == 'BooleanField':
+                value_raw = field.get_value(obj)
+                if value_raw is not None:
+                    return _('Yes') if value_raw else _('No')
+        return field.export(obj)
+
 
 class BaseAidAdmin(ExportActionMixin, admin.ModelAdmin):
     """Admin module for aids."""
