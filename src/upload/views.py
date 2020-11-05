dd784f33ddaf68ab024eac6b5467180af4391b82
Sylvain <sylvain@lazybird.co>
2020-11-05 16:43:02 +0100

    Upload image: add handlning for empty data posting

--- src/upload/views.py
+++ src/upload/views.py
@@ -8,11 +8,16 @@ class UploadImageView(View):
     http_method_names = ['post']
 
     def post(self, request, *args, **kwargs):
-        # import ipdb ; ipdb.set_trace()
-        image = self.request.FILES['image']
-        uploaded = UploadImage.objects.create(image=image)
+        image = self.request.FILES.get('image')
+        if not image:
+            return JsonResponse({'success': False})
+        description = self.request.POST.get('alt', '')
+        uploaded = UploadImage.objects.create(
+            image=image,
+            description=description,
+        )
         response_data = {
             'success': True,
-            'file': uploaded.image.url
+            'file': uploaded.image.url,
         }
         return JsonResponse(response_data)
