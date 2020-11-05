4fb2834e79b57e53b4f3b9a4fd5253d4cba8933f
Sylvain <sylvain@lazybird.co>
2020-11-06 00:03:41 +0100

    Refactor tests

--- src/upload/tests/test_views.py
+++ src/upload/tests/test_views.py
@@ -1,44 +1,60 @@
 import os
 
 import pytest
+
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 from upload.models import UploadImage
 
-pytestmark = pytest.mark.django_db
 
+pytestmark = pytest.mark.django_db
 
 IMAGE_PATH = os.path.join(os.getcwd(), "upload/tests/data/test-image.png")
 
+User = get_user_model()
 
-def test_upload_view_accept_post(client):
+
+def upload_image(client):
     url = reverse('upload_image')
     with open(IMAGE_PATH, 'rb') as img:
         post_data = {'alt': 'test image', 'image': img}
         response = client.post(url, post_data, format='multipart')
+    return response
+
+
+def test_superuser_can_post_to_upload_view(client, superuser):
+    client.force_login(superuser)
+    response = upload_image(client)
     assert response.status_code == 200
 
 
-def test_upload_view_creates_an_image(client):
-    url = reverse('upload_image')
+def test_annonymous_cannot_post_to_upload_view(client):
+    client.logout()
+    response = upload_image(client)
+    assert response.status_code == 302
+
+
+def test_superuser_can_upload_an_image(client, superuser):
+    client.force_login(superuser)
     count_before = UploadImage.objects.count()
-    with open(IMAGE_PATH, 'rb') as img:
-        post_data = {'alt': 'test image', 'image': img}
-        response = client.post(url, post_data, format='multipart')
+    upload_image(client)
     count_after = UploadImage.objects.count()
-    assert response.status_code == 200
     assert count_after == count_before + 1
 
 
-def test_upload_with_empty_data_return_false(client):
+def test_annonymous_cannot_upload_an_image(client):
+    client.logout()
+    count_before = UploadImage.objects.count()
+    upload_image(client)
+    count_after = UploadImage.objects.count()
+    assert count_after == count_before
+
+
+def test_upload_with_empty_data_fail_gracefully(client, superuser):
+    client.force_login(superuser)
     url = reverse('upload_image')
     empty_data = {}
     response = client.post(url, empty_data)
     assert response.status_code == 200
     assert '"success": false' in response.content.decode()
-
-
-# def test_annonymous_cannot_upload_image(client):
-#     url = reverse('upload_image')
-#     res = client.post(url)
-#     assert res.status_code == 200
