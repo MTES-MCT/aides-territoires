255728e0af5be7296c1a4a57b3803cfea63dd18b
SarahRubio <sarahrubio.pro@gmail.com>
2020-11-12 15:18:46 +0100

    use input instead of label to pass to matomo

--- src/static/js/search/track_theme_step_events.js
+++ src/static/js/search/track_theme_step_events.js
@@ -5,8 +5,8 @@
         form.submit(function () {
             try {
                 var labels = $(this).find(':checked').map(function (counter, checkbox) {
-                    var label = form.find('label[for=' + checkbox.id + '].custom-control-label');
-                    return label.text().trim();
+                    var label = form.find('input[id='+ checkbox.id + '][name=themes].custom-control-input');
+                    return label.val().trim();
                 });
                 var allLabels = labels.toArray().reduce(function (acc, value) {
                     var accumulated;
