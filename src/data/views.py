12e8bf1a3aba038bc7cf188b5427d1c75ec7d82e
Thibault Jouannic <thibault@miximum.fr>
2020-11-17 15:06:31 +0100

    Update api doc with versioning info

--- src/data/views.py
+++ src/data/views.py
@@ -1,7 +1,14 @@
 from django.views.generic import TemplateView
 
+from django.conf import settings
+
 
 class DataDocView(TemplateView):
     """Show aids api doc and licence."""
 
     template_name = 'data/doc.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['api_version'] = settings.CURRENT_API_VERSION
+        return context
