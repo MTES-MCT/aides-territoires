89eab7709ef3469e3ebb166179fb98e1742485b4
Sylvain <sylvain@lazybird.co>
2020-11-19 01:44:53 +0100

    There is a bug on the resizimg plugin and this is a workaround
    The issue we are dealing with is solved in upstream Trumbowyg repo.
    Details :
    - https://github.com/Alex-D/Trumbowyg/issues/1158
    - https://github.com/Alex-D/Trumbowyg/pull/1116
    
    I have found a better way to workaround this issue:
    - I have imported a copy of the resizimg JS plugin
    - Instead of loading the upstream version, I'm using that local version
    of the plugin.
    - This is a temporary fix and we should get back to using upstream once
    the fix is released.

--- src/upload/settings.py
+++ src/upload/settings.py
@@ -2,5 +2,5 @@ TRUMBOWYG_UPLOAD_ADMIN_JS = [
     '/static/trumbowyg/dist/plugins/upload/trumbowyg.upload.js',
     '/static/trumbowyg/dist/plugins/resizimg/resizable-resolveconflict.js',
     '/static/jquery-resizable-dom/dist/jquery-resizable.js',
-    '/static/trumbowyg/dist/plugins/resizimg/trumbowyg.resizimg.js',
+    '/static/js/trumbowyg-resizimg/trumbowyg.resizimg-with-canvas-fix.js',
 ]
