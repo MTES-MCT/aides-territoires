594c6c11a0f9a530a397112ab2b3269b7bbac333
Sylvain <sylvain@lazybird.co>
2020-11-19 02:12:01 +0100

    Add some comment about resizimg

--- src/upload/settings.py
+++ src/upload/settings.py
@@ -2,5 +2,11 @@ TRUMBOWYG_UPLOAD_ADMIN_JS = [
     '/static/trumbowyg/dist/plugins/upload/trumbowyg.upload.js',
     '/static/trumbowyg/dist/plugins/resizimg/resizable-resolveconflict.js',
     '/static/jquery-resizable-dom/dist/jquery-resizable.js',
+
+    # We are using a local version of trumbowyg.resizimg because there is
+    # a bug on the upstream version. Once that bug is resolved and released,
+    # we should get back to using upstream.
+    # See PR : https://github.com/Alex-D/Trumbowyg/pull/1116
+    # See issue : https://github.com/Alex-D/Trumbowyg/issues/1158
     '/static/js/trumbowyg-resizimg/trumbowyg.resizimg-with-canvas-fix.js',
 ]
