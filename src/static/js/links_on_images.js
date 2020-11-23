35437f6f54c61a639d03fc98504c346e91ab7fd5
Sylvain <sylvain@lazybird.co>
2020-11-23 06:40:07 +0100

    Add a comment for "no after effect"

--- src/static/js/links_on_images.js
+++ src/static/js/links_on_images.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $(document).ready(function() {
+        // We don't want the visual "after effect" on images links.
         let linksForImages = $("a:has(img)")
         for (link of linksForImages) {
             link.classList.add("no-after")
