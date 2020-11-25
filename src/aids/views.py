e2e9c4ea55cbc2ec31da07e56da7a619c90b473e
SarahRubio <sarahrubio.pro@gmail.com>
2020-11-25 17:00:31 +0100

    Fix style

--- src/aids/views.py
+++ src/aids/views.py
@@ -405,7 +405,9 @@ class AidStatusUpdate(ContributorRequiredMixin, AidEditMixin,
         STATES = AidWorkflow.states
         if aid.status == STATES.draft:
             aid.submit()
-            msg = _('Your aid will be reviewed by an admin soon. It will be published and visible for users once an admin has approved it.')
+            msg = _('Your aid will be reviewed by an admin soon. '
+                    'It will be published and visible for users '
+                    'once an admin has approved it.')
         elif aid.status in (STATES.reviewable, STATES.published):
             aid.unpublish()
             log_admins.delay(
