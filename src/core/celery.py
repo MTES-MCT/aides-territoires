490566aa9385650ab899c57f52315b26a0d611d3
Sylvain <lazybird@users.noreply.github.com>
2020-12-14 15:39:51 +0100

    Remove a comment

--- src/core/celery.py
+++ src/core/celery.py
@@ -8,8 +8,5 @@ from django.conf import settings  # noqa
 
 
 app = Celery('aides')
-
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
