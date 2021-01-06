ae6859297f15f12178754d6a04abc1e5aceb20ff
Raphael Odini <raphael.odini@protonmail.com>
2021-01-06 18:10:42 +0100

    Add 2 graphs per month in the stats page

--- src/stats/views.py
+++ src/stats/views.py
@@ -2,9 +2,10 @@ from datetime import timedelta
 
 from django.views.generic import TemplateView
 from django.utils import timezone
-from django.db.models import Sum, Q
+from django.db.models import Sum, Q, Count, F, Func, Value, CharField
 
 from aids.models import Aid
+from alerts.models import Alert
 from backers.models import Backer
 from stats.models import Event
 
@@ -21,12 +22,35 @@ class StatsView(TemplateView):
         one_week_ago = timezone.now() - timedelta(days=8)
         viewed_aids_qs = Event.objects.filter(category='aid', event='viewed') \
             .filter(date_created__gte=one_week_ago)
-        context['nb_viewed_aids'] = viewed_aids_qs.count()
+        context['nb_viewed_aids_1_week'] = viewed_aids_qs.count()
+
+        viewed_aids_timeseries = Event.objects \
+            .filter(category='aid', event='viewed') \
+            .annotate(day=Func(
+                F('date_created'),
+                Value('YYYY-MM-01'),
+                function='to_char',
+                output_field=CharField())) \
+            .values('day') \
+            .annotate(y=Count('id')) \
+            .order_by()
+        context['nb_viewed_aids_timeseries'] = list(viewed_aids_timeseries)
 
         alerts_qs = Event.objects \
             .filter(category='alert', event='sent') \
             .aggregate(nb_sent_alerts=Sum('value'))
         context['nb_sent_alerts'] = alerts_qs['nb_sent_alerts']
+
+        alerts_created_timeseries = Alert.objects.filter(validated=True) \
+            .annotate(day=Func(
+                F('date_created'),
+                Value('YYYY-MM-01'),
+                function='to_char',
+                output_field=CharField())) \
+            .values('day') \
+            .annotate(y=Count('token')) \
+            .order_by()
+        context['nb_alerts_created_timeseries'] = list(alerts_created_timeseries)  # noqa
 
         financers = aids_qs.values_list('financers', flat=True)
         instructors = aids_qs.values_list('instructors', flat=True)
