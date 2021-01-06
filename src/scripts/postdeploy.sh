614b0087a464644f8f516a2b115f07968c427aea
Sylvain <lazybird@users.noreply.github.com>
2021-01-06 12:37:28 +0100

    Update postdepoly pg_dump command

--- src/scripts/postdeploy.sh
+++ src/scripts/postdeploy.sh
@@ -3,7 +3,7 @@ echo "Entering postdeploy hook"
 
 if [ "$IS_REVIEW_APP" = true ] ; then
   echo "Review Apps : Database copy"
-  pg_dump $STAGING_DATABASE_URL | psql $DATABASE_URL
+  pg_dump --clean --if-exists --dbname $STAGING_DATABASE_URL --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' | psql $DATABASE_URL
 fi
 
 python manage.py migrate
