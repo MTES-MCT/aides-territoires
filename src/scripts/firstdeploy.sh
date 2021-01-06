d4f1a156bdd53baee1655e82b2f3a7c37d8c5ca7
Sylvain <lazybird@users.noreply.github.com>
2021-01-06 12:44:53 +0100

    Now using firstdeploy script for review apps

---
+++ src/scripts/firstdeploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Entering firstdeploy hook for Review Apps"
+pg_dump --clean --if-exists --dbname $STAGING_DATABASE_URL --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' | psql $DATABASE_URL
+bash scripts/postdeploy.sh
+echo "Completed firstdeploy hook"
--- src/scripts/postdeploy.sh
+++ src/scripts/postdeploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 echo "Entering postdeploy hook"
-
-if [ "$IS_REVIEW_APP" = true ] ; then
-  echo "Review Apps : Database copy"
-  pg_dump --clean --if-exists --dbname $STAGING_DATABASE_URL --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' | psql $DATABASE_URL
-fi
-
 python manage.py migrate
 python manage.py compilemessages
 python manage.py compress --force
