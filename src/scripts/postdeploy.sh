4539aba761daa7e627cea5fde268ca963a16e75f
Sylvain <lazybird@users.noreply.github.com>
2021-01-06 11:01:45 +0100

    Add database copy for review apps

--- src/scripts/postdeploy.sh
+++ src/scripts/postdeploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "Entering postdeploy hook"
+
+if [ "$IS_REVIEW_APP" = "true" ] ; then
+  echo "    Review Apps : Database copy"
+  pg_dump $STAGING_DATABASE_URL | psql $DATABASE_URL
+fi
+
 python manage.py migrate
 python manage.py compilemessages
 python manage.py compress --force
