f28c0d3b31f5422baeb62d51ae1c60c5ff9b15e5
Sylvain <lazybird@users.noreply.github.com>
2021-01-06 11:01:45 +0100

    Add first deploy hook to copy database for review apps
    - Add scalingo.json and firstdeploy script for review apps
    - Add some comments on deploy scripts

--- src/scripts/postdeploy.sh
+++ src/scripts/postdeploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+# This script is used by scalingo each time the application
+# is deployed.
+# For review apps, if a first-deploy hook is defined, then
+# that first deploy hook will override this postdeploy hook.
+
 echo "Entering postdeploy hook"
 python manage.py migrate
-python manage.py compilemessages
 python manage.py compress --force
 echo "Completed postdeploy hook"
--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script is used by scalingo to start the web application
+# It run every time the app container starts, for instance, 
+# after a deployment or when the container is restarted.
+
 echo "Entering deployment start script"
 python manage.py compilemessages
 gunicorn core.wsgi_scalingo --log-file -
