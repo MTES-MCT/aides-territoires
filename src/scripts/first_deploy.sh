d4b0a4cbea8dcac07ad6eae4e7f7fcbea8324a4e
Sylvain <lazybird@users.noreply.github.com>
2021-01-11 12:45:07 +0100

    On first deploy Scalingo script : change pg_dump command
    The schema exclusion probably prevent the extensions to be copied

--- src/scripts/first_deploy.sh
+++ src/scripts/first_deploy.sh
@@ -6,7 +6,7 @@
 # replaces the post deploy hook.
 
 echo "Entering first deploy hook for Review Apps"
-pg_dump --clean --if-exists --dbname $STAGING_DATABASE_URL --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' | psql $DATABASE_URL
+pg_dump --clean --if-exists --dbname $STAGING_DATABASE_URL --no-owner --no-privileges --no-comments | psql $DATABASE_URL
 
 # We want to include commands from the post deploy hook as well:
 bash $HOME/scripts/post_deploy.sh
