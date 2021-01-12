8b8d53a521934b7e67edee5cec876aab6bb32a88
Thibault Jouannic <thibault@miximum.fr>
2021-01-12 11:23:12 +0100

    Add extension that was deleted by mistake

--- src/scripts/first_deploy.sh
+++ src/scripts/first_deploy.sh
@@ -11,6 +11,7 @@ PG_EXCLUDE_TABLE="-T django_session -T actstream_action -T stats_event"
 PG_EXCLUDE_SCHEMA="-N 'information_schema' -N '^pg_*'"
 pg_dump $PG_OPTIONS $PG_EXCLUDE_TABLE $PG_EXCLUDE_SCHEMA --dbname $STAGING_DATABASE_URL --format c --file /tmp/dump.pgsql
 pg_restore $PG_OPTIONS --dbname $DATABASE_URL /tmp/dump.pgsql
+psql -d $DATABASE_URL -c 'CREATE EXTENSION IF NOT EXISTS pg_trgm;'
 
 # We want to include commands from the post deploy hook as well:
 bash $HOME/scripts/post_deploy.sh
