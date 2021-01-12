663e23bc2fc0b95dd24d78ada178fc392dc3e8e5
Thibault Jouannic <thibault@miximum.fr>
2021-01-12 10:37:48 +0100

    Exclude some more schemas from pg dump

--- src/scripts/first_deploy.sh
+++ src/scripts/first_deploy.sh
@@ -7,8 +7,9 @@
 
 echo "Entering first deploy hook for Review Apps"
 PG_OPTIONS="--clean --if-exists --no-owner --no-privileges --no-comments"
-PG_EXCLUDE_TABLE="-T django_session -T actstream_action -T geofr_perimeter_contained_in -T stats_event"
-pg_dump $PG_OPTIONS $PG_EXCLUDE_TABLE --dbname $STAGING_DATABASE_URL --format c --file /tmp/dump.pgsql
+PG_EXCLUDE_TABLE="-T django_session -T actstream_action -T stats_event"
+PG_EXCLUDE_SCHEMA="-N 'information_schema' -N '^pg_*'"
+pg_dump $PG_OPTIONS $PG_EXCLUDE_TABLE $PG_EXCLUDE_SCHEMA --dbname $STAGING_DATABASE_URL --format c --file /tmp/dump.pgsql
 pg_restore $PG_OPTIONS --dbname $DATABASE_URL /tmp/dump.pgsql
 
 # We want to include commands from the post deploy hook as well:
