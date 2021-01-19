77833ce0cd33bee05c0a680d808e07097be86794
Thibault Jouannic <thibault@miximum.fr>
2021-01-19 11:54:10 +0100

    Fix a bad unaccent lookup

--- src/geofr/api/views.py
+++ src/geofr/api/views.py
@@ -1,11 +1,18 @@
+import unicodedata
 from rest_framework import viewsets
 from django.contrib.postgres.search import TrigramSimilarity
+from django.contrib.postgres.lookups import Unaccent
 
 from geofr.models import Perimeter
 from geofr.api.serializers import PerimeterSerializer
 
 
 MIN_SEARCH_LENGTH = 1
+
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 
 class PerimeterViewSet(viewsets.ReadOnlyModelViewSet):
@@ -18,8 +25,8 @@ class PerimeterViewSet(viewsets.ReadOnlyModelViewSet):
         q = self.request.query_params.get('q', '')
         if len(q) >= MIN_SEARCH_LENGTH:
             qs = qs \
-                .annotate(similarity=TrigramSimilarity('name__unaccent', q)) \
-                .filter(name__unaccent__trigram_similar=q) \
+                .annotate(similarity=TrigramSimilarity(Unaccent('name'), q)) \
+                .filter(name__unaccent__trigram_similar=remove_accents(q)) \
                 .order_by('-similarity', '-scale', 'name')
 
         return qs
