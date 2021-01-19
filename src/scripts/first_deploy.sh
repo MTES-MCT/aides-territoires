a744a9c18ea13920787e35d6e0e163394ee4a373
Thibault Jouannic <thibault@miximum.fr>
2021-01-19 14:32:54 +0100

    Activate the unaccent extension on db scripts

--- src/scripts/first_deploy.sh
+++ src/scripts/first_deploy.sh
@@ -11,6 +11,7 @@ PG_EXCLUDE_SCHEMA="-N 'information_schema' -N '^pg_*'"
 pg_dump $PG_OPTIONS $PG_EXCLUDE_SCHEMA --dbname $STAGING_DATABASE_URL --format c --file /tmp/dump.pgsql
 pg_restore $PG_OPTIONS --dbname $DATABASE_URL /tmp/dump.pgsql
 psql -d $DATABASE_URL -c 'CREATE EXTENSION IF NOT EXISTS pg_trgm;'
+psql -d $DATABASE_URL -c 'CREATE EXTENSION IF NOT EXISTS unaccent;'
 
 # We want to include commands from the post deploy hook as well:
 bash $HOME/scripts/post_deploy.sh
