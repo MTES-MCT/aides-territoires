7c62d0240fdeba62a4c4ab10c235b40ae0c1d594
SarahRubio <sarahrubio.pro@gmail.com>
2021-02-04 12:36:29 +0100

    catch next & last_page url

--- src/static/js/aids/show_more.js
+++ src/static/js/aids/show_more.js
@@ -1,11 +1,22 @@
+// Hide pagination's div and display #show_more button instead
 $('#show_more').removeClass('d-none')
 $('.pagination').addClass('d-none')
+
+// Catch and serialize research form  
 next_url = $(".form-body").serialize()
 
-$("#show_more_btn").on("click", function(){
+// Catch next_page and last_page url 
+next_page = $(".next")[0].href
+last_page = $(".last")[0].href
+
+// On click on #show_more button display new results on the same page. 
+$("#show_more_btn").on("click", function(e){
+    e.preventDefault();
+    next_page = $(".next")[0].href
+    last_page = $(".last")[0].href
     $.ajax({
         type: "GET",
-        url: next_url,
+        url: $(".next")[0].href,
         cache: false,
         success: function(html){
             html_parsed = $.parseHTML(html)
@@ -16,5 +27,6 @@ $("#show_more_btn").on("click", function(){
         }  
     });
     console.log(next_url)
+    console.log(next_page)
+    console.log(last_page)
 })
-
