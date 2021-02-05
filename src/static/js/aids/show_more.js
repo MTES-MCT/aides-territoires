0390d6d7660cbaf0dd2b5fcb4a0b947361364469
SarahRubio <sarahrubio.pro@gmail.com>
2021-02-05 09:58:35 +0100

    hide show-more btn for last page

--- src/static/js/aids/show_more.js
+++ src/static/js/aids/show_more.js
@@ -1,32 +1,33 @@
-// Hide pagination's div and display #show_more button instead
-$('#show_more').removeClass('d-none')
-$('.pagination').addClass('d-none')
+$(document).ready(function () {
 
-// Catch and serialize research form  
-next_url = $(".form-body").serialize()
+    // Hide pagination's div and display #show_more button instead
+    $('#show_more').removeClass('d-none')
+    $('.pagination').addClass('d-none')
 
-// Catch next_page and last_page url 
-next_page = $(".next")[0].href
-last_page = $(".last")[0].href
+    // On click on #show_more button display new results on the same page. 
+    $("#show_more_btn").on("click", function(e){
+        e.preventDefault();
+        next_url = $(".form-body").serialize()
+        next_page = $(".next")[0].href
+        last_page = $(".last")[0].href
 
-// On click on #show_more button display new results on the same page. 
-$("#show_more_btn").on("click", function(e){
-    e.preventDefault();
-    next_page = $(".next")[0].href
-    last_page = $(".last")[0].href
-    $.ajax({
-        type: "GET",
-        url: $(".next")[0].href,
-        cache: false,
-        success: function(html){
-            html_parsed = $.parseHTML(html)
-            $('.aids').append($(html_parsed).find(".aids > .col"))
-        },
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest'
-        }  
-    });
-    console.log(next_url)
-    console.log(next_page)
-    console.log(last_page)
-})
+        if(next_page !== last_page) {
+            searchXHR = $.ajax({
+                type: "GET",
+                url: next_page,
+                cache: false,
+                success: function(html){
+                    html_parsed = $.parseHTML(html)
+                    $('.aids').append($(html_parsed).find(".aids > .col"))
+                },
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            })        
+        } else {
+            $('#show_more').removeClass('d-none')
+        }
+        console.log(next_page)
+        console.log(last_page)
+    })
+});
