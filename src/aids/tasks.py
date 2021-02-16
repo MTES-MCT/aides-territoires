0463933aeba6ad49cf9e2e4c22464876aa2b8c17
Sylvain <lazybird@users.noreply.github.com>
2021-02-17 00:48:42 +0100

    Add email sending when aid is published

--- src/aids/tasks.py
+++ src/aids/tasks.py
@@ -1,8 +1,11 @@
-from django.core.mail import mail_admins
+from django.conf import settings
 from django.contrib.sites.models import Site
+from django.core.mail import mail_admins
 from django.template.loader import render_to_string
 
 from core.celery import app
+from django.apps import apps
+from emails.utils import send_template_email
 
 
 @app.task
@@ -21,3 +24,27 @@ def log_admins(subject, body, url):
         subject,
         email_body,
         fail_silently=False)
+
+
+@app.task
+def send_publication_email(aid_id):
+    """
+    Sends an email when an aid gets published.
+    """
+    if not settings.SIB_PUBLICATION_EMAIL_ENABLED:
+        return
+
+    Aid = apps.get_model('aids.Aid')
+    aid = Aid.objects.get(pk=aid_id)
+    author = aid.author
+    data = {
+        'PRENOM': author.first_name,
+        'NOM': author.last_name,
+        'AIDE_NOM': aid.name,
+    }
+    send_template_email(
+        recipient_list=[author.email],
+        template_id=settings.SIB_PUBLICATION_EMAIL_TEMPLATE_ID,
+        data=data,
+        tags=['publication', settings.ENV_NAME],
+        fail_silently=True)
