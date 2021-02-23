b29d95e285c9e3302e2c887d8e26500ef4d1f198
SarahRubio <sarahrubio.pro@gmail.com>
2021-02-23 12:35:06 +0100

    add categories-field to projectsuggestform

--- src/projects/forms.py
+++ src/projects/forms.py
@@ -1,14 +1,31 @@
 from django import forms
 
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.utils.translation import gettext_lazy as _
+
 from projects.models import Project
+from categories.fields import CategoryMultipleChoiceField
+from categories.models import Category, Theme
 
 
 class ProjectSuggestForm(forms.ModelForm):
     """form for project suggested by user."""
 
+    CATEGORIES_QS = Category.objects \
+        .select_related('theme') \
+        .order_by('theme__name', 'name')
+
+    categories = CategoryMultipleChoiceField(
+        group_by_theme=True,
+        label=_('Themes'),
+        queryset=CATEGORIES_QS,
+        to_field_name='slug',
+        required=False,
+        initial="voirie")
+
     class Meta:
         model = Project
-        fields = ['name', 'description']
+        fields = ['name', 'description', 'categories']
 
     def clean_name(self):
         data = self.cleaned_data['name']
