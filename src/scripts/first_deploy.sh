053d5b065955f0918c08cfb8f732c81494603f07
Sylvain <lazybird@users.noreply.github.com>
2021-03-08 11:15:22 +0100

    Add table data exclude in order to have smaller db for review apps

--- src/scripts/first_deploy.sh
+++ src/scripts/first_deploy.sh
@@ -7,8 +7,9 @@
 
 echo "Entering first deploy hook for Review Apps"
 PG_OPTIONS="--clean --if-exists --no-owner --no-privileges --no-comments"
-PG_EXCLUDE_SCHEMA="-N 'information_schema' -N '^pg_*'"
-pg_dump $PG_OPTIONS $PG_EXCLUDE_SCHEMA --dbname $STAGING_DATABASE_URL --format c --file /tmp/dump.pgsql
+PG_EXCLUDE_SCHEMA="--exclude-schema 'information_schema' --exclude-schema '^pg_*'"
+PG_EXCLUDE_DATA="--exclude-table-data=stats_* --exclude-table-data=django_session"
+pg_dump $PG_OPTIONS $PG_EXCLUDE_SCHEMA $PG_EXCLUDE_DATA --dbname $STAGING_DATABASE_URL --format c --file /tmp/dump.pgsql
 pg_restore $PG_OPTIONS --dbname $DATABASE_URL /tmp/dump.pgsql
 psql -d $DATABASE_URL -c 'CREATE EXTENSION IF NOT EXISTS pg_trgm;'
 psql -d $DATABASE_URL -c 'CREATE EXTENSION IF NOT EXISTS unaccent;'
