ba33b7585b25f566ee196475f35cc3e0192356bd
Thibault Jouannic <thibault@miximum.fr>
2021-03-09 10:39:24 +0100

    Add number of aids by the same author in admin form
    Add translations

--- src/aids/admin.py
+++ src/aids/admin.py
@@ -1,7 +1,7 @@
 from functools import reduce
 from operator import and_
 
-from django.db.models import Q, CharField, Value as V
+from django.db.models import Q, CharField, Count, Value as V
 from django.db.models.functions import Concat
 from django.contrib import admin
 from django.contrib.admin.views.main import ChangeList
@@ -189,7 +189,7 @@ class BaseAidAdmin(ImportMixin, ExportActionMixin, admin.ModelAdmin):
 
     filter_vertical = ['categories']  # Overriden in the widget definition
     readonly_fields = [
-        'is_imported', 'import_uniqueid', 'import_data_url',
+        'sibling_aids', 'is_imported', 'import_uniqueid', 'import_data_url',
         'import_share_licence', 'import_last_access', 'date_created',
         'date_updated', 'date_published']
     raw_id_fields = ['generic_aid']
@@ -198,6 +198,7 @@ class BaseAidAdmin(ImportMixin, ExportActionMixin, admin.ModelAdmin):
             'fields': (
                 'name',
                 'slug',
+                'sibling_aids',
                 'in_france_relance',
                 'short_title',
                 'categories',
@@ -280,6 +281,23 @@ class BaseAidAdmin(ImportMixin, ExportActionMixin, admin.ModelAdmin):
             )
         }),
     ]
+
+    def sibling_aids(self, aid):
+        """Number of other (non draft) aids created by the same author."""
+
+        return Aid.objects \
+            .exclude(id=aid.id) \
+            .filter(author=aid.author) \
+            .filter(status__in=('reviewable', 'published')) \
+            .count()
+    sibling_aids.short_description = _('From the same author')
+    sibling_aids.help_text = _('Nb of (non-draft) aids by the same author')
+
+    def get_form(self, request, obj=None, **kwargs):
+        """Set readonly fields help texts."""
+        help_texts = {'sibling_aids': self.sibling_aids.help_text}
+        kwargs.update({'help_texts': help_texts})
+        return super().get_form(request, obj, **kwargs)
 
     def author_name(self, aid):
         return aid.author.full_name
