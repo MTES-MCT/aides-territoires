3ed661b9b4e1dfa29d60ea83a58de7a69a18d8d8
Sylvain <lazybird@users.noreply.github.com>
2021-03-09 12:49:58 +0100

    Change the purpose of export contacts to sendingblue function
    Now updating contacts for CRM project

--- src/accounts/management/commands/export_contacts.py
+++ src/accounts/management/commands/export_contacts.py
@@ -1,7 +1,8 @@
 import requests
 import json
-from django.core.management.base import BaseCommand
+
 from django.conf import settings
+from django.core.management.base import BaseCommand
 
 from accounts.models import User
 
@@ -17,20 +18,41 @@ class Command(BaseCommand):
     help = 'Export all accounts to the newsletter provider'
 
     def handle(self, *args, **options):
-        accounts = User.objects.all()[:5]
+        accounts = User.objects.filter(email__in=['TODO'])
         for account in accounts:
             self.export_account(account)
 
     def export_account(self, user):
         endpoint = 'https://api.sendinblue.com/v3/contacts/'
+        existing_aids = user.aid_set.existing()
+        live_aids = existing_aids.live().order_by('date_published')
+        latest_published = live_aids.last()
+        draft_aids = existing_aids.drafts().order_by('date_created')
+        latest_draft = draft_aids.last()
+        attributes = {
+            'PRENOM': user.first_name,
+            'NOM': user.last_name,
+            'NOMBRE_AIDES_ACTIVES': existing_aids.count(),
+            'DATE_CREATION_COMPTE': user.date_joined.isoformat(),
+            'NOMBRE_AIDES_BROUILLONS': draft_aids.count(),
+            'NOMBRE_AIDES_AFICHEES': live_aids.count()
+        }
+        if latest_draft:
+            draft_date = latest_draft.date_created.isoformat()
+            attributes.update({
+                'DATE_DERNIER_BROUILLON': draft_date
+            })
+        if latest_published:
+            published_date = latest_published.date_published.isoformat()
+            attributes.update({
+                'DATE_DERNIERE_AIDE_PUBLIEE': published_date,
+            })
         data = {
             'email': user.email,
-            'attributes': {
-                'DOUBLE_OPT-IN': 1,
-            },
-            'emailBlacklisted': not user.ml_consent,
-            'listIds': [settings.SIB_LIST_ID],
+            'attributes': attributes,
+            'listIds': [settings.SIB_EXPORT_CONTACTS_LIST_ID],
             'updateEnabled': True,
         }
         requests.post(endpoint, headers=API_HEADERS, data=json.dumps(data))
         self.stdout.write('Exporting {}'.format(user.email))
+        self.stdout.write('Exporting {}'.format(data))
