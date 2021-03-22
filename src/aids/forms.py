6f636454860046973cc4a826486a4b2f2065fcca
SarahRubio <sarahrubio.pro@gmail.com>
2021-03-22 16:16:04 +0100

    overwrite multiwidget get_context

--- src/aids/forms.py
+++ src/aids/forms.py
@@ -53,9 +53,49 @@ class AidTypesWidget(forms.MultiWidget):
             forms.CheckboxSelectMultiple(choices=TECHNICAL_AIDS)
         ]
         super().__init__(self.widgets, *args, **kwargs)
+    
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        if self.is_localized:
+            for widget in self.widgets:
+                widget.is_localized = self.is_localized
+        # value is a list of values, each corresponding to a widget
+        # in self.widgets.
+
+        value = self.decompress(value)
+
+        final_attrs = context['widget']['attrs']
+        input_type = final_attrs.pop('type', None)
+        id_ = final_attrs.get('id')
+        subwidgets = []
+        for i, (widget_name, widget) in enumerate(zip(self.widgets_names, self.widgets)):
+            if input_type is not None:
+                widget.input_type = input_type
+            widget_name = name + widget_name
+            try:
+                widget_value = value[i]
+            except IndexError:
+                widget_value = None
+            if id_:
+                widget_attrs = final_attrs.copy()
+                widget_attrs['id'] = '%s_%s' % (id_, i)
+            else:
+                widget_attrs = final_attrs
+            subwidgets.append(widget.get_context(widget_name, widget_value, widget_attrs)['widget'])
+        context['widget']['subwidgets'] = subwidgets
+        return context
+
 
     def decompress(self, value):
         if value:
+        # If value contains more than 2 values, we have to regroup value[1:]
+        # because multiwidget only accepts a number of values equal to the number
+        # of subwidgets.
+            if value[1]:
+                technical_values = value[1].split()
+                technical_values.extend(value[2:])
+                value = [value[0], technical_values]
+                return value
             return value
         return ['', '']
 
