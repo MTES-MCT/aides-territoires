4615b64ff10ec50ea80e8eddc4c26f1b762858ef
SarahRubio <sarahrubio.pro@gmail.com>
2021-04-01 09:17:00 +0200

    checkstyle

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,7 +4,8 @@ from blog.views import BlogPostList, BlogPostDetail
 
 urlpatterns = [
     path('', BlogPostList.as_view(), name='blog_post_list_view'),
-    path('categorie/<category>', BlogPostList.as_view(), name='blog_post_list_view'),
+    path('categorie/<category>', BlogPostList.as_view(),
+         name='blog_post_list_view'),
 
     path('<slug:slug>/', include([
         path('', BlogPostDetail.as_view(), name='blog_post_detail_view'),
