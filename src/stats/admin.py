bcbd5baa4b4003ca83bd1c123d8d635e37febc61
Thibault Jouannic <thibault@miximum.fr>
2021-04-27 16:07:05 +0200

    Display feedback events in admin

--- src/stats/admin.py
+++ src/stats/admin.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
-from stats.models import (AidViewEvent, AidSearchEvent,
-                          AidMatchProjectEvent, AidEligibilityTestEvent,
-                          Event)
+from stats.models import (AidViewEvent, AidSearchEvent, Event,
+                          AidMatchProjectEvent, AlertFeedbackEvent,
+                          AidEligibilityTestEvent)
 
 
-class AidViewEventAdmin(admin.ModelAdmin):
-    """The model is set to readonly"""
-
-    list_display = ['id', 'aid', 'source', 'date_created']
-    list_filter = ['source']
-
+class ReadOnlyModelAdminMixin:
     def has_add_permission(self, request):
         return False
 
@@ -21,20 +17,17 @@ class AidViewEventAdmin(admin.ModelAdmin):
         return False
 
 
-class AidSearchEventAdmin(admin.ModelAdmin):
+class AidViewEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
     """The model is set to readonly"""
 
     list_display = ['id', 'source', 'results_count', 'date_created']
     list_filter = ['source']
 
-    def has_add_permission(self, request):
-        return False
 
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
+class AidSearchEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
+    """The model is set to readonly"""
+    list_display = ['id', 'source', 'results_count', 'date_created']
+    list_filter = ['source']
 
 
 class AidEligibilityTestEventAdmin(admin.ModelAdmin):
@@ -54,19 +47,22 @@ class AidEligibilityTestEventAdmin(admin.ModelAdmin):
         return False
 
 
-class AidMatchProjectEventAdmin(admin.ModelAdmin):
+class AidMatchProjectEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
     """The model is set to readonly"""
 
     list_display = ['id', 'aid', 'project', 'date_created']
 
-    def has_add_permission(self, request):
-        return False
 
-    def has_change_permission(self, request, obj=None):
-        return False
+class AlertFeedbackEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
+    """The model is set to readonly"""
+    list_display = [
+        'alert', 'email', 'rate', 'feedback', 'date_created']
+    search_fields = ['alert__email']
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    def email(self, obj):
+        return obj.alert.email
+    email.admin_order_field = 'alert__email'
+    email.short_description = _('Email')
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -88,3 +84,4 @@ admin.site.register(AidSearchEvent, AidSearchEventAdmin)
 admin.site.register(AidEligibilityTestEvent, AidEligibilityTestEventAdmin)
 admin.site.register(AidMatchProjectEvent, AidMatchProjectEventAdmin)
 admin.site.register(Event, EventAdmin)
+admin.site.register(AlertFeedbackEvent, AlertFeedbackEventAdmin)
