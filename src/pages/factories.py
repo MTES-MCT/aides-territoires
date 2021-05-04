44c00dee0dd96b1d143be13f930a264098e83aa5
Thibault Jouannic <thibault@miximum.fr>
2021-05-04 15:04:24 +0200

    Filter queryset by minisite

---
+++ src/pages/factories.py
@@ -0,0 +1,12 @@
+import factory
+from factory.django import DjangoModelFactory
+
+from pages.models import Page
+
+
+class PageFactory(DjangoModelFactory):
+    class Meta:
+        model = Page
+
+    title = factory.Faker('company')
+    url = factory.Sequence(lambda n: '/page_{}/'.format(n))
