d2f46534edad013d7faf4bffb7542ef432137097
Thibault Jouannic <thibault@miximum.fr>
2021-05-17 10:49:10 +0200

    Add some missing mixin inheritance

--- src/stats/admin.py
+++ src/stats/admin.py
@@ -18,20 +18,11 @@ class ReadOnlyModelAdminMixin:
         return False
 
 
-class AidContactClickEventAdmin(admin.ModelAdmin):
+class AidContactClickEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
     """The model is set to readonly"""
 
     list_display = ['id', 'aid', 'source', 'date_created']
     list_filter = ['source']
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 
 class AidViewEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
@@ -47,21 +38,12 @@ class AidSearchEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
     list_filter = ['source']
 
 
-class AidEligibilityTestEventAdmin(admin.ModelAdmin):
+class AidEligibilityTestEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
     """The model is set to readonly"""
 
     list_display = ['id', 'aid', 'eligibility_test', 'answer_success',
                     'source', 'date_created']
     list_filter = ['eligibility_test', 'source']
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 
 class AidMatchProjectEventAdmin(ReadOnlyModelAdminMixin, admin.ModelAdmin):
