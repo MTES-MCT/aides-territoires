a68140ea9c39f23ac721437f17e06a7f72911190
Raphael Odini <raphodn@users.noreply.github.com>
2021-05-18 15:01:59 +0200

    Add token display in User admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -2,6 +2,8 @@ from django import forms
 from django.db.models import Count, Q, CharField, Value as V
 from django.db.models.functions import Concat
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -25,6 +27,27 @@ class AuthorFilter(InputFilter):
                         output_field=CharField())) \
                 .filter(Q(author_name__icontains=value))
             return qs
+
+
+class ApiTokenFilter(admin.SimpleListFilter):
+    """Custom admin filter to target users with API Tokens."""
+
+    title = 'Token API'
+    parameter_name = 'has_api_token'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('Yes', _('Yes')),
+            ('No', _('No')),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == 'Yes':
+            return queryset.with_api_token()
+        elif value == 'No':
+            return queryset.filter(auth_token__isnull=True)
+        return queryset
 
 
 class UserAdminForm(forms.ModelForm):
@@ -51,10 +74,12 @@ class UserAdmin(BaseUserAdmin):
     list_editable = ['first_name', 'last_name']
     search_fields = ['email', 'first_name', 'last_name']
     ordering = ['last_name', 'email']
-    list_filter = ['is_superuser', 'is_contributor', 'is_certified',
-                   'ml_consent']
 
-    readonly_fields = ['nb_aids', 'last_login', 'date_joined']
+    list_filter = ['is_superuser', 'is_contributor', 'is_certified',
+                   ApiTokenFilter, 'ml_consent']
+
+    readonly_fields = ['nb_aids', 'api_token', 'last_login', 'date_joined']
+
     fieldsets = (
         (None, {
             'fields': (
@@ -85,6 +110,7 @@ class UserAdmin(BaseUserAdmin):
         (_('Permissions'), {
             'fields': (
                 'is_superuser',
+                'api_token'
             )
         }),
         (_('Misc.'), {
@@ -121,11 +147,21 @@ class UserAdmin(BaseUserAdmin):
     nb_aids.short_description = "Nombre d'aides"
     nb_aids.admin_order_field = 'aid_count'
 
-    def in_mailing_list(self, obj):
-        return obj.ml_consent
+    def in_mailing_list(self, user):
+        return user.ml_consent
     in_mailing_list.short_description = mark_safe(
         _('<abbr title="Newsletter subscriber">NL</abbr>'))
     in_mailing_list.boolean = True
+
+    def api_token(self, user):
+        try:
+            token = user.auth_token
+            return token.key
+        except AttributeError:
+            return format_html(
+                'Non. <a href="{obj_url}">Créer</a>',
+                obj_url=reverse('admin:authtoken_tokenproxy_changelist'))
+    api_token.short_description = "Token API"
 
     def get_changelist_form(self, request, **kwargs):
         return UserAdminForm
