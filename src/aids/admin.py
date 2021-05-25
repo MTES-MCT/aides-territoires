1e0340aed10da8439216566222ba0b7d1d2823c6
SarahRubio <sarahrubio.pro@gmail.com>
2021-05-25 16:31:34 +0200

    checkstyle

--- src/aids/admin.py
+++ src/aids/admin.py
@@ -215,7 +215,7 @@ class BaseAidAdmin(FieldsetsInlineMixin,
         EligibilityTestFilter,
         LiveAidListFilter, AuthorFilter, BackersFilter,
         PerimeterAutocompleteFilter,
-        'programs', 'categories', 'categories__theme',]
+        'programs', 'categories', 'categories__theme']
 
     autocomplete_fields = ['author', 'financers', 'instructors', 'perimeter',
                            'programs']
