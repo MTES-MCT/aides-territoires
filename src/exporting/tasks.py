dbab69990d182a36c423ce42d3103c3f16d6ca4d
Sylvain <lazybird@users.noreply.github.com>
2021-05-27 08:56:52 +0200

    Rename a variable

--- src/exporting/tasks.py
+++ src/exporting/tasks.py
@@ -13,19 +13,19 @@ def export_aids(aids_id_list, author_id, file_format):
     queryset = Aid.objects.filter(id__in=aids_id_list)
     exported_data = AidResource().export(queryset)
     if file_format == 'csv':
-        content_file = ContentFile(exported_data.csv.encode())
+        file_content = ContentFile(exported_data.csv.encode())
     if file_format == 'xlsx':
-        content_file = ContentFile(exported_data.xlsx)
+        file_content = ContentFile(exported_data.xlsx)
     file_name = 'export-aides-'
     file_name += dateformat.format(timezone.now(), 'Y-m-d_H-i-s')
     file_name += f'.{file_format}'
-    file_object = files.File(content_file, name=file_name)
+    file_object = files.File(file_content, name=file_name)
     DataExport.objects.create(
         author_id=author_id,
         exported_file=file_object,
     )
     file_object.close()
-    content_file.close()
+    file_content.close()
 
 
 @app.task
