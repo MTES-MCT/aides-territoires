ec3bc6d689a80e0ce0b856bc9974cc69a274e5e1
SarahRubio <sarahrubio.pro@gmail.com>
2021-05-31 09:55:13 +0200

    add promotionDisplayEvent model

--- src/stats/admin.py
+++ src/stats/admin.py
@@ -4,7 +4,8 @@ from core.admin import pretty_print_readonly_jsonfield
 from stats.models import (AidSearchEvent,
                           AidViewEvent, AidContactClickEvent,
                           AidMatchProjectEvent, AidEligibilityTestEvent,
-                          Event, PromotionClickEvent)
+                          PromotionClickEvent, PromotionDisplayEvent,
+                          Event)
 
 
 class AidViewEventAdmin(admin.ModelAdmin):
@@ -94,6 +95,24 @@ class AidMatchProjectEventAdmin(admin.ModelAdmin):
         return False
 
 
+class PromotionDisplayEventAdmin(admin.ModelAdmin):
+    """The model is set to readonly"""
+
+    list_display = [
+        'id', 'promotion', 'querystring',
+        'source', 'date_created']
+    list_filter = ['source']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 class PromotionClickEventAdmin(admin.ModelAdmin):
     """The model is set to readonly"""
 
@@ -131,5 +150,6 @@ admin.site.register(AidContactClickEvent, AidContactClickEventAdmin)
 admin.site.register(AidSearchEvent, AidSearchEventAdmin)
 admin.site.register(AidEligibilityTestEvent, AidEligibilityTestEventAdmin)
 admin.site.register(AidMatchProjectEvent, AidMatchProjectEventAdmin)
+admin.site.register(PromotionDisplayEvent, PromotionDisplayEventAdmin)
 admin.site.register(PromotionClickEvent, PromotionClickEventAdmin)
 admin.site.register(Event, EventAdmin)
--- src/stats/models.py
+++ src/stats/models.py
@@ -228,6 +228,30 @@ class AidMatchProjectEvent(models.Model):
         verbose_name_plural = _('Aid Match Project Events')
 
 
+class PromotionDisplayEvent(models.Model):
+    promotion = models.ForeignKey(
+        'blog.PromotionPost',
+        verbose_name=_('Promotion Post'),
+        on_delete=models.PROTECT)
+
+    querystring = models.TextField(
+        _('Querystring'),
+        default='')
+
+    source = models.CharField(
+        'Source',
+        max_length=256,
+        blank=True, default='')
+
+    date_created = models.DateTimeField(
+        _('Date created'),
+        default=timezone.now)
+
+    class Meta:
+        verbose_name = _('Événement affichage promotion')
+        verbose_name_plural = _('Événements affichage promotion')
+
+
 class PromotionClickEvent(models.Model):
     promotion = models.ForeignKey(
         'blog.PromotionPost',
