768d4c1295abb9c5add78f64f757089a34264c48
SarahRubio <sarahrubio.pro@gmail.com>
2021-06-10 09:22:02 +0200

    unuse relatedfield for array

--- src/geofr/api/serializers.py
+++ src/geofr/api/serializers.py
@@ -3,20 +3,13 @@ from rest_framework import serializers
 from geofr.models import Perimeter
 
 
-class ZipcodesRelatedField(serializers.StringRelatedField):
-
-    def to_representation(self, value):
-        return f'{value}'
-
-
 class PerimeterSerializer(serializers.ModelSerializer):
 
     id = serializers.CharField(source='id_slug')
     scale = serializers.CharField(source='get_scale_display')
     text = serializers.CharField(source='__str__')
 
-    zipcodes = ZipcodesRelatedField(
-        many=True)
+    zipcodes = serializers.ListField(child=serializers.CharField())
 
     class Meta:
         model = Perimeter
