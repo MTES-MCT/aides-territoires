e89f5f7cb8d0312afa2bef992c10c8fbc74a7e66
Sylvain <lazybird@users.noreply.github.com>
2021-06-24 10:52:04 +0200

    Change the way admin lite page is saved
    Now we save and stay on the change form page

--- src/admin_lite/mixins.py
+++ src/admin_lite/mixins.py
@@ -1,11 +1,2 @@
-from django.shortcuts import redirect
-
-
 class AdminLiteMixin:
     change_form_template = 'admin/admin_lite/change_form.html'
-
-    def response_change(self, request, obj):
-        url = '/'
-        if hasattr(self.model, 'get_absolute_url'):
-            url = obj.get_absolute_url()
-        return redirect(url)
