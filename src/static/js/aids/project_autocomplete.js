45cc88de76c7292dcefd1b718c36cc0409792410
SarahRubio <sarahrubio.pro@gmail.com>
2021-06-28 11:29:21 +0200

    use text-filterif user doesn't select a project option

--- src/static/js/aids/project_autocomplete.js
+++ src/static/js/aids/project_autocomplete.js
@@ -6,9 +6,12 @@ $(document).ready(function () {
         minimumInputLength: 3,
         language: {
             inputTooShort: function() {
+                // If the user enter a value in the autocomplete field input but
+                // doesn't select an option we want to use the value to fill
+                // the #id_text input and pursue the research with the text filter. 
                 $('.select2-search__field').on('input', function() {
                     $('input#id_text').val($('.select2-search__field').val());
-                }) 
+                })
                 return catalog.autocomplete_placeholder; 
             },
         },
@@ -29,7 +32,7 @@ $(document).ready(function () {
 
                   if (data.results.length === 0) {
                     $("#other_project_box").removeClass("d-none");
-                  } 
+                  }
                   return {
                       results: data.results,
                       pagination: {
@@ -41,4 +44,10 @@ $(document).ready(function () {
         theme: 'bootstrap4',
         width: '',
     });
+
+    // If user select a project in the list we erase the value of #id_text input
+    // to search with the project filter instead of the text filter 
+    $('select#id_projects').on('select2:selecting', function() {
+        $('input#id_text').val('');
+    })
 });
