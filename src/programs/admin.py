0972382a4a5b0fe000f62f559e6698fa95fd93ab
Raphael Odini <raphodn@users.noreply.github.com>
2021-08-04 10:11:30 +0200

    Add perimeter field to programs (#737)

--- src/programs/admin.py
+++ src/programs/admin.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib import admin
 from django.db.models import Count
-from django.utils.translation import gettext_lazy as _
 
 from core.forms import RichTextField
 from programs.models import Program
@@ -9,11 +8,54 @@ from upload.settings import TRUMBOWYG_UPLOAD_ADMIN_JS
 
 
 class ProgramAdminForm(forms.ModelForm):
-    description = RichTextField(
-        label=_('Description'))
+    description = RichTextField(label='Description')
 
 
 class ProgramAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'perimeter', 'nb_aids', 'date_created']
+    search_fields = ['name']
+
+    form = ProgramAdminForm
+    prepopulated_fields = {'slug': ('name',)}
+    autocomplete_fields = ['perimeter']
+    readonly_fields = ['nb_aids', 'date_created']
+
+    fieldsets = [
+            ('', {
+                'fields': (
+                    'name',
+                    'slug',
+                    'short_description',
+                    'description',
+                    'logo',
+                    'perimeter',
+                )
+            }),
+            ('SEO', {
+                'fields': (
+                    'meta_title',
+                    'meta_description',
+                )
+            }),
+            ('Données diverses', {
+                'fields': (
+                    'nb_aids',
+                    'date_created',
+                )
+            }),
+        ]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.prefetch_related('aids') \
+               .annotate(aid_count=Count('aids'))
+        return qs
+
+    def nb_aids(self, obj):
+        return obj.aid_count
+    nb_aids.short_description = "Nombre d'aides"
+    nb_aids.admin_order_field = 'aid_count'
 
     class Media:
         css = {
@@ -29,42 +71,6 @@ class ProgramAdmin(admin.ModelAdmin):
             '/static/trumbowyg/dist/langs/fr.js',
             '/static/js/enable_rich_text_editor.js',
         ] + TRUMBOWYG_UPLOAD_ADMIN_JS
-
-    form = ProgramAdminForm
-    list_display = ['name', 'slug', 'nb_aids', 'date_created']
-    search_fields = ['name']
-    prepopulated_fields = {'slug': ('name',)}
-    readonly_fields = ['date_created']
-
-    fieldsets = [
-            ('', {
-                'fields': (
-                    'name',
-                    'slug',
-                    'short_description',
-                    'description',
-                    'logo',
-                    'date_created',
-                )
-            }),
-            (_('SEO'), {
-                'fields': (
-                    'meta_title',
-                    'meta_description',
-                )
-            }),
-        ]
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        qs = qs.prefetch_related('aids') \
-               .annotate(aid_count=Count('aids'))
-        return qs
-
-    def nb_aids(self, obj):
-        return obj.aid_count
-    nb_aids.short_description = "Nombre d'aides"
-    nb_aids.admin_order_field = 'aid_count'
 
 
 admin.site.register(Program, ProgramAdmin)
