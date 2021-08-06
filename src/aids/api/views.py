48d372080d16498c9fc6c17b3d9c7ac23368e4b2
Raphael Odini <raphodn@users.noreply.github.com>
2021-08-06 15:17:51 +0200

    fix merge

--- src/aids/api/views.py
+++ src/aids/api/views.py
@@ -3,19 +3,12 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 
 from rest_framework import viewsets
-<<<<<<< HEAD
-=======
 from rest_framework.views import APIView
->>>>>>> a289e07f... API : ajout d'un endpoint pour lister tous les bénéficiaires (#756)
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 
 from aids.models import Aid
-<<<<<<< HEAD
 from aids.constants import AUDIENCES_GROUPED, TYPES_GROUPED
-=======
-from aids.constants import AUDIENCES_GROUPED
->>>>>>> a289e07f... API : ajout d'un endpoint pour lister tous les bénéficiaires (#756)
 from aids.api.serializers import (
     AidSerializer10, AidSerializer11, AidSerializer12, AidSerializerLatest)
 from aids.api.pagination import AidsPagination
