b71aa8332b47d6b7e0ae0f55f764cc2132c48d5b
Raphael Odini <raphodn@users.noreply.github.com>
2021-08-09 17:19:04 +0200

    Add parameter documentation to Perimeters

--- src/geofr/api/views.py
+++ src/geofr/api/views.py
@@ -2,6 +2,8 @@ from django.db.models import Q
 from django.contrib.postgres.search import TrigramSimilarity
 
 from rest_framework import viewsets, mixins
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 from core.utils import remove_accents
 from geofr.models import Perimeter
@@ -9,6 +11,32 @@ from geofr.api.serializers import PerimeterSerializer, PerimeterScaleSerializer
 
 
 MIN_SEARCH_LENGTH = 1
+
+q_param = openapi.Parameter(
+    'q',
+    openapi.IN_QUERY,
+    description="""
+    Rechercher par nom.
+    Il est possible d'avoir des résultats pertinents avec seulement le début du nom, \
+    ou un nom légerement erroné.
+    
+    Exemples : 'lyon', 'par', 'grenble'
+    """,
+    type=openapi.TYPE_STRING)
+scale_param = openapi.Parameter(
+    'scale',
+    openapi.IN_QUERY,
+    description="""
+    Filtrer par l'échelle.
+    
+    Exemple : 'department'
+    """,
+    type=openapi.TYPE_STRING)
+# is_visible_to_users_param = openapi.Parameter(
+#     'is_visible_to_users',
+#     openapi.IN_QUERY,
+#     description="Afficher les périmètres cachés. Exemple : 'true'",
+#     type=openapi.TYPE_BOOLEAN)
 
 
 class PerimeterViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -44,6 +72,10 @@ class PerimeterViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
             qs = qs.filter(is_visible_to_users=True)
 
         return qs
+
+    @swagger_auto_schema(manual_parameters=[q_param, scale_param])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, args, kwargs)
 
 
 class PerimeterScalesViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
