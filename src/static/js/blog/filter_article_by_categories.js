c74e56d1adb26fa99a1d6f406ba910ebed89668a
SarahRubio <sarahrubio.pro@gmail.com>
2021-08-12 17:23:54 +0200

    create file for blog js

---
+++ src/static/js/blog/filter_article_by_categories.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('#select').on('change', function () {
+        var url = $(this).val(); // get selected value
+        if (url) { // require a URL
+            window.location = url; // redirect
+        }
+        return false;
+    });
+
+})
