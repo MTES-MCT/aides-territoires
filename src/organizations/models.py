ea9713976841833f68eb628e732ee444f9d0f70f
SarahRubio <sarahrubio.pro@gmail.com>
2021-08-23 10:31:46 +0200

    add keys_numbers fields in organizations model

--- src/organizations/models.py
+++ src/organizations/models.py
@@ -26,6 +26,101 @@ class Organization(models.Model):
         base_field=models.CharField(
             max_length=32,
             choices=ORGANIZATION_TYPE))
+
+    address = models.CharField(
+        'Adresse postale',
+        max_length=900,
+        null=True, blank=True)
+    city_name = models.CharField(
+        'Nom de la ville',
+        max_length=256,
+        null=True, blank=True)
+    zip_code = models.PositiveIntegerField(
+        'Code postal',
+        null=True, blank=True)
+
+    siren_code = models.PositiveIntegerField(
+        'Code SIREN',
+        null=True, blank=True)
+    siret_code = models.PositiveIntegerField(
+        'Code SIRET',
+        null=True, blank=True)
+    ape_code = models.PositiveIntegerField(
+        'Code APE',
+        null=True, blank=True)
+
+    inhabitants_number = models.PositiveIntegerField(
+        "Nombre d'habitants",
+        null=True, blank=True)
+    voters_number = models.PositiveIntegerField(
+        'Nombre de votants',
+        null=True, blank=True)
+    corporates_number = models.PositiveIntegerField(
+        "Nombre d'entreprises",
+        null=True, blank=True)
+    associations_number = models.PositiveIntegerField(
+        "Nombre d'associations",
+        null=True, blank=True)
+
+    municipal_roads = models.PositiveIntegerField(
+        "Routes communales (kms)",
+        null=True, blank=True)
+    departmental_roads = models.PositiveIntegerField(
+        "Routes départementales (kms)",
+        null=True, blank=True)
+    tram_roads = models.PositiveIntegerField(
+        "Tramways (kms)",
+        null=True, blank=True)
+    lamppost_number = models.PositiveIntegerField(
+        "Nombre de lampadaires",
+        null=True, blank=True)
+
+    library_number = models.PositiveIntegerField(
+        "Nombre de bibliothèques",
+        null=True, blank=True)
+    medialibrary_number = models.PositiveIntegerField(
+        "Nombre de mediathèques",
+        null=True, blank=True)
+    theater_number = models.PositiveIntegerField(
+        "Nombre de théâtres",
+        null=True, blank=True)
+    museum_number = models.PositiveIntegerField(
+        "Nombre de musées",
+        null=True, blank=True)
+
+    kindergarten_number = models.PositiveIntegerField(
+        "Nombre d'écoles maternelles",
+        null=True, blank=True)
+    primary_school_number = models.PositiveIntegerField(
+        "Nombre d'écoles primaires",
+        null=True, blank=True)
+    middle_school_number = models.PositiveIntegerField(
+        "Nombre de collèges",
+        null=True, blank=True)
+    high_school_number = models.PositiveIntegerField(
+        "Nombre de lycées",
+        null=True, blank=True)
+    university_number = models.PositiveIntegerField(
+        "Nombre d'universités",
+        null=True, blank=True)
+
+    gymnasium_number = models.PositiveIntegerField(
+        "Nombre de gymnases et salles de sport",
+        null=True, blank=True)
+    sports_ground_number = models.PositiveIntegerField(
+        "Nombre de stades et structures extérieures",
+        null=True, blank=True)
+    swimming_pool_number = models.PositiveIntegerField(
+        "Nombre de piscines",
+        null=True, blank=True)
+
+    place_of_worship_number = models.PositiveIntegerField(
+        "Nombre de lieux de cultes",
+        null=True, blank=True)
+    cemetery_number = models.PositiveIntegerField(
+        "Nombre de cimetières",
+        null=True, blank=True)
+
     beneficiaries = models.ManyToManyField(
         'accounts.User',
         verbose_name='Bénéficiaires',
