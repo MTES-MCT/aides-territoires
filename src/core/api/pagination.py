21a84a430b2c27d65f818c55a5be6e11ffe082b3
SarahRubio <60744147+SarahRubio@users.noreply.github.com>
2021-11-05 10:58:57 +0100

    increase max_page api (#809)

--- src/core/api/pagination.py
+++ src/core/api/pagination.py
@@ -4,4 +4,4 @@ from rest_framework.pagination import PageNumberPagination
 class ApiPagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = 'size'
-    max_page_size = 200
+    max_page_size = 1000
