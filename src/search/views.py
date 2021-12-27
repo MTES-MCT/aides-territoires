8c1f004221acdf524eb9d30e64a9d2047c3f5457
SarahRubio <sarahrubio.pro@gmail.com>
2021-12-27 13:30:08 +0100

    get initial values for fields perimeter & targeted-audiences if user is authenticated

--- src/search/views.py
+++ src/search/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import FormView
 
 from search.forms import GeneralSearchForm
+from geofr.models import Perimeter
 
 
 class SearchMixin:
@@ -10,8 +11,29 @@ class SearchMixin:
         return context
 
 
-class GeneralSearch(SearchMixin, FormView):
+class GeneralSearch(FormView):
     """general search form."""
 
     template_name = 'search/general_search.html'
     form_class = GeneralSearchForm
+
+    def get_initial(self):
+        # if user is authenticated
+        # and if user organization type and user organization's perimeter are defined
+        # we pre-populate targeted_audience & perimeter fields
+
+        if self.request.user.is_authenticated:
+            if self.request.user.beneficiary_organization is not None:
+                user_org = self.request.user.beneficiary_organization
+                if user_org.organization_type is not None and user_org.zip_code:
+                    org_zip_code = str(user_org.zip_code).split()
+                    org_perimeter = Perimeter.objects.filter(zipcodes=org_zip_code).first()
+                    # here we check if zipcode is related to an existing perimeter
+                    if org_perimeter:
+                        PERIMETER = org_perimeter.id_slug
+                        TARGETED_AUDIENCES = user_org.organization_type[0]
+                        initial = {
+                            'targeted_audiences': TARGETED_AUDIENCES,
+                            'perimeter': PERIMETER,
+                        }
+                        return initial
