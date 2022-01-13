e1c1a3ee16e9be9174873e6e4f0bb067b8818386
SarahRubio <60744147+SarahRubio@users.noreply.github.com>
2022-01-13 15:05:33 +0100

    fix export projects options (#861)

--- src/projects/resources.py
+++ src/projects/resources.py
@@ -4,37 +4,22 @@ from import_export import fields, resources
 from import_export.widgets import ManyToManyWidget
 
 from projects.models import Project
-from categories.models import Category
-
-PROJECTS_BOOLEAN_FIELDS = ['is_suggested']
-PROJECTS_IMPORT_EXCLUDE_FIELDS = ['slug', 'date_created']
+from organizations.models import Organization
 
 
 class ProjectResource(resources.ModelResource):
 
-    categories = fields.Field(
-        column_name='categories',
-        attribute='categories',
-        widget=ManyToManyWidget(Category, field='name')
+    organizations = fields.Field(
+        column_name='organizations',
+        attribute='organizations',
+        widget=ManyToManyWidget(Organization, field='name')
     )
 
     class Meta:
         model = Project
         import_id_fields = ('slug',)
-        export_order = ('name', 'description', 'categories')
-
-    def get_import_fields(self):
-        return [field for field in self.get_fields() if field.column_name not in PROJECTS_IMPORT_EXCLUDE_FIELDS]  # noqa
-
-    def before_import_row(self, row, **kwargs):
-
-        # remove keys
-        for key in PROJECTS_IMPORT_EXCLUDE_FIELDS:
-            if key in row:
-                del row[key]
-        # add/set keys
-        row['is_suggested'] = False
-        row['status'] = "published"
+        fields = ('name', 'organizations', 'description', 'date_created')
+        export_order = ('name', 'organizations', 'description')
 
     def export_field(self, field, obj):
         """override export_field() to translate field values."""
