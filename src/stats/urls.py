f268fb32c9015f90824d5e7ee24236b56438534e
SarahRubio <sarahrubio.pro@gmail.com>
2022-01-17 19:07:38 +0100

    remove unwanted space in urls.py

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -5,5 +5,4 @@ from stats.views import StatsView, DashboardView
 urlpatterns = [
     path('', StatsView.as_view(), name='stats_view'),
     path('dashboard/', DashboardView.as_view(), name='dashboard_view'),
-
 ]
