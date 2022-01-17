2b0981032c3957225acd707b672a8e57ba847966
pocmaker42 <thierryv@gmail.com>
2022-01-17 16:16:21 +0100

    initiate dashboard

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from stats.views import StatsView
+from stats.views import StatsView, DashboardView
 
 urlpatterns = [
     path('', StatsView.as_view(), name='stats_view'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard_view'),
+
 ]
--- src/stats/views.py
+++ src/stats/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from django.db.models import Sum
 
 from aids.models import Aid
+from accounts.models import User
 from backers.models import Backer
 from stats.models import AidViewEvent, Event
 
@@ -18,7 +19,38 @@ class StatsView(TemplateView):
         aids_qs = Aid.objects.live()
         context['nb_live_aids'] = aids_qs.count()
 
-        one_week_ago = timezone.now() - timedelta(days=8)
+        one_week_ago = timezone.now() - timedelta(days=7)
+        viewed_aids_qs = AidViewEvent.objects \
+            .filter(date_created__gte=one_week_ago)
+        context['nb_viewed_aids'] = viewed_aids_qs.count()
+
+        alerts_qs = Event.objects \
+            .filter(category='alert', event='sent') \
+            .aggregate(nb_sent_alerts=Sum('value'))
+        context['nb_sent_alerts'] = alerts_qs['nb_sent_alerts']
+
+        active_backers = Backer.objects.has_financed_aids()
+        context['nb_backers'] = active_backers.count()
+
+        return context
+
+
+class DashboardView(TemplateView):
+    template_name = 'stats/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        one_week_ago = timezone.now() - timedelta(days=7)
+        #print ("one_week_ago", str(one_week_ago))
+
+        accounts_qs = User.objects \
+            .exclude(beneficiary_organization_id=None)
+        context['nb_accounts'] = accounts_qs.count()
+
+        aids_qs = Aid.objects.live()
+        context['nb_live_aids'] = aids_qs.count()
+
         viewed_aids_qs = AidViewEvent.objects \
             .filter(date_created__gte=one_week_ago)
         context['nb_viewed_aids'] = viewed_aids_qs.count()
