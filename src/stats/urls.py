aed7a41d8083a92857ee2e3f8dc28682d9413efc
SarahRubio <sarahrubio.pro@gmail.com>
2022-01-24 21:31:34 +0100

    create users_stats url

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from stats.views import StatsView, DashboardView
+from stats.views import StatsView, DashboardView, UsersStatsView
 
 urlpatterns = [
     path('', StatsView.as_view(), name='stats_view'),
     path('dashboard/', DashboardView.as_view(), name='dashboard_view'),
+    path('utilisateurs-stats/', UsersStatsView.as_view(), name='users_stats'),
 ]
