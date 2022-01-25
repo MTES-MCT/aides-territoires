4e2fb59f591865f4c074f4fdc3f65ce236c1857f
SarahRubio <sarahrubio.pro@gmail.com>
2022-01-25 10:19:52 +0100

    create projects-stats url

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from stats.views import StatsView, DashboardView, UsersStatsView
+from stats.views import StatsView, DashboardView, UsersStatsView, ProjectsStatsView
 
 urlpatterns = [
     path('', StatsView.as_view(), name='stats_view'),
     path('dashboard/', DashboardView.as_view(), name='dashboard_view'),
-    path('utilisateurs-stats/', UsersStatsView.as_view(), name='users_stats'),
+    path('stats-utilisateurs/', UsersStatsView.as_view(), name='users_stats'),
+    path('stats-projets/', ProjectsStatsView.as_view(), name='projects_stats'),
 ]
