38036359ac060b3cf053b376a7decd89930b668e
SarahRubio <sarahrubio.pro@gmail.com>
2022-01-25 13:11:55 +0100

    create organizations-stats url

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from stats.views import StatsView, DashboardView, UsersStatsView, ProjectsStatsView
+from stats.views import StatsView, DashboardView, UsersStatsView, ProjectsStatsView, OrganizationsStatsView
 
 urlpatterns = [
     path('', StatsView.as_view(), name='stats_view'),
     path('dashboard/', DashboardView.as_view(), name='dashboard_view'),
     path('stats-utilisateurs/', UsersStatsView.as_view(), name='users_stats'),
     path('stats-projets/', ProjectsStatsView.as_view(), name='projects_stats'),
+    path('stats-structures/', OrganizationsStatsView.as_view(), name='organizations_stats'),
 ]
