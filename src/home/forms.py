8435eca63a88fe86bf5fce3e7928dc450e1c771f
SarahRubio <sarahrubio.pro@gmail.com>
2022-02-04 14:03:08 +0100

    add contact link if post.author.is_superuser

--- src/home/forms.py
+++ src/home/forms.py
@@ -9,6 +9,7 @@ class ContactForm(forms.Form):
         ('contact_add', "Je veux en savoir plus sur l'ajout de mes aides sur la plateforme"),
         ('contact_com', 'Je souhaite communiquer sur Aides-territoires'),
         ('contact_question', "J'ai une question par rapport à une aide ou mon projet"),
+        ('contact_blog', "J’ai une question concernant le blog"),
         ('contact_api', 'Je souhaite utiliser les données d’Aides-territoires / API'),
         ('contact_tech', "J'ai un problème technique sur le site"),
         ('contact_other', 'Autres')
