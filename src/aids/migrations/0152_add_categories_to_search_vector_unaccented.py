f9ebdc2d3b604467fe8d7ca6ca1ddb0bd81dd25d
SarahRubio <sarahrubio.pro@gmail.com>
2022-02-17 17:19:22 +0100

    add missing )

--- src/aids/migrations/0152_add_categories_to_search_vector_unaccented.py
+++ src/aids/migrations/0152_add_categories_to_search_vector_unaccented.py
@@ -11,19 +11,20 @@ def update_search_vector_unaccented(apps, schema_editor):
         categories = aid.categories.all()
         search_vector_unaccented = \
             SearchVector(Value(aid.name), weight='A', config='french_unaccent') + \
-            SearchVector(Value(aid.name_initial, weight='A', config='french_unaccent') + \
+            SearchVector(Value(aid.name_initial), weight='A', config='french_unaccent') + \
             SearchVector(Value(aid.description), weight='B', config='french_unaccent') + \
-            SearchVector(Value(aid.project_examples, weight='B', config='french_unaccent') + \
+            SearchVector(Value(aid.project_examples), weight='B', config='french_unaccent') + \
             SearchVector(Value(aid.eligibility), weight='D', config='french_unaccent') + \
             SearchVector(Value(' '.join(backer.name for backer in backers)), weight='D', config='french_unaccent') + \
             SearchVector(Value(' '.join(category.name for category in categories)), weight='D', config='french_unaccent')
         aid.search_vector_unaccented = search_vector_unaccented
         aid.save()
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
-            ('aids', '0151_aid_author_notification'),
+        ('aids', '0151_aid_author_notification'),
     ]
 
     operations = [
