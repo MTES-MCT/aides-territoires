330e95fbda4311d49608fff80cda57afd00961d1
SarahRubio <sarahrubio.pro@gmail.com>
2022-02-17 15:54:24 +0100

    fix syntaxError

--- src/aids/migrations/0152_add_categories_to_search_vector_unaccented.py
+++ src/aids/migrations/0152_add_categories_to_search_vector_unaccented.py
@@ -15,7 +15,7 @@ def update_search_vector_unaccentedr(apps, schema_editor):
             SearchVector(Value(aid.description), weight='B', config='french_unaccent') + \
             SearchVector(Value(aid.project_examples, weight='B', config='french_unaccent') + \
             SearchVector(Value(aid.eligibility), weight='D', config='french_unaccent') + \
-            SearchVector(Value(' '.join(backer.name for backer in backers)), weight='D', config='french_unaccent')
+            SearchVector(Value(' '.join(backer.name for backer in backers)), weight='D', config='french_unaccent') + \
             SearchVector(Value(' '.join(category.name for category in categories)), weight='D', config='french_unaccent')
         aid.search_vector_unaccented = search_vector_unaccented
         aid.save()
