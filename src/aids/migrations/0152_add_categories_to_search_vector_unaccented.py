a0299a145447fc2c0d4a65a7215f36294a96570c
SarahRubio <sarahrubio.pro@gmail.com>
2022-02-17 17:55:12 +0100

    use str for backer & categories fields

--- src/aids/migrations/0152_add_categories_to_search_vector_unaccented.py
+++ src/aids/migrations/0152_add_categories_to_search_vector_unaccented.py
@@ -15,8 +15,8 @@ def update_search_vector_unaccented(apps, schema_editor):
             SearchVector(Value(aid.description, output_field=models.CharField()), weight='B', config='french_unaccent') + \
             SearchVector(Value(aid.project_examples, output_field=models.CharField()), weight='B', config='french_unaccent') + \
             SearchVector(Value(aid.eligibility, output_field=models.CharField()), weight='D', config='french_unaccent') + \
-            SearchVector(Value(' '.join(backer.name for backer in backers), output_field=models.CharField()), weight='D', config='french_unaccent') + \
-            SearchVector(Value(' '.join(category.name for category in categories), output_field=models.CharField()), weight='D', config='french_unaccent')
+            SearchVector(Value(' '.join(str(backer.name) for backer in backers), output_field=models.CharField()), weight='D', config='french_unaccent') + \
+            SearchVector(Value(' '.join(str(category.name) for category in categories), output_field=models.CharField()), weight='D', config='french_unaccent')
         aid.search_vector_unaccented = search_vector_unaccented
         aid.save()
 
