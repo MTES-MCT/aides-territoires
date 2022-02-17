b785a46dcc2d65bed04aa845f43e6e90d698c023
SarahRubio <sarahrubio.pro@gmail.com>
2022-02-17 17:36:04 +0100

    add output_field

--- src/aids/migrations/0152_add_categories_to_search_vector_unaccented.py
+++ src/aids/migrations/0152_add_categories_to_search_vector_unaccented.py
@@ -10,13 +10,13 @@ def update_search_vector_unaccented(apps, schema_editor):
         backers = aid.financers.all() | aid.instructors.all()
         categories = aid.categories.all()
         search_vector_unaccented = \
-            SearchVector(Value(aid.name), weight='A', config='french_unaccent') + \
-            SearchVector(Value(aid.name_initial), weight='A', config='french_unaccent') + \
-            SearchVector(Value(aid.description), weight='B', config='french_unaccent') + \
-            SearchVector(Value(aid.project_examples), weight='B', config='french_unaccent') + \
-            SearchVector(Value(aid.eligibility), weight='D', config='french_unaccent') + \
-            SearchVector(Value(' '.join(backer.name for backer in backers)), weight='D', config='french_unaccent') + \
-            SearchVector(Value(' '.join(category.name for category in categories)), weight='D', config='french_unaccent')
+            SearchVector(Value(aid.name, output_field=models.CharField()), weight='A', config='french_unaccent') + \
+            SearchVector(Value(aid.name_initial, output_field=models.CharField()), weight='A', config='french_unaccent') + \
+            SearchVector(Value(aid.description, output_field=models.CharField()), weight='B', config='french_unaccent') + \
+            SearchVector(Value(aid.project_examples, output_field=models.CharField()), weight='B', config='french_unaccent') + \
+            SearchVector(Value(aid.eligibility, output_field=models.CharField()), weight='D', config='french_unaccent') + \
+            SearchVector(Value(' '.join(backer.name for backer in backers), output_field=models.CharField()), weight='D', config='french_unaccent') + \
+            SearchVector(Value(' '.join(category.name for category in categories), output_field=models.CharField()), weight='D', config='french_unaccent')
         aid.search_vector_unaccented = search_vector_unaccented
         aid.save()
 
