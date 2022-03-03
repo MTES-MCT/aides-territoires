b1b48325ee95cd4bbc48c45408479d4471c4a79c
Ash Crow <sylvain@ashtree.eu>
2022-03-03 16:31:54 +0100

    Finished the departement pages (view, templates, js)

--- src/static/js/map/department_filter.js
+++ src/static/js/map/department_filter.js
@@ -1,29 +1,32 @@
-department_filter = function (return_page) {
-
-  /* go to the map for the selected department */
-  $( "#select-department" ).change(function() {
-      window.location = window.location.origin + "/cartographie/" + $( this ).val() + return_page;
-  });
-
-  /* use the org filter */
-  $( "#select-organization" ).change(function() {
-      let page_url = window.location.href.split('?')[0];
-      let audience = $( this ).val();
-      if (audience) {
-          window.location = page_url + "?target_audience=" + $( this ).val();
-      } else {
-          window.location = page_url;
-      }
-  });
-
-  /* use the aid_type filter */
-  $( "#select-aid-type" ).change(function() {
-    let page_url = window.location.href.split('?')[0];
-    let audience = $( this ).val();
-    if (audience) {
-        window.location = page_url + "?aid_type=" + $( this ).val();
-    } else {
-        window.location = page_url;
+set_param_value = function (param, value) {
+    /* update or remove a GET parameter from the current URL */
+    if ('URLSearchParams' in window) {
+        let searchParams = new URLSearchParams(window.location.search);
+        if (value) {
+            searchParams.set(param, value);
+        } else {
+            searchParams.delete(param);
+        }
+        window.location.search = searchParams.toString();
     }
-});
-}
+};
+
+department_filter = function (return_page) {
+    /* go to the map for the selected department */
+    $("#select-department").change(function () {
+        let new_url = window.location.origin + "/cartographie/" + $(this).val() + return_page;
+        window.location.replace(new_url + window.location.search);
+    });
+
+    /* use the org filter */
+    $("#select-organization").change(function () {
+        let audience = $(this).val();
+        set_param_value("target_audience", audience);
+    });
+
+    /* use the aid_type filter */
+    $("#select-aid-type").change(function () {
+        let aid_type = $(this).val();
+        set_param_value("aid_type", aid_type);
+    });
+};
