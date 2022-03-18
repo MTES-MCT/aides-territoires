8779cb1a7498f3f6a96a9b1dae77a307d5209ab7
SarahRubio <sarahrubio.pro@gmail.com>
2022-03-18 18:31:05 +0100

    resolve migrations conflict

--- src/aids/migrations/0154_create_import_raw_object_temp.py
+++ src/aids/migrations/0154_create_import_raw_object_temp.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('aids', '0151_aid_author_notification'),
+        ('aids', '0153_alter_aid_targeted_audiences'),
     ]
 
     operations = [
