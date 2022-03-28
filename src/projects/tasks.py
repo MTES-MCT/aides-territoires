9a4d956597cecf9909acfddb6b18ffb533386981
SarahRubio <60744147+SarahRubio@users.noreply.github.com>
2022-03-28 12:27:25 +0200

    Supprimer un projet via une modale et notifier la suppression aux membres de l’organisation (#921)
    * use modal to delete project from list
    
    * make checkstyle
    
    * change email subject
    
    * add project name in email subject
    
    * remove disable button function

---
+++ src/projects/tasks.py
@@ -0,0 +1,36 @@
+from django.template.loader import render_to_string
+from django.conf import settings
+
+from core.celery import app
+from accounts.models import User
+from emails.utils import send_email
+
+
+@app.task
+def send_project_deleted_email(user_email, project_name,
+                               eraser_name, body_template='emails/project_deleted.txt'):
+    """Send a notification email to all organization's users.
+
+    The email notify users that a project has been deleted.
+
+    """
+    try:
+        user = User.objects.get(email=user_email)
+    except User.DoesNotExist:
+        # In case we could not find any valid user with the given email
+        # we don't raise any exception, because we can't give any hints
+        # about whether or not any particular email has an account
+        # on our site.
+        return
+
+    email_body = render_to_string(body_template, {
+        'user_name': user.full_name,
+        'eraser_name': eraser_name,
+        'project_name': project_name})
+    send_email(
+        subject=f"Suppression du projet {project_name}",
+        body=email_body,
+        recipient_list=[user.email],
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        tags=['connexion', settings.ENV_NAME],
+        fail_silently=False)
