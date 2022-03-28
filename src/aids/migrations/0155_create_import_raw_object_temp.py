f8aaee70ffe636ae64c2986d123f7152bf4f6c73
SarahRubio <sarahrubio.pro@gmail.com>
2022-03-28 14:07:15 +0200

    rebase migration

--- src/aids/migrations/0155_create_import_raw_object_temp.py
+++ src/aids/migrations/0155_create_import_raw_object_temp.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('aids', '0153_alter_aid_targeted_audiences'),
+        ('aids', '0154_alter_aid_date_updated'),
     ]
 
     operations = [
