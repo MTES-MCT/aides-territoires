8dbbb2bc81bc4c96f85673a2b77783632f6a427c
Ash Crow <sylvain@ashtree.eu>
2022-04-20 16:41:49 +0200

    Moved logic to a service

--- src/geofr/management/commands/populate_scots.py
+++ src/geofr/management/commands/populate_scots.py
@@ -1,66 +1,13 @@
 from django.core.management.base import BaseCommand
-import pandas
 
-from geofr.models import Perimeter
-from geofr.utils import attach_perimeters
+from geofr.services.populate_scots import populate_scots
 
 
 class Command(BaseCommand):
-    """Import the list of SCoTs.
-
-    Our data source comes from the DGALN.
-    https://www.data.gouv.fr/fNr/datasets/schema-de-coherence-territoriale-scot-donnees-sudocuh-dernier-etat-des-lieux-annuel-au-31-decembre-2021/
-
-    """
-
-    DATA_URL = (
-        "https://www.data.gouv.fr/fr/datasets/r/6e0cfffc-803d-4394-8461-af6e47795c19"
-    )
-    SHEET_NAME = "Communes Scot"
+    """Import the list of SCoTs."""
 
     def handle(self, *args, **options):
-        scots = {}
-        nb_created = 0
-        nb_updated = 0
-
-        # Parse the file
-        df = pandas.read_excel(self.DATA_URL, sheet_name=self.SHEET_NAME, header=2)
-
-        for _index, row in df.iterrows():
-            scot_id = row["id SCoT"]
-            scot_name = row["SCoT"]
-            insee_code = row["Commune"]
-
-            # Exclude the credit line at the end ("Réalisation : [...]")
-            if isinstance(scot_id, int):
-                if scot_id not in scots:
-                    scots[scot_id] = {"name": scot_name, "communes": []}
-
-                scots[scot_id]["communes"].append(insee_code)
-
-        for scot_id in scots.keys():
-
-            # id is just an integer, we use a custom code to make it unique
-            scot_code = "SCOT-{}".format(scot_id)
-            scot_name = scots[scot_id]["name"]
-
-            # Create the scot perimeter
-            scot, created = Perimeter.objects.update_or_create(
-                scale=Perimeter.SCALES.adhoc,
-                code=scot_code,
-                defaults={
-                    "name": scot_name,
-                },
-            )
-            if created:
-                nb_created += 1
-            else:
-                nb_updated += 1
-
-            # Link the scot with the related communes
-            codes = scots[scot_id]["communes"]
-            attach_perimeters(scot, codes)
-
-        self.stdout.write(
-            self.style.SUCCESS(f"{nb_created} scots created, {nb_updated} updated.")
-        )
+        result = populate_scots()
+        self.stdout.write(self.style.SUCCESS(
+            f"{result['created']} created, {result['updated']} updated."
+        ))
---
+++ src/geofr/services/populate_scots.py
@@ -0,0 +1,63 @@
+from django.core.management.base import BaseCommand
+import pandas
+
+from geofr.models import Perimeter
+from geofr.utils import attach_perimeters
+from django.db import transaction
+
+
+"""
+Our data source comes from the DGALN.
+https://www.data.gouv.fr/fNr/datasets/schema-de-coherence-territoriale-scot-donnees-sudocuh-dernier-etat-des-lieux-annuel-au-31-decembre-2021/
+"""
+DATA_URL = "https://www.data.gouv.fr/fr/datasets/r/6e0cfffc-803d-4394-8461-af6e47795c19"
+SHEET_NAME = "Communes Scot"
+
+@transaction.atomic
+def populate_scots() -> dict:
+    """
+    Import the list of SCoTs.
+    """
+    scots = {}
+    nb_created = 0
+    nb_updated = 0
+
+    # Parse the file
+    df = pandas.read_excel(DATA_URL, sheet_name=SHEET_NAME, header=2)
+
+    for _index, row in df.iterrows():
+        scot_id = row["id SCoT"]
+        scot_name = row["SCoT"]
+        insee_code = row["Commune"]
+
+        # Exclude the credit line at the end ("Réalisation : [...]")
+        if isinstance(scot_id, int):
+            if scot_id not in scots:
+                scots[scot_id] = {"name": scot_name, "communes": []}
+
+            scots[scot_id]["communes"].append(insee_code)
+
+    for scot_id in scots.keys():
+
+        # id is just an integer, we use a custom code to make it unique
+        scot_code = "SCOT-{}".format(scot_id)
+        scot_name = scots[scot_id]["name"]
+
+        # Create the scot perimeter
+        scot, created = Perimeter.objects.update_or_create(
+            scale=Perimeter.SCALES.adhoc,
+            code=scot_code,
+            defaults={
+                "name": scot_name,
+            },
+        )
+        if created:
+            nb_created += 1
+        else:
+            nb_updated += 1
+
+        # Link the scot with the related communes
+        codes = scots[scot_id]["communes"]
+        attach_perimeters(scot, codes)
+
+    return {"created": nb_created, "updated": nb_updated}
