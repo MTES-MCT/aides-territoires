47d033ebea16818cf6559d11f75a8a2ee7652137
Ash Crow <sylvain@ashtree.eu>
2022-04-22 11:15:11 +0200

    black on the files where checkstyle failed

--- src/geofr/management/commands/populate_regions.py
+++ src/geofr/management/commands/populate_regions.py
@@ -2,11 +2,14 @@ from django.core.management.base import BaseCommand
 
 from geofr.services.populate import populate_regions
 
+
 class Command(BaseCommand):
     """Import the list of all regions."""
 
     def handle(self, *args, **options):
         result = populate_regions()
-        self.stdout.write(self.style.SUCCESS(
-            f"{result['created']} created, {result['updated']} updated."
-        ))
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"{result['created']} created, {result['updated']} updated."
+            )
+        )
