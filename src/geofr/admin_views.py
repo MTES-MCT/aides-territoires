b622d30d3c4392270d09799024e0a16bf6f72745
Sylvain Boissel <sylvain@ashtree.eu>
2022-04-26 10:12:31 +0200

    Permettre d'ajouter des listes d'EPCI avec leur code Siren (#948)
    * [geofr/Perimeter import] Add an option to upload lists of EPCI codes
    
    * renamed variable for clarity

--- src/geofr/admin_views.py
+++ src/geofr/admin_views.py
@@ -5,23 +5,26 @@ from braces.views import MessageMixin
 
 from geofr.models import Perimeter
 from geofr.forms import PerimeterUploadForm, PerimeterCombineForm
-from geofr.utils import (attach_perimeters_check, extract_perimeters_from_file,
-                         attach_epci_perimeters,
-                         combine_perimeters)
+from geofr.utils import (
+    attach_perimeters_check,
+    extract_perimeters_from_file,
+    attach_epci_perimeters,
+    combine_perimeters,
+)
 
 import logging
 
-logger = logging.getLogger('console_log')
+logger = logging.getLogger("console_log")
 logger.setLevel(logging.DEBUG)
 
 
 class PerimeterUpload(MessageMixin, SingleObjectMixin, FormView):
     """Gets a list of city codes and update the corresponding perimeters."""
 
-    template_name = 'admin/perimeter_upload.html'
-    pk_url_kwarg = 'object_id'
-    context_object_name = 'perimeter'
-    success_url = reverse_lazy('admin:geofr_perimeter_changelist')
+    template_name = "admin/perimeter_upload.html"
+    pk_url_kwarg = "object_id"
+    context_object_name = "perimeter"
+    success_url = reverse_lazy("admin:geofr_perimeter_changelist")
     form_class = PerimeterUploadForm
 
     def get(self, request, *args, **kwargs):
@@ -39,28 +42,40 @@ class PerimeterUpload(MessageMixin, SingleObjectMixin, FormView):
 
     def form_valid(self, form):
         current_perimeter = self.get_object()
-        perimeter_type = form.cleaned_data['perimeter_type']
+        perimeter_type = form.cleaned_data["perimeter_type"]
 
-        if perimeter_type == 'city_code':
+        if perimeter_type == "city_code":
             # Fetch the list of commune perimeters from the uploaded file
             # The list should be error-free (cleaned in PerimeterUploadForm)
             city_codes = extract_perimeters_from_file(
-                form.cleaned_data['city_code_list'])
+                form.cleaned_data["city_code_list"]
+            )
 
             result = attach_perimeters_check(
-                current_perimeter,
-                city_codes,
-                self.request.user,
-                logger)
+                current_perimeter, city_codes, self.request.user, logger
+            )
 
-        elif perimeter_type == 'epci_name':
+        elif perimeter_type == "epci_name":
             # Fetch the list of EPCI perimeters from the uploaded file
             # The list should be error-free (cleaned in PerimeterUploadForm)
             epci_names = extract_perimeters_from_file(
-                form.cleaned_data['epci_name_list'])
-            result = attach_epci_perimeters(current_perimeter, epci_names, self.request.user)
+                form.cleaned_data["epci_name_list"]
+            )
+            result = attach_epci_perimeters(
+                current_perimeter, epci_names, self.request.user, "names"
+            )
 
-        if result['method'] == 'delayed import':
+        elif perimeter_type == "epci_code":
+            # Fetch the list of EPCI perimeters from the uploaded file
+            # The list should be error-free (cleaned in PerimeterUploadForm)
+            epci_codes = extract_perimeters_from_file(
+                form.cleaned_data["epci_code_list"]
+            )
+            result = attach_epci_perimeters(
+                current_perimeter, epci_codes, self.request.user, "codes"
+            )
+
+        if result["method"] == "delayed import":
             msg = "Votre périmètre est en cours de création. \
                 Veuillez s’il vous plaît contacter l’équipe dev."
         else:
@@ -72,9 +87,9 @@ class PerimeterUpload(MessageMixin, SingleObjectMixin, FormView):
 class PerimeterCombine(MessageMixin, SingleObjectMixin, FormView):
     """Create a new perimeter by combining other perimeters."""
 
-    template_name = 'admin/perimeter_combine.html'
-    pk_url_kwarg = 'object_id'
-    context_object_name = 'perimeter'
+    template_name = "admin/perimeter_combine.html"
+    pk_url_kwarg = "object_id"
+    context_object_name = "perimeter"
     form_class = PerimeterCombineForm
 
     def get(self, request, *args, **kwargs):
@@ -91,17 +106,19 @@ class PerimeterCombine(MessageMixin, SingleObjectMixin, FormView):
 
     def get_success_url(self):
         return reverse_lazy(
-            'admin:geofr_perimeter_change', args=[self.kwargs['object_id']]
+            "admin:geofr_perimeter_change", args=[self.kwargs["object_id"]]
         )
 
     def form_valid(self, form):
         current_perimeter = self.get_object()
-        add_perimeters = form.cleaned_data['add_perimeters']
-        rm_perimeters = form.cleaned_data['rm_perimeters']
+        add_perimeters = form.cleaned_data["add_perimeters"]
+        rm_perimeters = form.cleaned_data["rm_perimeters"]
         city_codes = list(combine_perimeters(add_perimeters, rm_perimeters))
-        result = attach_perimeters_check(current_perimeter, city_codes, self.request.user)
+        result = attach_perimeters_check(
+            current_perimeter, city_codes, self.request.user
+        )
 
-        if result['method'] == 'delayed import':
+        if result["method"] == "delayed import":
             msg = "Votre périmètre est en cours de création. \
                 Veuillez s’il vous plaît contacter l’équipe dev."
         else:
--- src/geofr/tests/test_utils.py
+++ src/geofr/tests/test_utils.py
@@ -1,9 +1,14 @@
 import pytest
 
 from geofr.models import Perimeter
-from geofr.utils import (attach_perimeters_check, department_from_zipcode, is_overseas,
-                         get_all_related_perimeter_ids,
-                         attach_perimeters, attach_epci_perimeters)
+from geofr.utils import (
+    attach_perimeters_check,
+    department_from_zipcode,
+    is_overseas,
+    get_all_related_perimeter_ids,
+    attach_perimeters,
+    attach_epci_perimeters,
+)
 from geofr.factories import PerimeterFactory
 
 
@@ -11,114 +16,108 @@ pytestmark = pytest.mark.django_db
 
 
 def test_department_from_zipcode():
-    assert department_from_zipcode('34110') == '34'
-    assert department_from_zipcode('27370') == '27'
-    assert department_from_zipcode('97200') == '972'
-    assert department_from_zipcode('97414') == '974'
+    assert department_from_zipcode("34110") == "34"
+    assert department_from_zipcode("27370") == "27"
+    assert department_from_zipcode("97200") == "972"
+    assert department_from_zipcode("97414") == "974"
 
 
 def test_is_overseas():
-    assert not is_overseas('34110')
-    assert not is_overseas('27370')
-    assert is_overseas('97200')
-    assert is_overseas('97414')
+    assert not is_overseas("34110")
+    assert not is_overseas("27370")
+    assert is_overseas("97200")
+    assert is_overseas("97414")
 
 
 def test_get_all_related_perimeter_ids(perimeters):
     """Finding related perimeters works as expected."""
 
-    related_perimeters_france = get_all_related_perimeter_ids(perimeters['herault'].id)  # noqa
+    related_perimeters_france = get_all_related_perimeter_ids(
+        perimeters["herault"].id
+    )  # noqa
 
-    assert {'id': perimeters['france'].id} in related_perimeters_france
-    assert {'id': perimeters['montpellier'].id} in related_perimeters_france
-    assert {'id': perimeters['aveyron'].id} not in related_perimeters_france
+    assert {"id": perimeters["france"].id} in related_perimeters_france
+    assert {"id": perimeters["montpellier"].id} in related_perimeters_france
+    assert {"id": perimeters["aveyron"].id} not in related_perimeters_france
 
 
 def test_attach_perimeters(perimeters):
     """Attaching perimeters works as expected."""
 
-    adhoc = PerimeterFactory(
-        name='Communes littorales',
-        scale=Perimeter.SCALES.adhoc)
-    attach_perimeters(
-        adhoc,
-        ['34333', '97209'])  # Vic-la-gardiole, Fort-de-France
+    adhoc = PerimeterFactory(name="Communes littorales", scale=Perimeter.SCALES.adhoc)
+    attach_perimeters(adhoc, ["34333", "97209"])  # Vic-la-gardiole, Fort-de-France
 
-    assert adhoc in perimeters['vic'].contained_in.all()
-    assert adhoc in perimeters['herault'].contained_in.all()
-    assert adhoc in perimeters['occitanie'].contained_in.all()
-    assert adhoc in perimeters['métropole'].contained_in.all()
-    assert adhoc in perimeters['outre-mer'].contained_in.all()
+    assert adhoc in perimeters["vic"].contained_in.all()
+    assert adhoc in perimeters["herault"].contained_in.all()
+    assert adhoc in perimeters["occitanie"].contained_in.all()
+    assert adhoc in perimeters["métropole"].contained_in.all()
+    assert adhoc in perimeters["outre-mer"].contained_in.all()
 
     # Make sure perimeter does not contain itself
     assert adhoc not in adhoc.contained_in.all()
 
     # Make sure france and europe are not contained in the adhoc perimeter
-    assert adhoc not in perimeters['france'].contained_in.all()
-    assert adhoc not in perimeters['europe'].contained_in.all()
+    assert adhoc not in perimeters["france"].contained_in.all()
+    assert adhoc not in perimeters["europe"].contained_in.all()
 
 
 def test_attach_perimeters_check_executes_directly_with_small_list(user):
-    adhoc = PerimeterFactory(
-        name='Communes littorales',
-        scale=Perimeter.SCALES.adhoc)
-    result = attach_perimeters_check(
-        adhoc,
-        ['34333', '97209'],
-        user)
+    adhoc = PerimeterFactory(name="Communes littorales", scale=Perimeter.SCALES.adhoc)
+    result = attach_perimeters_check(adhoc, ["34333", "97209"], user)
 
-    assert 'direct import' in result['method']
+    assert "direct import" in result["method"]
 
 
 def test_attach_perimeters_check_is_delayed_with_big_list(user):
-    adhoc = PerimeterFactory(
-        name='Communes littorales',
-        scale=Perimeter.SCALES.adhoc)
-    result = attach_perimeters_check(
-        adhoc,
-        list(range(10001)),
-        user)
+    adhoc = PerimeterFactory(name="Communes littorales", scale=Perimeter.SCALES.adhoc)
+    result = attach_perimeters_check(adhoc, list(range(10001)), user)
 
-    assert 'delayed import' in result['method']
+    assert "delayed import" in result["method"]
 
 
 def test_attach_perimeters_cleans_old_data(perimeters):
     """Attaching perimeters removes all other attachments."""
 
-    adhoc = PerimeterFactory(
-        name='Communes littorales',
-        scale=Perimeter.SCALES.adhoc)
-    perimeters['rodez'].contained_in.add(adhoc)
-    attach_perimeters(
-        adhoc,
-        ['34333', '97209'])  # Vic-la-gardiole, Fort-de-France
+    adhoc = PerimeterFactory(name="Communes littorales", scale=Perimeter.SCALES.adhoc)
+    perimeters["rodez"].contained_in.add(adhoc)
+    attach_perimeters(adhoc, ["34333", "97209"])  # Vic-la-gardiole, Fort-de-France
 
-    assert adhoc not in perimeters['rodez'].contained_in.all()
+    assert adhoc not in perimeters["rodez"].contained_in.all()
 
 
-def test_attach_epci_perimeters(perimeters,  user):
-    """Attaching epci perimeters works as expected."""
+def test_attach_epci_perimeters_with_names(perimeters, user):
+    """Attaching epci perimeters through a names list works as expected."""
 
-    epci_1 = PerimeterFactory(
-        name='EPCI 1',
-        scale=Perimeter.SCALES.epci)
-    perimeters['vic'].contained_in.add(epci_1)
-    perimeters['herault'].contained_in.add(epci_1)
+    epci_1 = PerimeterFactory(name="EPCI 1", scale=Perimeter.SCALES.epci)
+    perimeters["vic"].contained_in.add(epci_1)
+    perimeters["herault"].contained_in.add(epci_1)
 
-    epci_2 = PerimeterFactory(
-        name='EPCI 2',
-        scale=Perimeter.SCALES.epci)
-    perimeters['rodez'].contained_in.add(epci_2)
+    epci_2 = PerimeterFactory(name="EPCI 2", scale=Perimeter.SCALES.epci)
+    perimeters["rodez"].contained_in.add(epci_2)
 
-    adhoc = PerimeterFactory(
-        name='Syndicat mixte',
-        scale=Perimeter.SCALES.adhoc)
+    adhoc = PerimeterFactory(name="Syndicat mixte", scale=Perimeter.SCALES.adhoc)
 
-    attach_epci_perimeters(
-        adhoc,
-        [epci_1.name, epci_2.name],
-        user)
+    attach_epci_perimeters(adhoc, [epci_1.name, epci_2.name], user, "names")
 
-    assert adhoc in perimeters['vic'].contained_in.all()
-    assert adhoc in perimeters['herault'].contained_in.all()
-    assert adhoc in perimeters['vic'].contained_in.all()
+    assert adhoc in perimeters["vic"].contained_in.all()
+    assert adhoc in perimeters["herault"].contained_in.all()
+    assert adhoc in perimeters["rodez"].contained_in.all()
+
+
+def test_attach_epci_perimeters_with_codes(perimeters, user):
+    """Attaching epci perimeters through a Siren codes list works as expected."""
+
+    epci_1 = PerimeterFactory(code="200070506", scale=Perimeter.SCALES.epci)
+    perimeters["vic"].contained_in.add(epci_1)
+    perimeters["herault"].contained_in.add(epci_1)
+
+    epci_2 = PerimeterFactory(code="200069433", scale=Perimeter.SCALES.epci)
+    perimeters["rodez"].contained_in.add(epci_2)
+
+    adhoc = PerimeterFactory(name="Syndicat mixte", scale=Perimeter.SCALES.adhoc)
+
+    attach_epci_perimeters(adhoc, [epci_1.code, epci_2.code], user, "codes")
+
+    assert adhoc in perimeters["vic"].contained_in.all()
+    assert adhoc in perimeters["herault"].contained_in.all()
+    assert adhoc in perimeters["rodez"].contained_in.all()
