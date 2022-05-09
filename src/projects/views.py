ee9321e1f37fb5f8d241689a169bc1c110ddf75f
SarahRubio <sarahrubio.pro@gmail.com>
2022-05-09 18:37:41 +0200

    create modal to display the number of potential aids matching the project's name

--- src/projects/views.py
+++ src/projects/views.py
@@ -1,4 +1,11 @@
-from django.views.generic import CreateView, ListView, UpdateView, DetailView, DeleteView
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DetailView,
+    DeleteView,
+    TemplateView,
+)
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib import messages
@@ -8,17 +15,18 @@ from braces.views import MessageMixin
 from projects.tasks import send_project_deleted_email
 from projects.forms import ProjectCreateForm, ProjectUpdateForm
 from projects.models import Project
-from aids.models import AidProject
+from aids.models import AidProject, Aid
 from aids.views import AidPaginator
+from aids.forms import AidSearchForm
 from accounts.mixins import ContributorAndProfileCompleteRequiredMixin
 
 
 class ProjectCreateView(ContributorAndProfileCompleteRequiredMixin, CreateView):
     """Allows users to create their own projects."""
 
-    template_name = 'projects/_create_project_modal.html'
+    template_name = "projects/_create_project_modal.html"
     form_class = ProjectCreateForm
-    context_object_name = 'project'
+    context_object_name = "project"
 
     def form_valid(self, form):
 
@@ -28,52 +36,67 @@ class ProjectCreateView(ContributorAndProfileCompleteRequiredMixin, CreateView):
         project.author.add(self.request.user)
         project.organizations.add(self.request.user.beneficiary_organization)
 
-        msg = 'Votre nouveau projet a été créé&nbsp;!'
+        msg = "Votre nouveau projet a été créé&nbsp;!"
         messages.success(self.request, msg)
-        url = reverse('project_list_view')
-        url = '{}'.format(url)
+        url = reverse("project_list_view")
+        project = f"project_created={project.pk}"
+        url = "{}?{}".format(url, project)
         return HttpResponseRedirect(url)
 
 
 class ProjectListView(ContributorAndProfileCompleteRequiredMixin, ListView):
     """User Project Dashboard"""
 
-    template_name = 'projects/projects_list.html'
-    context_object_name = 'projects'
+    template_name = "projects/projects_list.html"
+    context_object_name = "projects"
     paginate_by = 18
     paginator_class = AidPaginator
 
     def get_queryset(self):
         if self.request.user.beneficiary_organization is not None:
-            queryset = Project.objects \
-                .filter(organizations=self.request.user.beneficiary_organization.pk)
+            queryset = Project.objects.filter(
+                organizations=self.request.user.beneficiary_organization.pk
+            )
         else:
             queryset = Project.objects.none()
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user
-        context['project_create_form'] = ProjectCreateForm(label_suffix='')
+        context["user"] = self.request.user
+        if "project_created" in self.request.GET:
+            context["project_created"] = self.request.GET["project_created"]
+            project_created = Project.objects.get(
+                pk=self.request.GET["project_created"]
+            )
+            text = project_created.name.split(" ")
+            text_encoded = "+".join(text)
+            aids = Aid.objects.live()
+            form = AidSearchForm({"text": text_encoded})
+            qs = form.filter_queryset(aids)
+            aid_results = qs.count()
+            context["aid_results"] = aid_results
+            context["text_encoded"] = text_encoded
+
+        context["project_create_form"] = ProjectCreateForm(label_suffix="")
 
         return context
 
 
 class ProjectDetailView(ContributorAndProfileCompleteRequiredMixin, DetailView):
-    template_name = 'projects/project_detail.html'
-    context_object_name = 'project'
+    template_name = "projects/project_detail.html"
+    context_object_name = "project"
 
     def get_queryset(self):
-        queryset = Project.objects \
-            .prefetch_related('aid_set')
+        queryset = Project.objects.prefetch_related("aid_set")
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user
-        context['aid_set'] = self.object.aid_set.all()
-        context['AidProject'] = AidProject.objects.filter(project=self.object.pk)
-        context['project_update_form'] = ProjectUpdateForm(label_suffix='')
+        context["user"] = self.request.user
+        context["aid_set"] = self.object.aid_set.all()
+        context["AidProject"] = AidProject.objects.filter(project=self.object.pk)
+        context["project_update_form"] = ProjectUpdateForm(label_suffix="")
         return context
 
 
@@ -86,7 +109,7 @@ class ProjectDeleteView(ContributorAndProfileCompleteRequiredMixin, DeleteView):
         return super().get_queryset()
 
     def get_success_url(self):
-        url = reverse('project_list_view')
+        url = reverse("project_list_view")
         return url
 
     def delete(self, *args, **kwargs):
@@ -99,16 +122,18 @@ class ProjectDeleteView(ContributorAndProfileCompleteRequiredMixin, DeleteView):
             user_email = user.email
             if user_email != eraser_email:
                 send_project_deleted_email.delay(user_email, project_name, eraser_name)
-        msg = f"Votre projet { project_name } a bien été supprimé." # noqa
+        msg = f"Votre projet { project_name } a bien été supprimé."  # noqa
         messages.success(self.request, msg)
         return res
 
 
-class ProjectUpdateView(ContributorAndProfileCompleteRequiredMixin, MessageMixin, UpdateView):
+class ProjectUpdateView(
+    ContributorAndProfileCompleteRequiredMixin, MessageMixin, UpdateView
+):
     """Edit an existing project."""
 
-    template_name = 'projects/update_project.html'
-    context_object_name = 'project'
+    template_name = "projects/update_project.html"
+    context_object_name = "project"
     form_class = ProjectUpdateForm
 
     def get_queryset(self):
@@ -127,9 +152,9 @@ class ProjectUpdateView(ContributorAndProfileCompleteRequiredMixin, MessageMixin
 
     def get_success_url(self):
         url = self.object.get_absolute_url()
-        return '{}'.format(url)
+        return "{}".format(url)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['project'] = self.object
+        context["project"] = self.object
         return context
