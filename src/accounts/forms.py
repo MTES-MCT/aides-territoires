c737296b9a60db2659c7ab41d3f0b057dccc7d05
Ash Crow <sylvain@ashtree.eu>
2022-05-16 17:17:46 +0200

    Fix more errors from the W3C checker, add skiplinks to home

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -65,9 +65,13 @@ class RegisterForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs.update({'autofocus': True})
-        self.fields['email'].widget.attrs.update({
-            'placeholder': "Merci de bien vérifier l'adresse saisie."})
+        self.fields["first_name"].widget.attrs.update({"autofocus": True})
+        self.fields["email"].widget.attrs.update(
+            {
+                "placeholder": "Merci de bien vérifier l'adresse saisie.",
+                "autofocus": False,
+            }
+        )
 
     def clean_email(self):
         email = self.cleaned_data['email']
