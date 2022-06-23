666f8799dfc38222cd189b301502724d28b8a9cb
Ash Crow <sylvain@ashtree.eu>
2022-06-23 11:14:38 +0200

    Fix form for stats

--- src/stats/forms.py
+++ src/stats/forms.py
@@ -2,43 +2,45 @@ import datetime
 from django import forms
 from django.utils import timezone
 
+from dsfr.forms import DsfrBaseForm
 
-class StatSearchForm(forms.Form):
+
+class StatSearchForm(DsfrBaseForm):
 
     start_date = forms.DateTimeField(
         label="Date de début",
         required=True,
-        widget=forms.TextInput(
-            attrs={'type': 'date', 'placeholder': 'jj/mm/aaaa'}))
+        widget=forms.TextInput(attrs={"type": "date", "placeholder": "jj/mm/aaaa"}),
+    )
 
     end_date = forms.DateTimeField(
         label="Date de fin",
         required=False,
-        widget=forms.TextInput(
-            attrs={'type': 'date', 'placeholder': 'jj/mm/aaaa'}))
+        widget=forms.TextInput(attrs={"type": "date", "placeholder": "jj/mm/aaaa"}),
+    )
 
     def clean(self):
         """Validation routine (frontend form only)."""
 
         data = super().clean()
 
-        if data.get('start_date'):
+        if data.get("start_date"):
             try:
-                start_date = data.get('start_date').strftime('%Y-%m-%d')
-                datetime.datetime.strptime(start_date, '%Y-%m-%d')
+                start_date = data.get("start_date").strftime("%Y-%m-%d")
+                datetime.datetime.strptime(start_date, "%Y-%m-%d")
             except ValueError:
                 msg = "Le format de la date est incorrect."
-                self.add_error('start_date', msg)
+                self.add_error("start_date", msg)
 
-        if data.get('start_date'):
-            if data.get('start_date') > timezone.now():
+        if data.get("start_date"):
+            if data.get("start_date") > timezone.now():
                 msg = "la date de début ne peut être dans le futur."
-                self.add_error('start_date', msg)
-            elif data.get('end_date') > timezone.now():
+                self.add_error("start_date", msg)
+            elif data.get("end_date") > timezone.now():
                 msg = "la date de fin ne peut être dans le futur."
-                self.add_error('start_date', msg)
-            elif data.get('start_date') > data.get('end_date'):
+                self.add_error("start_date", msg)
+            elif data.get("start_date") > data.get("end_date"):
                 msg = "Merci de choisir une date de début antérieure à la date de fin."
-                self.add_error('start_date', msg)
+                self.add_error("start_date", msg)
 
         return data
