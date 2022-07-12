2a800787538d0e021c0ddef3922fdb6d91d35e86
SarahRubio <sarahrubio.pro@gmail.com>
2022-07-12 10:59:34 +0200

    add send_invite_collaborator_email tests

--- src/accounts/tests/test_invite_collaborator.py
+++ src/accounts/tests/test_invite_collaborator.py
@@ -140,3 +140,63 @@ def test_can_invite_user_with_no_existing_account(client, user):
     collaborator_list_page = reverse("collaborators")
     res = client.get(collaborator_list_page, follow=True)
     assert "Votre invitation a bien été envoyée" in res.content.decode()
+
+
+def test_send_an_invitation_email_to_user_with_no_existing_account(client, user, mailoutbox):
+    """An email is send to the invited user with no existing account"""
+
+    organization = OrganizationFactory()
+    user = UserFactory(beneficiary_organization=organization)
+    organization.beneficiaries.add(user)
+    organization.save()
+
+    client.force_login(user)
+
+    invite_collaborator_url = reverse("invite_collaborator")
+    res = client.post(
+        invite_collaborator_url,
+        {
+            "first_name": "New",
+            "last_name": "Collaborator",
+            "email": "hasno.account@example.org",
+        },
+    )
+
+    assert res.status_code == 302
+    assert len(mailoutbox) == 1
+
+    mail = mailoutbox[0]
+    assert mail.subject == "invitation à collaborer sur Aides-territoires"
+
+
+def test_send_an_invitation_email_to_user_with_an_existing_account(client, user, mailoutbox):
+    """An email is send to the invited user with an existing account"""
+
+    organization = OrganizationFactory()
+    organization2 = OrganizationFactory()
+    user = UserFactory(beneficiary_organization=organization)
+    user2 = UserFactory(
+        email="hasalready.anaccount@example.org", beneficiary_organization=organization2
+    )
+    organization.beneficiaries.add(user)
+    organization.save()
+    organization2.beneficiaries.add(user2)
+    organization2.save()
+
+    client.force_login(user)
+
+    invite_collaborator_url = reverse("invite_collaborator")
+    res = client.post(
+        invite_collaborator_url,
+        {
+            "first_name": "New",
+            "last_name": "Collaborator",
+            "email": "hasalready.anaccount@example.org",
+        },
+    )
+
+    assert res.status_code == 302
+    assert len(mailoutbox) == 1
+
+    mail = mailoutbox[0]
+    assert mail.subject == "invitation à collaborer sur Aides-territoires"
