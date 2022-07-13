ea37abe0fb8774fdf72915fdf3e4b3e0753e52f2
SarahRubio <sarahrubio.pro@gmail.com>
2022-07-13 14:17:20 +0200

    add test_invited_user_can_refuse_to_join_a_new_organization

--- src/accounts/tests/test_invite_collaborator.py
+++ src/accounts/tests/test_invite_collaborator.py
@@ -1,4 +1,4 @@
-"""Test invite collaborator functionnality."""
+"""Test invite collaborator and fusion organization functionnalities."""
 
 import pytest
 
@@ -109,6 +109,9 @@ def test_can_invite_user_with_an_existing_account(client):
         },
     )
     assert res.status_code == 302
+    user2.refresh_from_db()
+    assert user2.proposed_organization == organization
+    assert user2.invitation_author == user
     collaborator_list_page = reverse("collaborators")
     res = client.get(collaborator_list_page, follow=True)
     assert (
@@ -142,7 +145,7 @@ def test_can_invite_user_with_no_existing_account(client):
     assert "Votre invitation a bien été envoyée" in res.content.decode()
 
 
-def test_send_an_invitation_email_to_user_with_no_existing_account(client, mailoutbox):
+def test_an_invitation_email_is_sent_to_user_with_no_existing_account(client, mailoutbox):
     """An email is send to the invited user with no existing account"""
 
     organization = OrganizationFactory()
@@ -169,7 +172,7 @@ def test_send_an_invitation_email_to_user_with_no_existing_account(client, mailo
     assert mail.subject == "invitation à collaborer sur Aides-territoires"
 
 
-def test_send_an_invitation_email_to_user_with_an_existing_account(client, mailoutbox):
+def test_an_invitation_email_is_sent_to_user_with_an_existing_account(client, mailoutbox):
     """An email is send to the invited user with an existing account"""
 
     organization = OrganizationFactory()
@@ -200,3 +203,38 @@ def test_send_an_invitation_email_to_user_with_an_existing_account(client, mailo
 
     mail = mailoutbox[0]
     assert mail.subject == "invitation à collaborer sur Aides-territoires"
+
+
+def test_invited_user_can_refuse_to_join_a_new_organization(client, mailoutbox):
+    """An email is send to the invited user with an existing account"""
+    
+    organization = OrganizationFactory()
+    organization2 = OrganizationFactory()
+    invitation_author = UserFactory(email="author.oftheinvitation@example.org", beneficiary_organization=organization2)
+    user = UserFactory(beneficiary_organization=organization, proposed_organization=organization2, invitation_author=invitation_author)
+    organization.beneficiaries.add(user)
+    organization.save()
+
+    client.force_login(user)
+
+    join_organization_url = reverse('join_organization')
+    res = client.post(
+        join_organization_url,
+        {
+            "no-join": True,
+        },
+    )
+
+    assert res.status_code == 302
+
+    user.refresh_from_db()
+    assert user.beneficiary_organization == organization
+    assert user.proposed_organization == None
+
+    assert len(mailoutbox) == 1
+    mail = mailoutbox[0]
+    assert mail.subject == "Rejet de votre invitation à collaborer sur Aides-territoires"
+    
+    user_dashboard_page = reverse("user_dashboard")
+    res = client.get(user_dashboard_page, follow=True)
+    assert "Votre refus a bien été pris en compte." in res.content.decode()
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -405,14 +405,14 @@ class JoinOrganization(ContributorAndProfileCompleteRequiredMixin, FormView):
         proposed_organization = self.request.user.proposed_organization
         invitation_author = self.request.user.invitation_author
 
-        if self.request.POST.get("action") == "no-join":
+        if 'no-join' in self.request.POST:
             send_reject_invitation_email.delay(
                 invitation_author.email, user.full_name, proposed_organization.pk
             )
             track_goal(self.request.session, settings.GOAL_REGISTER_ID)
             user_queryset.update(proposed_organization=None)
             msg = "Votre refus a bien été pris en compte. Un email indiquant votre refus a été envoyé."  # noqa
-        elif self.request.POST.get("action") == "yes-join":
+        elif 'yes-join' in self.request.POST:
             projects = form.cleaned_data["projects"]
             collaborators = form.cleaned_data["collaborators"]
 
