066a32dac1e3c06fac8a73cadff47de4cfc02548
Ash Crow <sylvain@ashtree.eu>
2022-08-01 16:49:40 +0200

    Add validators for siret and APE/NAF

--- src/geofr/services/validators.py
+++ src/geofr/services/validators.py
@@ -1,8 +1,7 @@
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 from stdnum.exceptions import InvalidChecksum, InvalidFormat, InvalidLength
-from stdnum.fr import siren
+from stdnum.fr import siren, siret
 
 
 validate_insee_region = RegexValidator(r"^\d\d$")
@@ -15,12 +14,26 @@ validate_insee_commune = RegexValidator(
 # Departement number has to be valid, but for the following digits,
 # only their count is checked, not if the code actually exists
 
+validate_naf = RegexValidator(r"^[0-9]{2}\.?[0-9]{2}[a-zA-Z]{1}$")
+# APE (Activité Principale Exercée) or NAF (Nomenclature des Activités Françaises):
+# 4 numbers and a letter. There can be a dot (.) between the first two numbers and the next ones
+
 
 def validate_siren(value):
     try:
         siren.validate(value)
     except (InvalidChecksum, InvalidFormat, InvalidLength):
         raise ValidationError(
-            _("%(value)s is not a valid siren id"),
+            "%(value)s n’est pas un identifiant SIREN valide",
+            params={"value": value},
+        )
+
+
+def validate_siret(value):
+    try:
+        siret.validate(value)
+    except (InvalidChecksum, InvalidFormat, InvalidLength):
+        raise ValidationError(
+            "%(value)s n’est pas un identifiant SIRET valide",
             params={"value": value},
         )
