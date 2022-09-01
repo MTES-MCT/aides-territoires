54ca9734bfb6c8e119199b69597accc774b44979
Ash Crow <sylvain@ashtree.eu>
2022-09-01 11:18:34 +0200

    black on projects

--- src/projects/migrations/0012_alter_project_options.py
+++ src/projects/migrations/0012_alter_project_options.py
@@ -6,12 +6,16 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0011_translate_verbose_name'),
+        ("projects", "0011_translate_verbose_name"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='project',
-            options={'ordering': ['-id'], 'verbose_name': 'projet', 'verbose_name_plural': 'projets'},
+            name="project",
+            options={
+                "ordering": ["-id"],
+                "verbose_name": "projet",
+                "verbose_name_plural": "projets",
+            },
         ),
     ]
