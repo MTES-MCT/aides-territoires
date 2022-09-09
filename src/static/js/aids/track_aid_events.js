84c08140418fba5127b9aaad85401250af49e628
SarahRubio <sarahrubio.pro@gmail.com>
2022-09-09 15:08:05 +0200

    log stats from originurl-btn & applicationurl-btn

--- src/static/js/aids/track_aid_events.js
+++ src/static/js/aids/track_aid_events.js
@@ -5,9 +5,9 @@
      * Hide some piece of information until some button is clicked.
      * This is required to track user engagement.
      */
-    exports.enableCTA = function(dataDiv, revealBtn, aid_slug) {
+    exports.originUrlCTA = function(OriginUrlBtn, aid_slug) {
 
-        revealBtn.click(function() {
+        OriginUrlBtn.click(function() {
 
             // Send an event to our stats DB
             var statsData = JSON.stringify({
@@ -16,7 +16,7 @@
             });
             $.ajax({
                 type: 'POST',
-                url: `/api/stats/aid-contact-click-events/`,
+                url: `/api/stats/aid-originurl-click-events/`,
                 contentType: 'application/json',
                 headers: { 'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value },
                 dataType: 'json',
@@ -25,7 +25,32 @@
 
             // Send an event to Matomo
             if (_paq) {
-                _paq.push(['trackEvent', 'Fiche aide', 'Voir contacts', aid_slug]);
+                _paq.push(['trackEvent', 'Fiche aide', 'Clic lien vers le descriptif complet', aid_slug]);
+            }
+        });
+    };
+
+    exports.applicationUrlCTA = function(ApplicationUrlBtn, aid_slug) {
+
+        ApplicationUrlBtn.click(function() {
+
+            // Send an event to our stats DB
+            var statsData = JSON.stringify({
+                aid: AID_ID,
+                querystring: CURRENT_SEARCH
+            });
+            $.ajax({
+                type: 'POST',
+                url: `/api/stats/aid-applicationurl-click-events/`,
+                contentType: 'application/json',
+                headers: { 'X-CSRFToken': document.getElementsByName('csrfmiddlewaretoken')[0].value },
+                dataType: 'json',
+                data: statsData
+            })
+
+            // Send an event to Matomo
+            if (_paq) {
+                _paq.push(['trackEvent', 'Fiche aide', 'Clic lien candidater', aid_slug]);
             }
         });
     };
@@ -42,11 +67,13 @@
 })(this);
 
 $(document).ready(function () {
-    var dataDiv = $('div#going-further');
-    var revealBtn = $('button#going-further-reveal-button');
+    var dataDiv = $('div#contact');
+    var OriginUrlBtn = $('a#origin_url_btn');
+    var ApplicationUrlBtn = $('a#application_url_btn');
 
-    // Track clicks on "Going further" button
-    enableCTA(dataDiv, revealBtn, AID_SLUG);
+    // Track clicks on "application_url" & "origin_url" buttons
+    originUrlCTA(OriginUrlBtn, AID_SLUG);
+    applicationUrlCTA(ApplicationUrlBtn, AID_SLUG);
 
     // Track clicks on outlinks
     var links = dataDiv.find('a');
