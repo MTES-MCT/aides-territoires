50808ff5913bab105cb0d70f21c107b89eee1d1e
Sylvain Boissel <sylvain@boissel.dev>
2022-09-16 16:57:38 +0200

    Add a sr-only warning tag on external links in imported content

--- src/dataproviders/tests/test_utils.py
+++ src/dataproviders/tests/test_utils.py
@@ -33,6 +33,29 @@ def test_prettify_creates_absolute_urls():
     assert '<a href="https://www.example.com/toto.html">' in text
 
 
+def test_prettify_creates_normalizes_external_urls():
+    html = """
+    Ceci est un <a href="https://www.example.com/toto.html" target="_blank">
+    lien externe</a>
+    """
+    text = content_prettify(html, base_url="https://www.example.org")
+
+    # URLs that open in a new window have a noopener tag appened
+    assert (
+        '<a href="https://www.example.com/toto.html" rel="noopener" target="_blank">'
+        in text
+    )
+
+    # URLs that open in a new window have a special warning for screen readers appened
+    assert (
+        """lien externe
+ <span class="sr-only">
+  Ouvre une nouvelle fenêtre
+ </span>"""
+        in text
+    )
+
+
 def test_get_category_list_from_name():
     category_1 = CategoryFactory(name="Category 1")
     category_2 = CategoryFactory(name="Category 2")
