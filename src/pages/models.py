215007beca971f07d33b9406179714a24282afb7
Sylvain Boissel <sylvain@ashtree.eu>
2022-09-20 14:41:58 +0200

    Black on pages (#1052)

--- src/pages/models.py
+++ src/pages/models.py
@@ -32,30 +32,35 @@ class Page(FlatPage):
     objects = PageQueryset.as_manager()
 
     meta_title = models.CharField(
-        _('Meta title'),
+        _("Meta title"),
         max_length=180,
-        blank=True, default='',
-        help_text=_('This will be displayed in SERPs. '
-                    'Keep it under 60 characters. '
-                    'Leave empty and we will reuse the page title.'))
+        blank=True,
+        default="",
+        help_text=_(
+            "This will be displayed in SERPs. "
+            "Keep it under 60 characters. "
+            "Leave empty and we will reuse the page title."
+        ),
+    )
     meta_description = models.TextField(
-        _('Meta description'),
-        blank=True, default='',
+        _("Meta description"),
+        blank=True,
+        default="",
         max_length=256,
-        help_text=_('This will be displayed in SERPs. '
-                    'Keep it under 120 characters.'))
+        help_text=_(
+            "This will be displayed in SERPs. " "Keep it under 120 characters."
+        ),
+    )
 
     minisite = models.ForeignKey(
-        'search.SearchPage',
-        verbose_name=_('Minisite'),
-        related_name='pages',
-        help_text=_('Optional, link this page to a minisite.'),
+        "search.SearchPage",
+        verbose_name=_("Minisite"),
+        related_name="pages",
+        help_text=_("Optional, link this page to a minisite."),
         on_delete=models.PROTECT,
-        null=True, blank=True)
+        null=True,
+        blank=True,
+    )
 
-    date_created = models.DateTimeField(
-        _('Date created'),
-        default=timezone.now)
-    date_updated = models.DateTimeField(
-        _('Date updated'),
-        auto_now=True)
+    date_created = models.DateTimeField(_("Date created"), default=timezone.now)
+    date_updated = models.DateTimeField(_("Date updated"), auto_now=True)
