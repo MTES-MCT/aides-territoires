ec6680288f4884c8bbaec250e0de299c274c4303
Sylvain Boissel <sylvain@boissel.dev>
2022-09-29 16:41:27 +0200

    Manage acquisition channel fields if the URL parameter 'source' is defined

--- src/static/js/accounts/toggle_acquisition_channel_related_field.js
+++ src/static/js/accounts/toggle_acquisition_channel_related_field.js
@@ -19,23 +19,28 @@
 })(this);
 
 $(document).ready(function () {
+    let allowToggle = true;
     // If a "source" URL parameter is set, set its value in the acquisition_channel_comment field,
+    // And set the acquisition_channel value to "animator"
     let searchParams = new URLSearchParams(window.location.search);
     if (searchParams.has('source')) {
         let source = searchParams.get('source');
+        $("#id_acquisition_channel").val("animator");
         $("#id_acquisition_channel_comment").val(source);
+        allowToggle = false;
     }
 
-    // Only display acquisition_channel related field when the `other`
-    // option is selected.
-    var registerForm = $('form#register-form');
-    var AcquisitionChannelCommentFieldDiv = $('div#acquisition-channel-comment-collapse');
+    if (allowToggle) {
+        // Only display acquisition_channel related field when the `other`
+        // option is selected.
+        var registerForm = $('form#register-form');
+        var AcquisitionChannelCommentFieldDiv = $('div#acquisition-channel-comment-collapse');
 
-    toggleAcquisitionChannelCommentField(registerForm, AcquisitionChannelCommentFieldDiv);
-
-    registerForm.on('change', function () {
         toggleAcquisitionChannelCommentField(registerForm, AcquisitionChannelCommentFieldDiv);
-    });
 
+        registerForm.on('change', function () {
+            toggleAcquisitionChannelCommentField(registerForm, AcquisitionChannelCommentFieldDiv);
+        });
+    }
 
 });
