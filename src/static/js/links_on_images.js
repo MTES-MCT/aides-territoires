781a65da8c802080ebdf2d2a22ffc5094f22efde
Sylvain Boissel <sylvain@boissel.dev>
2022-10-06 17:57:08 +0200

    Fix links

--- src/static/js/links_on_images.js
+++ src/static/js/links_on_images.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // We don't want the visual "after effect" on images links.
     let linksForImages = $("a:has(img)")
-    linksForImages.addClass('no-after')
+    linksForImages.addClass('at-link-blank__none')
 });
