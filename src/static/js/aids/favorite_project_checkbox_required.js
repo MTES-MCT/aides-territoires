92b17eb4ededbcfbae304b15f48c1dfa72fea694
SarahRubio <60744147+SarahRubio@users.noreply.github.com>
2022-10-07 17:13:39 +0200

    Fix : suggérer une aide depuis la page détail d'une aide (#1082)
    * check if at least one checkbox is checked & add missing condition for suggested-aid from aid-detail-page
    
    * add an id for fieldset-favorite-projects
    
    * fix typo
    
    * use same identification for checkbox input

---
+++ src/static/js/aids/favorite_project_checkbox_required.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // Disable submit button until at least one enabled checkbox was checked
+    let submitBtn = $('form#suggest-aid-modal-form button[type=submit]');
+    let checkboxesEnabled = $("#form-group-favorite-projects input[type='checkbox']:not(:disabled)");
+
+    submitBtn.prop('disabled', true);
+    checkboxesEnabled.each(function(index) {
+    	$(this).on('click', function() {
+			if($("#form-group-favorite-projects input[type='checkbox']:checked:not(:disabled)").length) {
+    			submitBtn.prop('disabled', false);
+			} else {
+    			submitBtn.prop('disabled', true);
+			}
+		})
+    });
+});
