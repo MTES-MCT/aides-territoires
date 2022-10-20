6afdceb017d00536b939f5b52bf255c2702fd159
David Larlet <david@larlet.fr>
2022-10-20 18:46:59 -0400

    Amélioration du template_tag de rendu des pourcents

--- src/stats/templatetags/stats.py
+++ src/stats/templatetags/stats.py
@@ -1,14 +1,14 @@
 """Stat rendering helpers."""
 
 from django import template
-
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.simple_tag
-def percent_display(part, whole):
+def percent_display(part, whole, ndigits=2):
     """Percent rendering"""
-    percent = 100 * float(part)/float(whole)
-    round_percent = round(percent, 2)
-    return str(round_percent) + "%"
+    percent = 100 * float(part) / float(whole)
+    round_percent = round(percent, ndigits)
+    return mark_safe(f"{round_percent}&#8239;%")
