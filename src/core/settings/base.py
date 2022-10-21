c988336ee51b77cb8f4fe9a39f250ea1cf6b3461
Sylvain Boissel <sylvain@boissel.dev>
2022-10-21 17:08:12 +0200

    Set whitenoise manifest strict to false

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -155,6 +155,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+WHITENOISE_MANIFEST_STRICT = False
 
 STATIC_URL = "/static/"
 
