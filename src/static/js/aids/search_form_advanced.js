d6680b1a946a72bee9f008637a2c4cc3db26cd6b
Sylvain Boissel <sylvain@boissel.dev>
2022-11-14 16:15:52 +0100

    Improve the render of the search bar on the search page results page

--- src/static/js/aids/search_form_advanced.js
+++ src/static/js/aids/search_form_advanced.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
-
+    // Allows to show/hide the extra search fields
     const moreOptionsToggle = $("#search-form-more-options");
     const extraFields = $(".search-form-extra-field");
 
+    const moreOptionsText = {
+        expand: moreOptionsToggle.html(), // Get the default value from the actual template
+        contract: '<span class="fr-icon-subtract-line" aria-hidden="true"></span> Masquer les critères avancés',
+    }
 
     moreOptionsToggle.on('click', function () {
-        extraFields.show()
+        if (moreOptionsToggle.html() == moreOptionsText.expand) {
+            extraFields.show();
+            moreOptionsToggle.html(moreOptionsText.contract);
+        } else {
+            extraFields.hide();
+            moreOptionsToggle.html(moreOptionsText.expand);
+        }
     });
 });
