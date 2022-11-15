db73345927c7a873f2c54c42aa5e2f679fb1e285
Sylvain Boissel <sylvain@boissel.dev>
2022-11-15 16:47:46 +0100

    Remove SearchMixin

--- src/search/views.py
+++
@@ -1,5 +0,0 @@
-class SearchMixin:
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["querystring"] = self.request.GET.urlencode()
-        return context
