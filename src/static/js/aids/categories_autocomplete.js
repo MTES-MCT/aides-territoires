aead9167c50a25df862c9e30f151fad48b93a608
Sylvain Boissel <sylvain@boissel.dev>
2022-11-17 15:08:20 +0100

    Keep the remove checkboxes

--- src/static/js/aids/categories_autocomplete.js
+++ src/static/js/aids/categories_autocomplete.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     $("select#id_categories").select2({
-        placeholder: "Toutes les catégories",
+        placeholder: "Toutes les thématiques",
         language: "fr",
         theme: "select2-dsfr",
         dropdownAutoWidth: true,
         width: "auto",
     })
-        .on('select2:close', show_number_of_selected)
-        .each(show_number_of_selected);
 });
