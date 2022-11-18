1c2946c462b2aa6f5ac023af85cd822639acd176
Sylvain Boissel <sylvain@boissel.dev>
2022-11-18 16:06:13 +0100

    more readable way to do it

--- src/static/js/aids/sort_by.js
+++ src/static/js/aids/sort_by.js
@@ -1,11 +1,10 @@
 // Manage the "sort by" button outside of the form on the aid search result page"
 $("#order-by").change(function () {
-    let allowed_values = ["relevance", "publication_date", "submission_deadline"]
     let sort_key = $(this).val();
-    if ($.inArray(sort_key, allowed_values) == 0) {
+    if (sort_key == "relevance") {
         // Remove the parameter from the URL in the default case
         set_param_value("order_by", "");
-    } else if ($.inArray(sort_key, allowed_values)) {
+    } else if (["publication_date", "submission_deadline"].indexOf(sort_key) >= 0) {
         set_param_value("order_by", sort_key);
     } else {
         console.log("Invalid sorting value");
