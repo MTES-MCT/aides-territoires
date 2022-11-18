3d50e07fc9bf2e5637ada4c50f72b1da673b54d4
SarahRubio <sarahrubio.pro@gmail.com>
2022-11-18 11:01:12 +0100

    allow superuser to export aidproject-objects from admin

--- src/exporting/tasks.py
+++ src/exporting/tasks.py
@@ -4,8 +4,8 @@ from django.utils import timezone, dateformat
 
 from core.celery import app
 
-from aids.models import Aid
-from aids.resources import AidResource
+from aids.models import Aid, AidProject
+from aids.resources import AidResource, AidProjectResource
 from accounts.models import User
 from accounts.resources import UserResource
 from exporting.models import DataExport
@@ -30,6 +30,25 @@ def export_aids(aids_id_list, author_id, file_format):
     file_content.close()
 
 
+def export_aidprojects(aidprojects_id_list, author_id, file_format):
+    queryset = AidProject.objects.filter(id__in=aidprojects_id_list)
+    exported_data = AidProjectResource().export(queryset)
+    if file_format == "csv":
+        file_content = ContentFile(exported_data.csv.encode())
+    if file_format == "xlsx":
+        file_content = ContentFile(exported_data.xlsx)
+    file_name = "export-aides-"
+    file_name += dateformat.format(timezone.now(), "Y-m-d_H-i-s")
+    file_name += f".{file_format}"
+    file_object = files.File(file_content, name=file_name)
+    DataExport.objects.create(
+        author_id=author_id,
+        exported_file=file_object,
+    )
+    file_object.close()
+    file_content.close()
+
+
 @app.task
 def export_aids_as_csv(aids_id_list, author_id):
     export_aids(aids_id_list, author_id, file_format="csv")
@@ -38,6 +57,16 @@ def export_aids_as_csv(aids_id_list, author_id):
 @app.task
 def export_aids_as_xlsx(aids_id_list, author_id):
     export_aids(aids_id_list, author_id, file_format="xlsx")
+
+
+@app.task
+def export_aidprojects_as_csv(aidprojects_id_list, author_id):
+    export_aidprojects(aidprojects_id_list, author_id, file_format="csv")
+
+
+@app.task
+def export_aidprojects_as_xlsx(aidprojects_id_list, author_id):
+    export_aidprojects(aidprojects_id_list, author_id, file_format="xlsx")
 
 
 def export_users(users_id_list, author_id, file_format):
