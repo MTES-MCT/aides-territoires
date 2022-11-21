21feb0d326966921a9197fdbb2f03c231148e8fb
Sylvain Boissel <sylvain@boissel.dev>
2022-11-21 11:02:31 +0100

    Minor changes

--- src/static/js/aids/destinations_autocomplete.js
+++ src/static/js/aids/destinations_autocomplete.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('select#id_destinations').select2({
-        placeholder: "Toutes les destinations",
+        placeholder: "Tous les types de dépense",
         language: "fr",
         theme: "select2-dsfr",
         dropdownAutoWidth: true,
