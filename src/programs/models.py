6db2c21c2fd2c754647d8b9de65f2e3408dd581d
Sylvain Boissel <sylvain@boissel.dev>
2022-11-23 14:03:45 +0100

    Set ordering of programs by alphabetic order

--- src/programs/models.py
+++ src/programs/models.py
@@ -3,7 +3,6 @@ from os.path import splitext
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
-from django.utils.translation import gettext_lazy as _
 
 
 def logo_upload_to(instance, filename):
@@ -18,28 +17,28 @@ class Program(models.Model):
     """Represents a single aid program.
 
     Real-life examples of such programs:
-      - Territoires d'Industrie
+      - Territoires d’Industrie
       - Action Cœur de Ville
 
     Aid programs regroup new or existing aids, and generally
     are limited to a specific perimeter.
     """
 
-    name = models.CharField(_("Name"), max_length=256)
-    slug = models.SlugField(_("Slug"))
+    name = models.CharField("Nom", max_length=256)
+    slug = models.SlugField("Fragment d’URL")
     short_description = models.CharField(
-        _("Short description"),
-        help_text=_("Will only appear in search results. 300 chars. max."),
+        "Brève description",
+        help_text="300 caractères max. Résultats de recherche uniquement.",
         max_length=300,
     )
-    description = models.TextField(_("Description"))
+    description = models.TextField("Description")
 
     logo = models.FileField(
-        _("Logo"),
+        "Logo",
         null=True,
         blank=True,
         upload_to=logo_upload_to,
-        help_text=_("Make sure the file is not too heavy. Prefer svg files."),
+        help_text="Évitez les fichiers trop lourds. Préférez les fichiers SVG.",
     )
 
     perimeter = models.ForeignKey(
@@ -52,31 +51,29 @@ class Program(models.Model):
 
     # SEO
     meta_title = models.CharField(
-        _("Meta title"),
+        "Titre (balise meta)",
         max_length=60,
         blank=True,
         default="",
-        help_text=_(
-            "This will be displayed in SERPs. "
-            "Keep it under 60 characters. "
-            "Leave empty and we will reuse the program's name."
-        ),
+        help_text="""
+        Le titre qui sera affiché dans les SERPs. Il est recommandé de le garder < 60
+        caractères. Laissez vide pour réutiliser le nom du programme.
+        """,
     )
     meta_description = models.TextField(
-        _("Meta description"),
+        "Description (balise meta)",
         blank=True,
         default="",
         max_length=120,
-        help_text=_(
-            "This will be displayed in SERPs. " "Keep it under 120 characters."
-        ),
+        help_text="Sera affichée dans les SERPs. À garder < 120 caractères.",
     )
 
-    date_created = models.DateTimeField(_("Date created"), default=timezone.now)
+    date_created = models.DateTimeField("Date de création", default=timezone.now)
 
     class Meta:
-        verbose_name = _("Aid program")
-        verbose_name_plural = _("Aid programs")
+        verbose_name = "Programme d’aides"
+        verbose_name_plural = "Programmes d’aides"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
