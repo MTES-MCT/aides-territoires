6cd96be23ea65773971887619d741e9c4f65e7ae
Sylvain Boissel <sylvain@boissel.dev>
2022-12-06 19:58:54 +0100

    allow the deletion of a notification

--- src/notifications/urls.py
+++ src/notifications/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
-from notifications.views import (
-    NotificationListView,
-)
+from notifications.views import NotificationListView, NotificationDeleteView
 
 urlpatterns = [
     path(
         "mes-notifications/",
         NotificationListView.as_view(),
         name="notification_list_view",
+    ),
+    path(
+        "<int:pk>/suppression/",
+        NotificationDeleteView.as_view(),
+        name="notification_delete_view",
     ),
 ]
