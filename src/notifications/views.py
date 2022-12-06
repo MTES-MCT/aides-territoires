94a22a59f952a6d8453928ad541b2b19f8418a2b
Sylvain Boissel <sylvain@boissel.dev>
2022-12-06 19:58:54 +0100

    allow the deletion of a notification

--- src/notifications/views.py
+++ src/notifications/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
+from django.urls import reverse_lazy
+
 from accounts.mixins import ContributorAndProfileCompleteRequiredMixin
 
 from notifications.models import Notification
+from braces.views import MessageMixin
 
 
 class NotificationListView(ContributorAndProfileCompleteRequiredMixin, ListView):
@@ -13,3 +16,24 @@ class NotificationListView(ContributorAndProfileCompleteRequiredMixin, ListView)
     def get_queryset(self):
         queryset = Notification.objects.filter(recipient=self.request.user)
         return queryset
+
+
+class NotificationDeleteView(
+    ContributorAndProfileCompleteRequiredMixin, MessageMixin, DeleteView
+):
+    """Allow user to delete a notification"""
+
+    model = Notification
+    success_url = reverse_lazy("notification_list_view")
+
+    def get_queryset(self):
+        # Only allow deletion on own notifications
+        qs = Notification.objects.filter(recipient=self.request.user)
+        self.queryset = qs
+        return super().get_queryset()
+
+    def delete(self, *args, **kwargs):
+        res = super().delete(*args, **kwargs)
+        msg = "Votre notification a été supprimée."
+        self.messages.success(msg)
+        return res
