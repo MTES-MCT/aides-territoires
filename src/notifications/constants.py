f19713cd75ea7a8be609bb1550e7a83531504ed1
Sylvain Boissel <sylvain@boissel.dev>
2022-12-09 15:40:42 +0100

    Redefine groups

--- src/notifications/constants.py
+++ src/notifications/constants.py
@@ -1,6 +1,14 @@
 NOTIFICATION_TYPES_LIST = [
-    ("account", "Compte utilisateur"),
-    ("aids", "Aides"),
-    ("projects", "Projets"),
+    ("aid_team_frequent", "Notification aide structure (fréquente)"),
+    ("aid_user_frequent", "Notification aide personnelle (fréquente)"),
+    ("aid_user_occasional", "Notification aide personnelle (occasionnelle)"),
+    ("aid_suggested_accepted", "Notification aide suggérée acceptée"),
+    ("aid_suggested_rejected", "Notification aide suggérée rejetée"),
+    ("aid", "Notification aide"),
+    ("internal_team_occasional", "Notification interne structure (occasionnelle)"),
+    ("internal_team_frequent", "Notification interne structure (fréquente)"),
+    ("internal_user_occasional", "Notification interne personnelle (occasionnelle)"),
+    ("project_team", "Notification projet structure"),
+    ("favouriteprojects_user", "Notification projets favori personnelle"),
     ("other", "Autres"),
 ]
