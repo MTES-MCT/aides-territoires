5eedcf86399c16556ccae70502c368e741d74db0
Sylvain Boissel <sylvain@boissel.dev>
2022-12-13 16:53:53 +0100

    add test

--- src/home/tests/test_views.py
+++ src/home/tests/test_views.py
@@ -2,6 +2,8 @@ import pytest
 
 from django.urls import reverse
 
+from accounts.factories import UserFactory
+
 pytestmark = pytest.mark.django_db
 
 
@@ -19,3 +21,16 @@ def test_logged_in_user_has_custom_search_link_in_header(client, contributor):
     client.force_login(contributor)
     res = client.get(reverse("home"))
     assert "?targeted_audiences=commune&amp;perimeter=1" in res.content.decode()
+
+
+def test_logged_in_user_has_pre_filled_contact_form(client):
+    test_user = UserFactory(
+        first_name="Pacôme",
+        last_name="de Champignac",
+        email="pacome.de.champignac@example.org",
+    )
+    client.force_login(test_user)
+    res = client.get(reverse("contact"))
+    assert "Pacôme" in res.content.decode()
+    assert "de Champignac" in res.content.decode()
+    assert "pacome.de.champignac@example.org" in res.content.decode()
