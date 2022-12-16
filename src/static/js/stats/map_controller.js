5b42c4f1419f766e430584e090e40f377f13af14
David Larlet <david@larlet.fr>
2022-12-16 12:35:35 -0500

    Ajout d’informations relatives aux tableaux carto

--- src/static/js/stats/map_controller.js
+++ src/static/js/stats/map_controller.js
@@ -172,6 +172,12 @@ export default class extends Controller {
       <h2>
         Communes et leurs organisations (département ${department})
       </h2>
+      <div class="fr-alert fr-alert--info">
+        <p>
+          Ce tableau comporte l’ensemble des communes
+          (incluant les doublons/inscriptions multiples pour un même périmètre).
+        </p>
+      </div>
       <table
         data-controller="table"
         data-downloadable="true"
@@ -233,6 +239,12 @@ export default class extends Controller {
       <h2>
         EPCI et leurs organisations (département ${department})
       </h2>
+      <div class="fr-alert fr-alert--info">
+        <p>
+          Ce tableau comporte l’ensemble des EPCI
+          (incluant les doublons/inscriptions multiples pour un même périmètre).
+        </p>
+      </div>
       <table
         data-controller="table"
         data-downloadable="true"
