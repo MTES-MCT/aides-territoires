fde3cd010b32f070b6e94312b763e858f69d83de
Sylvain Boissel <sylvain@boissel.dev>
2023-01-25 18:12:51 +0100

    test name

--- src/core/tests/test_templatetags.py
+++ src/core/tests/test_templatetags.py
@@ -6,7 +6,7 @@ def test_phone_number_is_converted():
     assert phone("0123456789") == "01 23 45 67 89"
 
 
-def test_settings():
+def test_cloud_file_url():
     assert (
         cloud_file_url("test_file.mp4")
         == "https://cloud.example.org/test-bucket/upload/test_file.mp4"
