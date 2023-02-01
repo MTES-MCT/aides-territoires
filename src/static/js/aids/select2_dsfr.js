3fc8165d8c56b11c5eda2c5413af8947ea85fc81
Sylvain Boissel <sylvain@boissel.dev>
2023-02-01 12:01:47 +0100

    Fix the error with select2 not able to focus

--- src/static/js/aids/select2_dsfr.js
+++ src/static/js/aids/select2_dsfr.js
@@ -4,3 +4,11 @@ $(document).ready(function () {
   $(".select2-selection--multiple").addClass("fr-select");
   $(".select2-selection--single").addClass("fr-select");
 });
+
+// Fix the bug on select2 not able to focus correctly with jQuery 3.6
+$(document).on('select2:open', function (e) {
+  let searchField = document.querySelector(`[aria-controls="select2-${e.target.id}-results"]`);
+  if (searchField) {
+    searchField.focus()
+  }
+});
