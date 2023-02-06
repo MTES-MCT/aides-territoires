df8fffe3f3ec43b36e9ffdf2062091e5fd7dd862
SarahRubio <60744147+SarahRubio@users.noreply.github.com>
2023-02-06 15:49:13 +0100

    Modification automatique de l’url de la page programme au clic sur un onglet de la page (#1210)
    * add tab parameter in url on click on tab-panel
    
    * use template literals instead of concatenation
    
    * revert last commit
    /bin/bash: q:: command not found
    
    * fix display tab-panel for program-description

---
+++ src/static/js/programs/tab_url_parameters.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    // update url parameter when user click on a program-tab
+    
+	let tabBtns = $('article#program button[id^="tabpanel-"]');
+    
+    tabBtns.each(function(index) {
+		$(this).on('click', function() {
+			let tabBtnId = $(this).attr("id").split("tabpanel-")[1]
+            window.history.pushState({}, '', `?tab=${tabBtnId}`);
+        })
+	})	
+})
