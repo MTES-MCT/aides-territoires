8e58542e5e0ef7e7de94574fecf3b6dc6655437b
SarahRubio <60744147+SarahRubio@users.noreply.github.com>
2023-02-09 10:19:10 +0100

    Update src/aids/forms.py
    Co-authored-by: Sylvain Boissel <sylvain@ashtree.eu>

--- src/aids/forms.py
+++ src/aids/forms.py
@@ -116,7 +116,7 @@ class BaseAidForm(forms.ModelForm, DsfrBaseForm):
             "new_backer": "…ou ajoutez un nouveau porteur d’aide",
             "destinations": "Types de dépenses / actions couvertes",
             "origin_url": "Plus d’informations",
-            "application_url": "Candidater à l'aide",
+            "application_url": "Candidater à l’aide",
             "is_call_for_project": "Cochez cette case s’il s’agit d’un appel à projets (AAP) ou d’un appel à manifestation d’intérêt (AMI)",  # noqa
         }
         for field, label in custom_labels.items():
