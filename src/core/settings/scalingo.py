f679ad604c03d7faebcb63560ac2a7345850a34e
Sylvain Boissel <sylvain@boissel.dev>
2023-02-24 16:57:51 +0100

    mise en place du suivi des requêtes dans Sentry

--- src/core/settings/scalingo.py
+++ src/core/settings/scalingo.py
@@ -34,6 +34,11 @@ sentry_sdk.init(
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
+    # To set a uniform sample rate
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
 )
 
 COMPRESS_OFFLINE = env.bool("COMPRESS_OFFLINE", default=True)
