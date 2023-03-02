b81ede95aad22b8bb4bc0f8f73d05c22e8400545
SarahRubio <sarahrubio.pro@gmail.com>
2023-03-02 14:00:35 +0100

    use ListView to display paginated-aids in backer-detail page

--- src/backers/views.py
+++ src/backers/views.py
@@ -1,23 +1,48 @@
-from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.http import Http404
 
 from backers.models import Backer
 from aids.models import Aid
 from programs.models import Program
 from categories.models import Category
+from aids.views import AidPaginator
 
 
-class BackerDetailView(DetailView):
-    context_object_name = "backer"
+class BackerDetailView(ListView):
     template_name = "backers/detail.html"
-    queryset = Backer.objects.all()
+    context_object_name = "aids"
+    paginate_by = 18
+    paginator_class = AidPaginator
+
+    def get(self, request, *args, **kwargs):
+
+        if "pk" in self.kwargs:
+            backer = self.kwargs.get("pk")
+
+            qs = Backer.objects.filter(pk=backer)
+            try:
+                obj = qs.get()
+            except qs.model.DoesNotExist:
+                raise Http404()
+
+            self.backer = obj
+
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+
+        qs = (
+            Aid.objects.live()
+            .filter(financers=self.backer.id)
+            .prefetch_related("financers")
+        )
+
+        return qs
 
     def get_context_data(self, **kwargs):
 
-        aids = (
-            Aid.objects.live()
-            .filter(financers=self.object.id)
-            .prefetch_related("financers")
-        )
+        backer = self.backer
+        aids = self.object_list
         categories = (
             Category.objects.filter(aids__in=aids)
             .select_related("theme")
@@ -30,7 +55,7 @@ class BackerDetailView(DetailView):
         programs = Program.objects.filter(aids__in=aids).distinct()
 
         context = super().get_context_data(**kwargs)
-        context["aids"] = aids
+        context["backer"] = backer
         context["programs"] = programs
         context["categories"] = categories
         context["backer_page"] = True
