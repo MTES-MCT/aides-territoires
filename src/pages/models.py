b0809ddd644b1242e683b41275bfcf8954c2b384
SarahRubio <sarahrubio.pro@gmail.com>
2023-03-10 09:51:26 +0100

    add search vector field and index to FaqQuestionAnswer model

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.db.models import Value
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.flatpages.models import FlatPage
+from django.contrib.postgres.search import SearchVector, SearchVectorField
+from django.contrib.postgres.indexes import GinIndex
 
 
 class PageQueryset(models.QuerySet):
@@ -147,6 +150,7 @@ class FaqQuestionAnswer(models.Model):
     )
 
     order = models.PositiveIntegerField("Rang", blank=False, default=1)
+    search_vector_faq = SearchVectorField("Search vector FAQ", null=True)
 
     date_created = models.DateTimeField("Date de création", default=timezone.now)
     date_updated = models.DateTimeField("Date de mise à jour", auto_now=True)
@@ -154,7 +158,42 @@ class FaqQuestionAnswer(models.Model):
     class Meta:
         verbose_name = "Question-Réponse de la FAQ"
         verbose_name_plural = "Question-Réponse de la FAQ"
+        indexes = [
+            GinIndex(fields=["search_vector_faq"]),
+        ]
         ordering = ["faq_category", "order", "pk"]
 
     def __str__(self):
         return self.question
+
+    def set_search_vector_faq(
+        self,
+        faq_category=None,
+    ):
+        """Update the full text faq cache field."""
+
+        search_vector_faq = SearchVector(
+            Value(self.answer, output_field=models.CharField()),
+            weight="A",
+            config="french_unaccent",
+        ) + SearchVector(
+            Value(self.question, output_field=models.CharField()),
+            weight="A",
+            config="french_unaccent",
+        )
+
+        if faq_category:
+            search_vector_faq += SearchVector(
+                Value(
+                    " ".join(str(category) for category in faq_category),
+                    output_field=models.CharField(),
+                ),
+                weight="A",
+                config="french_unaccent",
+            )
+
+        self.search_vector_faq = search_vector_faq
+
+    def save(self, *args, **kwargs):
+        self.set_search_vector_faq()
+        return super().save(*args, **kwargs)
