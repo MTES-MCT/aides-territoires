d985fc0fa145ba13fdad0aa54f3c0b336d3aae09
Sylvain Boissel <sylvain@boissel.dev>
2023-03-13 16:18:23 +0100

    Remove obsolete apps tags

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -85,8 +85,6 @@ LOCAL_APPS = [
     "organizations",
     "keywords",
     "notifications",
-    # deprecated apps
-    "tags",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- src/tags/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Tag management.
-
-At a point in the project, it was possible to add multiple tags to an Aid,
-and tags were used in the search form.
-
-This was set aside to be reviewed later, but we never worked on it again.
-
-Currently unused.
-"""
