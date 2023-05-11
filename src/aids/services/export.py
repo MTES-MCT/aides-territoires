07eef80904b812022f9ac813edfcd3125c89b2f6
SarahRubio <sarahrubio.pro@gmail.com>
2023-05-11 12:05:28 +0200

    add AidDetailStatsExportView

--- src/aids/services/export.py
+++ src/aids/services/export.py
@@ -1,8 +1,10 @@
 from io import BytesIO
 import os
+from openpyxl import Workbook
 from xhtml2pdf import pisa
-from datetime import date
+from datetime import date, datetime
 
+from django.http import HttpResponse
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.template.loader import get_template
@@ -94,5 +96,86 @@ def export_aids(organization, file_format: str) -> dict:
         result.close()
     else:
         response = {"error": "Unknown export format", "error_detail": file_format}
+
+    return response
+
+
+def export_aid_stats(
+    aid,
+    start_date,
+    end_date,
+    view_events,
+    application_url_click_events_count,
+    private_projects_linked_count,
+    public_projects_linked_count,
+) -> dict:
+
+    today = date.today()
+    today_formated = today.strftime("%d-%m-%Y")
+
+    if end_date is None:
+        end_date_formated = today_formated
+    else:
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        end_date_formated = end_date.strftime("%d-%m-%Y")
+
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    start_date_formated = start_date.strftime("%d-%m-%Y")
+
+    aid_name = aid.name
+    period = f"du {start_date_formated} au {end_date_formated}"
+    view_events = str(view_events)
+    application_url_click_events_count = str(application_url_click_events_count)
+    private_projects_linked_count = str(private_projects_linked_count)
+    public_projects_linked_count = str(public_projects_linked_count)
+
+    response = HttpResponse(
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    response[
+        "Content-Disposition"
+    ] = "attachment; filename=Aides-territoires-{today_formated}-{aid_name}.xlsx".format(
+        today_formated=today_formated, aid_name=aid_name
+    )
+
+    workbook = Workbook()
+
+    # Get active worksheet/tab
+    worksheet = workbook.active
+    worksheet.title = f"Aides-territoires-statistiques-{today_formated}"
+
+    # Define the titles for columns
+    columns = [
+        "Période choisie",
+        "Nom de l'aide",
+        "Nombre de vues",
+        "Nombre de clics sur Candidater",
+        "Nombre de projets privés liés",
+        "Nombre de projets publics liés",
+    ]
+    row_num = 1
+
+    # Assign the titles for each cell of the header
+    for col_num, column_title in enumerate(columns, 1):
+        cell = worksheet.cell(row=row_num, column=col_num)
+        cell.value = column_title
+
+    row_num += 1
+    # Define the data for each cell in the row
+    row = [
+        period,
+        aid_name,
+        view_events,
+        application_url_click_events_count,
+        private_projects_linked_count,
+        public_projects_linked_count,
+    ]
+
+    # Assign the data for each cell of the row
+    for col_num, cell_value in enumerate(row, 1):
+        cell = worksheet.cell(row=row_num, column=col_num)
+        cell.value = cell_value
+
+    workbook.save(response)
 
     return response
