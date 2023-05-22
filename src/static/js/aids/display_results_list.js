06d48e6d930a1aaac7411b96cd380df74d1bd18d
Sylvain Boissel <sylvain@ashtree.eu>
2023-05-22 10:16:22 +0200

    Améliorer l’accessibilité des boutons d’affichage liste/cartes sur la page de résultats (#1322)
    * Improve a11y of the cards/list toggle on the aids result page
    
    * Move the display out of the buttons themselves

--- src/static/js/aids/display_results_list.js
+++ src/static/js/aids/display_results_list.js
@@ -2,33 +2,39 @@ $(document).ready(function () {
 
     let display_mode = sessionStorage.getItem('display_mode');
 
-    let displayOnList = function () {
+    let displayAsList = function () {
         $('#btn-results-card').addClass('fr-btn--secondary')
         $('#btn-results-list').removeClass('fr-btn--secondary')
-        $('#aid_in_list').removeClass('at-display__none')
-        $('#aid_in_card').addClass('at-display__none')
+        $('#aids-as-list').removeClass('at-display__none')
+        $('#aids-as-card').addClass('at-display__none')
+        $('#btn-results-list').attr("aria-pressed", true)
+        $('#btn-results-card').removeAttr("aria-pressed")
+        $('#display-type').text("Affichage en liste")
     };
 
-    let displayOnCard = function () {
+    let displayAsCard = function () {
         $('#btn-results-card').removeClass('fr-btn--secondary')
         $('#btn-results-list').addClass('fr-btn--secondary')
-        $('#aid_in_list').addClass('at-display__none')
-        $('#aid_in_card').removeClass('at-display__none')
+        $('#aids-as-list').addClass('at-display__none')
+        $('#aids-as-card').removeClass('at-display__none')
+        $('#btn-results-list').removeAttr("aria-pressed")
+        $('#btn-results-card').attr("aria-pressed", true)
+        $('#display-type').text("Affichage en cartes")
     };
 
-    if(display_mode == 'list') {
-        displayOnList();
-    } else if(display_mode == 'card') {
-        displayOnCard();
+    if (display_mode == 'list') {
+        displayAsList();
+    } else if (display_mode == 'card') {
+        displayAsCard();
     }
 
-    $('#btn-results-list').on('click', function() {
-        displayOnList();
+    $('#btn-results-list').on('click', function () {
+        displayAsList();
         sessionStorage.setItem('display_mode', 'list');
     })
 
-    $('#btn-results-card').on('click', function() {
-        displayOnCard();
+    $('#btn-results-card').on('click', function () {
+        displayAsCard();
         sessionStorage.setItem('display_mode', 'card');
     })
 
