76a1c5ab7e7df58cd32c931d24fbf5825db7a811
Sylvain Boissel <sylvain@boissel.dev>
2023-06-01 17:34:57 +0200

    Add a departement filter for the users admin panel

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -17,6 +17,7 @@ from aids.models import Aid
 from accounts.models import User, UserLastConnexion
 from exporting.tasks import export_users_as_csv, export_users_as_xlsx
 from exporting.utils import get_admin_export_message
+from geofr.models import Perimeter
 
 
 class AuthorFilter(InputFilter):
@@ -35,6 +36,31 @@ class AuthorFilter(InputFilter):
                 )
             ).filter(Q(author_name__icontains=value))
             return qs
+
+
+class DepartmentFilter(admin.SimpleListFilter):
+    """Custom admin filter to get users by department."""
+
+    parameter_name = "by_department"
+    title = "Département"
+
+    def lookups(self, request, model_admin):
+        return [(d.code, d.name) for d in Perimeter.objects.departments()]
+
+    def queryset(self, request, queryset):
+        value = self.value()
+
+        if value:
+            is_dept = queryset.filter(
+                beneficiary_organization__perimeter__scale=Perimeter.SCALES.department,
+                beneficiary_organization__perimeter__code=value,
+            )
+            in_dept = queryset.filter(
+                beneficiary_organization__perimeter__departments__contains=[value]
+            )
+
+            return in_dept | is_dept
+        return queryset
 
 
 class SearchPageAdministratorFilter(admin.SimpleListFilter):
@@ -131,6 +157,7 @@ class UserAdmin(BaseUserAdmin, ImportExportActionModelAdmin):
         "is_certified",
         "ml_consent",
         "groups",
+        DepartmentFilter,
     ]
 
     autocomplete_fields = [
