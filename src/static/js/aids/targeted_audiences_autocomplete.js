dfbea7d4f37ad468a8ae46ded06495442d9e3ca4
SarahRubio <sarahrubio.pro@gmail.com>
2023-06-09 20:35:55 +0200

    improve combine_with_form_search function & targeted_audiences search_form_field

--- src/static/js/aids/targeted_audiences_autocomplete.js
+++ src/static/js/aids/targeted_audiences_autocomplete.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
     $('select#id_targeted_audiences').select2({
-        placeholder: "Saisissez quelques caractères pour des suggestions.",
-        language: "fr",
-        theme: "select2-dsfr",
+        placeholder: "Toutes les structures",
+        theme: "select2-dsfr select2-dsfr-checkboxes",
         dropdownAutoWidth: true,
-        width: "auto",
-    });
+        width: "100%",
+        closeOnSelect: false,
+        selectionAdapter: $.fn.select2.amd.require("NumberOfSelectedSelectionAdapter"),
+        templateSelection: (data) => {
+            return format_number_of_selected(data)
+        },
+        dropdownAdapter: $.fn.select2.amd.require("DropdownWithSearchAdapter")
+    })
 });
