a894049c46fc610c7971778893e9eaea190e89c4
Sylvain Boissel <sylvain@boissel.dev>
2023-06-13 12:52:18 +0200

    Remade populate_scots command without pandas

--- src/geofr/management/commands/populate_scots.py
+++ src/geofr/management/commands/populate_scots.py
@@ -1,23 +1,19 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
-# from geofr.services.populate_scots import populate_scots
+from geofr.services.populate_scots import populate_scots
 
 
 class Command(BaseCommand):
     """Import the list of SCoTs."""
 
     def handle(self, *args, **options):
-        print(
-            """Cette commande est temporairement désactivée,
-        cf. https://www.notion.so/Diminuer-la-taille-de-l-environnement-virtuel-Python-e696e339bcb0473490dd79311cc4e83c  # noqa
-        """
-        )
-        """
-        result = populate_scots()
-        self.stdout.write(
-            self.style.SUCCESS(
-                f"{result['created']} created, {result['updated']} \
-                    updated, {result['obsolete']} obsolete."
-            )
-        )
-        """
+        logger = logging.getLogger("console_log")
+        verbosity = int(options["verbosity"])
+        if verbosity > 1:
+            logger.setLevel(logging.DEBUG)
+        else:
+            logger.setLevel(logging.INFO)
+
+        populate_scots()
--- src/geofr/services/populate_scots.py
+++ src/geofr/services/populate_scots.py
@@ -1,23 +1,26 @@
-'''
-import pandas
+import logging
+from openpyxl import load_workbook
+import requests
 
 from django.db import transaction
 from django.utils import timezone
+from django.utils.text import BytesIO
 
 from geofr.models import Perimeter
 from geofr.utils import attach_perimeters
 
+logger = logging.getLogger("console_log")
 
 """
-Our data source comes from the DGALN.
-https://www.data.gouv.fr/fr/datasets/schema-de-coherence-territoriale-scot-donnees-sudocuh-dernier-etat-des-lieux-annuel-au-31-decembre-2021/
+Our data source comes from the Ministère de la Cohésion des territoires.
+https://www.data.gouv.fr/fr/datasets/schema-de-coherence-territoriale-scot-donnees-sudocuh-dernier-etat-des-lieux-annuel-au-31-decembre-2022/
 """
-DATA_URL = "https://www.data.gouv.fr/fr/datasets/r/6e0cfffc-803d-4394-8461-af6e47795c19"
+DATA_URL = "https://www.data.gouv.fr/fr/datasets/r/ffb89961-155d-4134-8682-b10f7aa715c2"
 SHEET_NAME = "Communes Scot"
 
 
 @transaction.atomic
-def populate_scots() -> dict:
+def populate_scots() -> None:
     """
     Import the list of SCoTs.
     """
@@ -26,14 +29,20 @@ def populate_scots() -> dict:
     scots = {}
     nb_created = 0
     nb_updated = 0
+    nb_obsolete = 0
 
-    # Parse the file
-    df = pandas.read_excel(DATA_URL, sheet_name=SHEET_NAME, header=2)
+    logger.debug(f"Getting file {DATA_URL} from distant URL")
+    response = requests.get(DATA_URL)
+    data_file = BytesIO(response.content)
 
-    for _index, row in df.iterrows():
-        scot_id = row["id SCoT"]
-        scot_name = row["SCoT"]
-        insee_code = row["Commune"]
+    wb = load_workbook(data_file)
+    ws = wb[SHEET_NAME]
+
+    logger.debug("Parsing file…")
+    for row in ws.iter_rows(values_only=True, min_row=5):
+        scot_id = row[0]
+        scot_name = row[1]
+        insee_code = row[3]
 
         # Exclude the credit line at the end ("Réalisation : [...]")
         if isinstance(scot_id, int):
@@ -42,19 +51,18 @@ def populate_scots() -> dict:
 
             scots[scot_id]["communes"].append(insee_code)
 
+    logger.debug("Importing data for SCoTs…")
     for scot_id in scots.keys():
-
         # id is just an integer, we use a custom code to make it unique
         scot_code = f"SCOT-{scot_id}"
         scot_name = scots[scot_id]["name"]
+        logger.debug(f"Importing data for {scot_code} ({scot_name})")
 
-        # Create the scot perimeter
+        # Create or update the SCoT perimeter
         scot, created = Perimeter.objects.update_or_create(
             scale=Perimeter.SCALES.adhoc,
             code=scot_code,
-            defaults={
-                "name": scot_name,
-            },
+            defaults={"name": scot_name, "is_obsolete": False, "date_obsolete": None},
         )
         if created:
             nb_created += 1
@@ -65,12 +73,15 @@ def populate_scots() -> dict:
         codes = scots[scot_id]["communes"]
         attach_perimeters(scot, codes)
 
-        # Mark obsolete scots
-        nb_obsolete = Perimeter.objects.filter(
-            scale=Perimeter.SCALES.adhoc,
-            code__startswith="SCOT-",
-            date_updated__lt=start_time,
-        ).update(is_obsolete=True, date_obsolete=start_time)
+    # Mark obsolete SCoTs
+    nb_obsolete = Perimeter.objects.filter(
+        scale=Perimeter.SCALES.adhoc,
+        code__startswith="SCOT-",
+        date_updated__lt=start_time,
+    ).update(is_obsolete=True, date_obsolete=start_time)
 
-    return {"created": nb_created, "updated": nb_updated, "obsolete": nb_obsolete}
-'''
+    end_time = timezone.now()
+    logger.info(f"Import made in {end_time - start_time}.")
+    logger.info(f"* {nb_created} entries created")
+    logger.info(f"* {nb_updated} entries updated")
+    logger.info(f"* {nb_obsolete} entries marked as obsolete")
