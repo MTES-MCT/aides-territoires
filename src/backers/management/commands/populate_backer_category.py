68a3e6a6cb2a5b7a7fb9c1669b85efb1fc8cf4bd
Sylvain Boissel <sylvain@boissel.dev>
2023-06-26 17:17:20 +0200

    Simplify populate script a bit

--- src/backers/management/commands/populate_backer_category.py
+++ src/backers/management/commands/populate_backer_category.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from backers.services.populate_backer_category import (
     create_backer_category,
@@ -13,6 +14,8 @@ class Command(BaseCommand):
     """Import the list of BackerCategories and BackerSubCategories."""
 
     def handle(self, *args, **options):
+        start_time = timezone.now()
+
         logger = logging.getLogger("console_log")
         verbosity = int(options["verbosity"])
         if verbosity > 1:
@@ -23,3 +26,6 @@ class Command(BaseCommand):
         create_backer_category()
         create_and_attached_backer_subcategory()
         attached_backer_group_to_subcategory()
+
+        end_time = timezone.now()
+        logger.info(f"Import made in {end_time - start_time}.")
--- src/backers/services/populate_backer_category.py
+++ src/backers/services/populate_backer_category.py
@@ -3,7 +3,6 @@ import csv
 import logging
 
 from django.db import transaction
-from django.utils import timezone
 
 from backers.models import BackerGroup, BackerCategory, BackerSubCategory
 
@@ -20,10 +19,9 @@ def create_backer_category() -> None:
     Create the backer's category if doesn't exists
     """
 
-    start_time = timezone.now()
-
     backer_category_all = []
     nb_created = 0
+    nb_updated = 0
 
     logger.debug("Parsing file and create backer_categories")
 
@@ -37,10 +35,10 @@ def create_backer_category() -> None:
             if created:
                 nb_created += 1
                 backer_category_all.append(backer_category)
+            else:
+                nb_updated += 1
 
-    end_time = timezone.now()
-    logger.info(f"Import made in {end_time - start_time}.")
-    logger.info(f"* {nb_created} entries created")
+    logger.info(f"{nb_created} categories created, {nb_updated} updated.")
 
 
 @transaction.atomic
@@ -49,10 +47,10 @@ def create_and_attached_backer_subcategory() -> None:
     Create the backer's subcategory if doesn't exists
     And attached the backer's subcategory to a backer's category
     """
-    start_time = timezone.now()
 
     backer_subcategory_all = []
     nb_created = 0
+    nb_updated = 0
 
     logger.debug("Parsing file and create backer_subcategories")
 
@@ -67,10 +65,10 @@ def create_and_attached_backer_subcategory() -> None:
             if created:
                 nb_created += 1
                 backer_subcategory_all.append(backer_subcategory)
+            else:
+                nb_updated += 1
 
-    end_time = timezone.now()
-    logger.info(f"Import made in {end_time - start_time}.")
-    logger.info(f"* {nb_created} entries created")
+    logger.info(f"{nb_created} subcategories created, {nb_updated} updated.")
 
 
 @transaction.atomic
@@ -78,7 +76,6 @@ def attached_backer_group_to_subcategory() -> None:
     """
     Aattached the backer's subcategory to a backer's group
     """
-    start_time = timezone.now()
     nb_updated = 0
 
     logger.debug("Get backer_group object and attached backer_subcategory")
@@ -102,6 +99,4 @@ def attached_backer_group_to_subcategory() -> None:
                 print(backer_group_name)
                 print(e)
 
-    end_time = timezone.now()
-    logger.info(f"Import made in {end_time - start_time}.")
-    logger.info(f"* {nb_updated} entries updated")
+    logger.info(f"{nb_updated} backer groups updated")
