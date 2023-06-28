6ff72683e4b21a60545fcd20318f8389e4537d55
Sylvain Boissel <sylvain@boissel.dev>
2023-06-28 14:48:03 +0200

    update list

--- src/static/js/aids/categories_autocomplete.js
+++ src/static/js/aids/categories_autocomplete.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("select#id_categories").select2({
+    $("#id_categories").select2({
         placeholder: "Toutes les sous-thématiques",
         theme: "select2-dsfr select2-dsfr-checkboxes",
         dropdownAutoWidth: true,
@@ -12,4 +12,14 @@ $(document).ready(function () {
         },
         dropdownAdapter: $.fn.select2.amd.require("DropdownWithSearchAdapter")
     })
+
+    let BACKERS_MAP = $('#department-search-form').length
+    let searchParams = new URLSearchParams(window.location.search)
+
+    if (BACKERS_MAP && searchParams.has('aid_category')) {
+        let categories = searchParams.get('aid_category').split(',')
+        $('#id_categories').val(categories)
+        $('#id_categories').trigger('change')
+    }
+
 });
