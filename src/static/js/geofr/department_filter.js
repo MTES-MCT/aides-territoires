4afdcb02cf7ad90807fa98de4929745dbabc8aec
Sylvain Boissel <sylvain@ashtree.eu>
2023-06-29 17:59:25 +0200

    Prevent department_filter.js to execute on the wrong fields on other pages than the dept view (#1380)

--- src/static/js/geofr/department_filter.js
+++ src/static/js/geofr/department_filter.js
@@ -24,49 +24,51 @@ function department_filter(return_page) {
         }
     });
 
-    /* use the org filter */
-    $("#select-organization").change(function () {
-        let audience = $(this).val();
-        if (audience.match(SANE_ID_REGEX)) {
-            set_param_value("target_audience", audience);
-        } else {
-            console.log("Invalid target audience id");
-        }
-    });
+    if ($("#department-search-form-full").length) {
+        /* use the org filter */
+        $("#select-organization").change(function () {
+            let audience = $(this).val();
+            if (audience.match(SANE_ID_REGEX)) {
+                set_param_value("target_audience", audience);
+            } else {
+                console.log("Invalid target audience id");
+            }
+        });
 
-    /* use the aid_type filter */
-    $("#select-aid-type").change(function () {
-        let aid_type = $(this).val();
-        if (aid_type.match(SANE_ID_REGEX)) {
-            set_param_value("aid_type", aid_type);
-        } else {
-            console.log("Invalid aid type id");
-        }
-    });
+        /* use the aid_type filter */
+        $("#select-aid-type").change(function () {
+            let aid_type = $(this).val();
+            if (aid_type.match(SANE_ID_REGEX)) {
+                set_param_value("aid_type", aid_type);
+            } else {
+                console.log("Invalid aid type id");
+            }
+        });
 
-    /* use the perimeter_scale filter */
-    $("#select-perimeter-scale").change(function () {
-        let perimeter_scale = $(this).val();
-        if (perimeter_scale.match(SANE_ID_REGEX)) {
-            set_param_value("perimeter_scale", perimeter_scale);
-        } else {
-            console.log("Invalid perimeter scale");
-        }
-    });
+        /* use the perimeter_scale filter */
+        $("#select-perimeter-scale").change(function () {
+            let perimeter_scale = $(this).val();
+            if (perimeter_scale.match(SANE_ID_REGEX)) {
+                set_param_value("perimeter_scale", perimeter_scale);
+            } else {
+                console.log("Invalid perimeter scale");
+            }
+        });
 
-    /* use the backer_category filter */
-    $("#select-backer-category").change(function () {
-        let backer_category = $(this).val();
-        if (backer_category.match(SANE_ID_REGEX)) {
-            set_param_value("backer_category", backer_category);
-        } else {
-            console.log("Invalid backer category");
-        }
-    });
+        /* use the backer_category filter */
+        $("#select-backer-category").change(function () {
+            let backer_category = $(this).val();
+            if (backer_category.match(SANE_ID_REGEX)) {
+                set_param_value("backer_category", backer_category);
+            } else {
+                console.log("Invalid backer category");
+            }
+        });
 
-    /* use the aid_category filter */
-    $("#id_categories").change(function () {
-        let aid_category = $(this).val();
-        set_param_value("aid_category", aid_category);
-    });
+        /* use the aid_category filter */
+        $("#id_categories").change(function () {
+            let aid_category = $(this).val();
+            set_param_value("aid_category", aid_category);
+        });
+    }
 };
