530f2a0fc810c1240ee223233ef1c67d8216c671
Sylvain Boissel <sylvain@ashtree.eu>
2023-08-07 10:58:26 +0200

    Limiter la taille des slugs des portails (#1406)
    * Add limit to the length of a SearchPage slug
    
    * Update model and migration

--- src/static/js/search/enable_softmaxlength.js
+++ src/static/js/search/enable_softmaxlength.js
@@ -3,6 +3,7 @@
 
     $(document).ready(function () {
         $('form #id_title').softmaxlength();
+        $('form #id_slug').softmaxlength();
         $('form #id_meta_title').softmaxlength();
         $('form #id_meta_description').softmaxlength();
     });
