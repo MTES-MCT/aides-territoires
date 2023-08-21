174293a25aedd62a9762bff480652ac2251a79e4
Sylvain Boissel <sylvain@boissel.dev>
2023-08-21 09:12:56 +0200

    remove print

--- src/pages/views.py
+++ src/pages/views.py
@@ -12,7 +12,6 @@ class PageView(DetailView):
 
     def get(self, request, *args, **kwargs):
         url = self.kwargs.get("url")
-        print(url)
         if "://" in url:
             raise PermissionDenied()
         elif url == "europe/":
