6aac77f93b61ba7d84d393c8052f779b6363dcf9
Sylvain Boissel <sylvain@boissel.dev>
2023-08-21 09:58:27 +0200

    https

--- src/pages/tests/test_views.py
+++ src/pages/tests/test_views.py
@@ -6,7 +6,7 @@ pytestmark = pytest.mark.django_db
 
 
 def test_open_redirects_are_forbidden(client):
-    base_url = reverse("page_view", args=["http://beta.gouv.fr"])
+    base_url = reverse("page_view", args=["https://beta.gouv.fr"])
     res = client.get(base_url)
 
     assert res.status_code == 403
