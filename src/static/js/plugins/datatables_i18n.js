190bf3fcbe48f613d5f8992f484d0568aa8877a0
Sylvain Boissel <sylvain@boissel.dev>
2023-10-05 17:33:49 +0200

    Manage aids draft list filtering on front end

--- src/static/js/plugins/datatables_i18n.js
+++ src/static/js/plugins/datatables_i18n.js
@@ -1,7 +1,7 @@
 let datatables_fr_strings = {
     search: "Filtrer :",
     zeroRecords: "Aucun résultat trouvé",
-    lengthMenu: "Afficher _MENU_ projets",
+    lengthMenu: "Afficher _MENU_ éléments",
     paginate: {
         first: "Premier",
         previous: "Pr&eacute;c&eacute;dent",
